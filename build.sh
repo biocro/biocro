75a155a00afd632d9c0c2ef0b15f84677511f0b0
dlebauer <dlebauer@gmail.com>
2013-08-02 10:32:13 -0500

    simplified build.sh

--- build.sh
+++ build.sh
@@ -11,30 +11,17 @@
 EMAIL="dlebauer@gmail.com"
 
 # location where to install package
-if [ ! -z $R_LIBS_USER ]; then
-    if [ ! -e ${R_LIBS_USER} ]; then mkdir -p ${R_LIBS_USER}; fi
-    rm -rf ${R_LIBS_USER}/PEcAn.*
-    R_LIB_INC="--library=${R_LIBS_USER}"
-else
-    echo "R_LIBS_USER not set"
-    echo "add the following line to your ~/.bashrc:"
-    echo "export R_LIBS_USER=${HOME}/R/library"
-fi
-
-# pull any changes
-git pull > changes.log
-if [ $? != 0 ]; then
-    echo Error pulling > changes.log
-    exit
-fi
+rm -rf ${R_LIBS_USER}/BioCro
+R_LIB_INC="--library=${R_LIBS_USER}"
 
 START=`date +'%s'`
 STATUS="OK"
 
 # get changes
-echo "----------------------------------------------------------------------" >> changes.log
+echo "----------------------------------------------------------------------" > changes.log
 echo "CHANGES" >> changes.log
 echo "----------------------------------------------------------------------" >> changes.log
+
 git log > newlog
 diff git.log newlog | grep '^> ' | sed 's/^> //' > changes.log
 mv newlog git.log
@@ -42,15 +29,7 @@ REVNO=$( git show -s --pretty=format:%T master )
 
 ## check/install package
 
-R CMD check --install-args="--resave-data" ${R_LIB_INC} ../biocro &> out.log
-
-if [ $? -ne 0 ]; then
-    STATUS="BROKEN"
-    echo "----------------------------------------------------------------------" >> changes.log
-    echo "CHECK BioCro BROKEN" >> changes.log
-    echo "----------------------------------------------------------------------" >> changes.log
-    cat out.log >> changes.log
-fi
+R CMD check ${R_LIB_INC} ../biocro &> out.log
 
 ## all done
 TIME=$(echo "`date +'%s'` - $START" |bc -l)
@@ -62,14 +41,16 @@ echo "-------------------------------------------------------" >> changes.log
 
 cat biocro.Rcheck/00install.out>> changes.log
 echo "-------------------------------------------------------" >> changes.log
+rm biocro.Rcheck/00install.out
 
 echo "contents of 00check.log" >> changes.log
 echo "-------------------------------------------------------" >> changes.log
 cat  biocro.Rcheck/00check.log >> changes.log
 echo "-------------------------------------------------------" >> changes.log
+rm biocro.Rcheck/00check.log
 
 cat changes.log
-if [ "$STATUS" == "BROKEN" ]; then
+if [ `grep failed changes.log` ]; then
     cat changes.log | mail -s "BioCro BUILD ${REVNO} is BROKEN" ${EMAIL}
 fi
 
