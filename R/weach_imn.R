ec5c684e7b32cb587917599f33614c05fb0c6b71
ajmann4 <ajmann4@illinois.edu>
2014-02-03 16:37:14 -0600

    updated documentation on OpEC4photo, RUEmod, and weach_imn

--- R/weach_imn.R
+++ R/weach_imn.R
@@ -1,5 +1,67 @@
 ##  BioCro/R/weach_imn.R by Fernando Ezequiel Miguez  Copyright (C) 2011
-
+#' Weather change Iowa Mesonet
+#' 
+#' Manipulates weather data in the format obtained from Iowa Mesonet (see link
+#' below) and returns the format and units needed for most functions in this
+#' package. This function should be used for one year at a time.  It takes and
+#' returns hourly weather information only.
+#' 
+#' This function should be used to transform data from the Iowa Mesonet at
+#' hourly intervals from here:
+#' 
+#' http://mesonet.agron.iastate.edu/agclimate/hist/hourlyRequest.php
+#' 
+#' When selecting to download variables: Air Temperature (Fahrenheit) Solar
+#' Radiation (kilocalories per meter squared) Precipitation (inches) Relative
+#' humidity (percent) Wind Speed (mph)
+#' 
+#' You can read the data directly as it is downloaded making sure you skip the
+#' first 6 lines (This includes the title row).
+#' 
+#' The imported data frame should have 9 columns with:
+#' 
+#' \enumerate{
+#' 
+#' \itemcol 1 site ID \itemcol 2 site name \itemcol 3 date in format
+#' "year-month-day", e.g. '2010-3-25' \itemcol 4 hour in format "hour:minute",
+#' e.g. '15:00' \itemcol 5 temperature (Fahrenheit) \itemcol 6 solar radiation
+#' (kilocalories per meter squared \itemcol 7 precipitation (inches) \itemcol 8
+#' relative humidity (\%). \itemcol 9 wind speed (mph)
+#' 
+#' } %% ~~ If necessary, more details than the description above ~~
+#' 
+#' @param data data as obtained from the Iowa Mesonet (see details) %%
+#' ~~Describe \code{data} here~~
+#' @param lati Latitude, not used at the moment %% ~~Describe \code{lati}
+#' here~~
+#' @param ts Time step, not used at the moment %% ~~Describe \code{ts} here~~
+#' @param temp.units Temperature units. %% ~~Describe \code{temp.units} here~~
+#' @param rh.units Relative humidity units %% ~~Describe \code{rh.units} here~~
+#' @param ws.units wind speed units %% ~~Describe \code{ws.units} here~~
+#' @param pp.units precipitation units %% ~~Describe \code{pp.units} here~~
+#' @param \dots %% ~~Describe \code{\dots} here~~
+#' @return
+#' 
+#' It will return a data frame in the same format as the \code{\link{weach}}
+#' function. %% ~Describe the value returned %% If it is a LIST, use %%
+#' \item{comp1 }{Description of 'comp1'} %% \item{comp2 }{Description of
+#' 'comp2'} %% ...
+#' @note %% ~~further notes~~
+#' @author Fernando E. Miguez
+#' @seealso \code{\link{weach}} %% ~~objects to See Also as \code{\link{help}},
+#' ~~~
+#' @references Iowa Mesonet http://mesonet.agron.iastate.edu/index.phtml %%
+#' ~put references to the literature/web site here ~
+#' @keywords datagen
+#' @examples
+#' 
+#' 
+#' ## Read an example data set from my website
+#' url <- "http://www.agron.iastate.edu/miguezlab/teaching/CropSoilModel/ames_2010-iowamesonet.txt"
+#' ames.wea <- read.table(url, skip = 6)
+#' ames.wea2 <- weach_imn(ames.wea)
+#' 
+#' 
 weach_imn <- function(data, lati, ts=1, temp.units=c("Fahrenheit","Celsius"),
                       rh.units=c("percent","fraction"),
                       ws.units=c("mph","mps"),
