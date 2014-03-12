7f5d53560586add3d790323705d6149def8f308e
ajmann4 <ajmann4@illinois.edu>
2014-03-12 13:22:20 -0500

    added documentation headers for weachNEW, weach365, weach366, and CheckLeapYear

--- R/CheckLeapYear.R
+++ R/CheckLeapYear.R
@@ -1,5 +1,9 @@
-# This fuction checks if year is a leap year. If yes, then returns 1 or 0.
-
+##' This fuction checks if year is a leap year. If yes, then returns 1 or 0.
+##' @param year the year in question
+##' @export CheckLeapYear
+##' @examples 
+##' year==2000 => 1
+##' year==1998 => 0 
 CheckLeapYear<-function(year)
   {
     if( (year%%400==0) || ((year%%100 !=0) && (year%%4==0))) temp<-1 else temp<-0
