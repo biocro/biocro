f39ca55478a238f3375e8ee9443fcd98da175b74
David LeBauer <dlebauer@gmail.com>
2015-03-18 18:36:52 -0500

    made sure that indesn <= nrow(WetDat) in BioGro

--- R/BioCro.R
+++ R/BioCro.R
@@ -340,7 +340,7 @@ BioGro <- function(WetDat, day1=NULL, dayn=NULL,
     tint <- 24 / timestep
     vecsize <- (dayn - (day1-1)) * tint
     indes1 <- (day1-1) * tint
-    indesn <- (dayn) * tint
+    indesn <- min((dayn) * tint, nrow(WetDat))
     
     doy <- WetDat[indes1:indesn,2]
     hr <- WetDat[indes1:indesn,3]
