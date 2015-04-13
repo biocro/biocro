8507cda271ead0a341a9ddec4d3919f7ffb0164c
Deepak Jaiswal <deepakuiuc@gmail.com>
2015-04-13 10:14:17 -0500

    changes made from Fernando are merged from Maize branch. Build/check and vignettes tests are successful without any breaking

--- src/CanA.h
+++ src/CanA.h
@@ -7,6 +7,9 @@
 #ifndef CANA_H
 #define CANA_H
 
+struct ET_Str EvapoTrans2(double Rad, double Iave, double Airtemperature, double RH,
+			 double WindSpeed,double LeafAreaIndex, double CanopyHeight, 
+			  double stomatacond, double leafw, int eteq);
 struct ET_Str EvapoTrans(double Rad, double Itot, double Airtemperature, double RH,
 			 double WindSpeed,double LeafAreaIndex, double CanopyHeight, double StomataWS, int ws,
 			 double vmax2, double alpha2, double kparm, double theta, double beta, double Rd2, double b02, double b12,double upperT,double lowerT);
