52e0e56fdbcf88a0ff6a7dbb989a86fda336ae59
justinmcgrath <mcgrathjustin+github@gmail.com>
2015-11-18 09:43:04 -0600

    Send all appropriate output of build.sh to the log files
    The redirection in build.sh was probably not doing what was intended.
    The direction was changed and the code was rearranged for readability.

--- build.sh
+++ build.sh
@@ -9,55 +9,56 @@
 #-------------------------------------------------------------------------------
 
 EMAIL="dlebauer@gmail.com"
+CHANGES_FILE="changes.log"
 
-# location where to install package
+# Remove the previous installation to avoid problems.
 rm -rf ${R_LIBS_USER}/BioCro
 R_LIB_INC="--library=${R_LIBS_USER}"
 
 START=`date +'%s'`
-STATUS="OK"
 
-# get changes
-echo "----------------------------------------------------------------------" > changes.log
-echo "CHANGES" >> changes.log
-echo "----------------------------------------------------------------------" >> changes.log
+R CMD check ${R_LIB_INC} . &> out.log
+R CMD INSTALL ${R_LIB_INC} . >> out.log 2>&1
+echo "devtools::test()" | R --vanilla >> out.log 2>&1
+
+exec 6>&1  # Store the location of stdout so it can be restored later.
+exec &> $CHANGES_FILE  # Change stdout to point to the file where changes will be logged.
 
 git log > newlog
-diff git.log newlog | grep '^> ' | sed 's/^> //' >> changes.log
+diff git.log newlog | grep '^> ' | sed 's/^> //'
 mv newlog git.log
-REVNO=$( git show -s --pretty=format:%T master )
 
-## check/install package
-
-R CMD check ${R_LIB_INC} . > out.log
-R CMD INSTALL ${R_LIB_INC} . &> out.log
-
-echo "devtools::test()" | R --vanilla &> out.log
-
-## all done
 TIME=$(echo "`date +'%s'` - $START" |bc -l)
-echo "build took ${TIME} seconds." >> changes.log
 
-echo "-------------------------------------------------------" >> changes.log
-echo "results of 00install.out">> changes.log
-echo "-------------------------------------------------------" >> changes.log
+printf "\n%s\n" \
+"-------------------------------------------------------
+CHANGES
+-------------------------------------------------------
+The build took ${TIME} seconds.
 
-cat biocro.Rcheck/00install.out>> changes.log
-echo "-------------------------------------------------------" >> changes.log
-rm biocro.Rcheck/00install.out
+*******************************************************
+The contents of 00install.out
+*******************************************************
+""$(cat BioCro.Rcheck/00install.out)" \
+"-------------------------------------------------------
 
-echo "contents of 00check.log" >> changes.log
-echo "-------------------------------------------------------" >> changes.log
-cat  biocro.Rcheck/00check.log >> changes.log
-echo "-------------------------------------------------------" >> changes.log
-rm biocro.Rcheck/00check.log
+*******************************************************
+The contents of 00check.log
+*******************************************************
+""$(cat BioCro.Rcheck/00check.log)" \
+"-------------------------------------------------------
+"
+
+exec 1>&6  # Make stdout point to whatever it had originally pointed to.
+
+rm BioCro.Rcheck/00check.log BioCro.Rcheck/00install.out
 
 cat changes.log
+
 if [ `grep failed changes.log` ]; then
+	REVNO=$( git show -s --pretty=format:%T master )
     # cat changes.log | mail -s "BioCro BUILD ${REVNO} is BROKEN" ${EMAIL}
-    echo "ERROR BioCro BUILD BROKEN" >&2
+    echo "ERROR: THE BioCro IS BUILD BROKEN" >&2
     exit 1
-
 fi
-
 
