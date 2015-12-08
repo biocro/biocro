5adc9ebc2369193c3999ffd6ff1acd7b8e8e7f2a
justinmcgrath <mcgrathjustin+github@gmail.com>
2015-12-07 18:31:56 -0600

    Add information to test output

--- tests/testthat/test.weach.data.table.R
+++ tests/testthat/test.weach.data.table.R
@@ -1,13 +1,19 @@
 data(forweach.test)
 test_that("weachDT produces the same result as weachNEW, only faster",{
-    fnew <- weachNEW(as.data.frame(forweach.test),
+    cat('Time taken to run weachNEW\n')
+    print(system.time({
+        fnew <- weachNEW(as.data.frame(forweach.test),
                     lat = 40,
                     ts = 1,
                     temp.units = "Celsius",
                     rh.units = "fraction",
                     ws.units = "mps",
                     pp.units = "mm")
-    fdt <- weach.data.table(forweach.test, lat = 40)
+    }))
+    cat('Time taken to run weach.data.table\n')
+    print(system.time({
+        fdt <- weach.data.table(forweach.test, lat = 40)
+    })
     expect_equal(fnew$year, fdt$year)
     expect_equal(fnew$doy, fdt$doy)
     expect_equal(fnew$hour, fdt$hour)
