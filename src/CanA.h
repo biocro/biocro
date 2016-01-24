00a10192e093a3a9e5feed6ea4c0a51d944348a6
justinmcgrath <mcgrathjustin+github@gmail.com>
2016-01-23 20:36:29 -0600

    Remove duplicate declaration of EvapoTrans

--- src/CanA.h
+++ src/CanA.h
@@ -10,9 +10,6 @@
 struct ET_Str EvapoTrans2(double Rad, double Iave, double Airtemperature, double RH,
 			 double WindSpeed,double LeafAreaIndex, double CanopyHeight, 
 			  double stomatacond, double leafw, int eteq);
-struct ET_Str EvapoTrans(double Rad, double Itot, double Airtemperature, double RH,
-			 double WindSpeed,double LeafAreaIndex, double CanopyHeight, double StomataWS, int ws,
-			 double vmax2, double alpha2, double kparm, double theta, double beta, double Rd2, double b02, double b12,double upperT,double lowerT);
 
 struct Light_model {
 	double irradiance_direct;
