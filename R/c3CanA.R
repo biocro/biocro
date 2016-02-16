0f32fd324cd2d67839503aaa868c69f35e268041
justinmcgrath <mcgrathjustin+github@gmail.com>
2016-02-15 21:58:04 -0600

    Formatting changes

--- R/c3CanA.R
+++ R/c3CanA.R
@@ -104,16 +104,13 @@
 ##'
 ##'
 
-c3CanA <- function(lai,doy,hr,solar,temp,rh,windspeed,
-                 lat=40,nlayers=8,kd=0.1,
-                 heightFactor=3,
-                 c3photoControl = list(),
-                 lnControl = list(),
-                   StomWS=1)
-  {
+c3CanA <- function(lai, doy, hr, solar, temp,
+    rh, windspeed, lat=40, nlayers=8, kd=0.1,
+    heightFactor=3, c3photoControl=list(), lnControl=list(), StomWS=1)
+{
     ## Add error checking to this function
-    if(length(c(lai,doy,hr,solar,temp,rh,windspeed)) != 7)
-      stop("all input should be of length 1")
+    if(length(c(lai, doy, hr, solar, temp, rh, windspeed)) != 7)
+      stop("All input should be of length 1")
 
     photoP <- c3photoParms()
     photoP[names(c3photoControl)] <- c3photoControl
@@ -146,15 +143,19 @@ c3CanA <- function(lai,doy,hr,solar,temp,rh,windspeed,
                  as.double(kd), as.double(heightFactor),
                  as.double(lnPs),as.double(StomWS),as.integer(ws))
     return(res)
-  }
-
-
-c3photoParms <- function(vmax=100, jmax=180, Rd=1.1, Catm=380, O2 = 210, b0=0.08, b1=5, theta=0.7,StomWS=1.0,ws=c("gs","vmax")){
-
-ws <- match.arg(ws)
- if(ws == "gs") ws <- 1
- else ws <- 0
-      
-  list(vmax=vmax,jmax=jmax,Rd=Rd,Catm=Catm,O2=O2,b0=b0,b1=b1,theta=theta,StomWS=StomWS,ws=ws)
-
 }
+
+
+c3photoParms <- function(vmax=100, jmax=180, Rd=1.1, Catm=380, O2 = 210,
+    b0=0.08, b1=5, theta=0.7, StomWS=1.0, ws=c("gs", "vmax"))
+{ 
+    if (is.character(ws)) {
+        ws <- match.arg(ws)
+        if(ws == "gs") ws <- 1
+        else ws <- 0
+    }
+
+        list(vmax=vmax, jmax=jmax, Rd=Rd, Catm=Catm, O2=O2,
+            b0=b0, b1=b1, theta=theta, StomWS=StomWS, ws=ws)
+}
+
