cbc5ba054a357a794a538933d2a4c31ad880a3ff
justinmcgrath <mcgrathjustin+github@gmail.com>
2016-03-03 10:16:55 -0600

    Add a parameter check

--- src/RGro.cpp
+++ src/RGro.cpp
@@ -22,6 +22,30 @@ SEXP RGro(SEXP initial_state,
     std::unique_ptr<IModule> canopy = make_module(CHAR(STRING_ELT(canopy_photosynthesis_module, 0)));
     std::unique_ptr<IModule> soil_evaporation = make_module(CHAR(STRING_ELT(soil_evaporation_module, 0)));
 
+    vector<string> required_state = {"iSp", "doy", "SpD", "Leaf",
+                            "LeafN_0", "LeafN", "vmaxb1", "vmax1", "alphab1",
+                            "alpha1", "TTc", "temp", "tbase", "timestep",
+                            "mrc1", "seneLeaf", "Stem", "seneStem",
+                            "mrc2", "Root", "seneRoot", "Rhizome", "seneRhizome"};
+
+    state_map all_state = combine_state(s, ip, vp, 0);
+
+    vector<string> missing_state;
+    for (auto it = required_state.begin(); it != required_state.end(); ++it) {
+        if (all_state.find(*it) == all_state.end()) {
+            missing_state.push_back(*it);
+        }
+    }
+
+    if (!missing_state.empty()) {
+        Rprintf("The following state variables are required but are missing: ");
+        for(vector<string>::iterator it = missing_state.begin(); it != missing_state.end() - 1; ++it) {
+            Rprintf("%s, ", it->c_str());
+        }
+        Rprintf("%s.\n", missing_state.back().c_str());
+        error("This function cannot continue unless all state variables are set.");
+    }
+
     state_vector_map ans;
     try {
         ans = Gro(s, ip, vp, canopy, soil_evaporation, biomass_leaf_nitrogen_limitation);
@@ -30,7 +54,6 @@ SEXP RGro(SEXP initial_state,
             Rprintf("Exception thrown: %s\n", oor.what());
     }
 
-    //SEXP result =  allocVector(VECSXP, 1);
     SEXP result = list_from_map(ans);
     return result;
 }
