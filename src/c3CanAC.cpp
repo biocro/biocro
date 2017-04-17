438b9d487936c52e9e9980bf8fea1450eae4cfe6
justinmcgrath <mcgrathjustin+github@gmail.com>
2017-04-16 21:46:17 -0500

    Clean up formatting.

--- src/c3CanAC.cpp
+++ src/c3CanAC.cpp
@@ -6,7 +6,7 @@ struct Can_Str c3CanAC(double LAI,
 		int DOY,
 		int hr,
 		double solarR,
-		double Temp,
+		double temperature,
 		double RH,
 		double WindSpeed,
 		double lat,
@@ -31,11 +31,6 @@ struct Can_Str c3CanAC(double LAI,
         double electrons_per_carboxylation,
         double electrons_per_oxygenation)
 {
-
-    struct ET_Str tmp5_ET, tmp6_ET; 
-    struct c3_str temp_photo_results = {0, 0, 0, 0};
-    struct Can_Str ans = {0, 0, 0};
-
     const double cf = 3600 * 1e-6 * 30 * 1e-6 * 10000;
 
     /* Need a different conversion factor for transpiration */
@@ -47,36 +42,24 @@ struct Can_Str c3CanAC(double LAI,
     /* 1e-6 - grams per  megagram */
     /* 10000 - meters squared per hectare */
 
-    double Idir, Idiff, cosTh;
-    double IDir, IDiff, Itot, rh, layerWindSpeed;
-    double pLeafsun, pLeafshade;
-    double Leafsun = 0.0, Leafshade = 0.0;
-    double CanHeight;
+    double CanopyA = 0.0, CanopyT = 0.0, GCanopyA = 0.0;
 
-    double TempIdir = 0.0,
-		   AssIdir = 0.0, AssIdiff = 0.0;
-	double GAssIdir = 0.0, GAssIdiff = 0.0; 
-    double TempIdiff = 0.0;
+    double IDir, IDiff;
 
-    double CanopyA = 0.0, CanopyT =0.0, GCanopyA = 0.0;
-    double chil=1.0;
+    double chil = 1.0;
     double vmax1;
 	double leafN_lay;
 
-    struct Light_model light_model;
-    light_model = lightME(lat, DOY, hr);
+    struct Light_model light_model = lightME(lat, DOY, hr);
 
-    Idir = light_model.direct_irradiance_fraction * solarR;
-    Idiff = light_model.diffuse_irradiance_fraction * solarR;
-    cosTh = light_model.cosine_zenith_angle;
+    double Idir = light_model.direct_irradiance_fraction * solarR;
+    double Idiff = light_model.diffuse_irradiance_fraction * solarR;
+    double cosTh = light_model.cosine_zenith_angle;
 
-    struct Light_profile light_profile;
-    light_profile = sunML(Idir, Idiff, LAI, nlayers, cosTh, kd, chil, heightf);
+    struct Light_profile light_profile = sunML(Idir, Idiff, LAI, nlayers, cosTh, kd, chil, heightf);
 
-    /* results from multilayer model */
     double LAIc = LAI / nlayers;
 
-    /* Next I need the RH and wind profile */
     double relative_humidity_profile[nlayers];
     RHprof(RH, nlayers, relative_humidity_profile);
 
@@ -86,66 +69,64 @@ struct Can_Str c3CanAC(double LAI,
     double leafN_profile[nlayers];
     LNprof(leafN, LAI, nlayers, kpLN, leafN_profile);
 
-    /* Next use the EvapoTrans function */
-    for(int i = 0; i < nlayers; ++i)
+    for (int i = 0; i < nlayers; ++i)
     {
         int current_layer = nlayers - 1 - i;
-        /* vmax depends on leaf nitrogen and this in turn depends on the layer */
 		leafN_lay = leafN_profile[current_layer];
-        if(lnfun == 0) {
+        if (lnfun == 0) {
 			vmax1 = Vmax;
 		} else {
 			vmax1 = leafN_lay * lnb1 + lnb0;
-			/* For now alpha is not affected by leaf nitrogen */
 		}
 
-        rh = relative_humidity_profile[current_layer];
-        layerWindSpeed = wind_speed_profile[current_layer];
+        double relative_humidity = relative_humidity_profile[current_layer];
+        double layer_wind_speed = wind_speed_profile[current_layer];
 
         IDir = light_profile.direct_irradiance[current_layer];
-        Itot = light_profile.total_irradiance[current_layer];
-        pLeafsun = light_profile.sunlit_fraction[current_layer];
-        CanHeight = light_profile.height[current_layer];
+        double Itot = light_profile.total_irradiance[current_layer];
+        double pLeafsun = light_profile.sunlit_fraction[current_layer];
+        double CanHeight = light_profile.height[current_layer];
 
-        Leafsun = LAIc * pLeafsun;
+        double Leafsun = LAIc * pLeafsun;
 
-        tmp5_ET = c3EvapoTrans(IDir, Itot, Temp, rh, layerWindSpeed, LAIc, CanHeight,
+        struct ET_Str et_direct = c3EvapoTrans(IDir, Itot, temperature, relative_humidity, layer_wind_speed, LAIc, CanHeight,
 				vmax1, Jmax, Rd, b0, b1, Catm, o2, theta, StomataWS, water_stress_approach, electrons_per_carboxylation, electrons_per_oxygenation);
 
-        TempIdir = Temp + tmp5_ET.Deltat;
-        temp_photo_results = c3photoC(IDir, TempIdir, rh, vmax1, Jmax, Rd, b0, b1, Catm, o2, theta, StomataWS, water_stress_approach, electrons_per_carboxylation, electrons_per_oxygenation);
-        AssIdir = temp_photo_results.Assim;
-        GAssIdir = temp_photo_results.GrossAssim;
+        double leaf_temperature_Idir = temperature + et_direct.Deltat;
+        struct c3_str temp_photo_results = c3photoC(IDir, leaf_temperature_Idir, relative_humidity, vmax1, Jmax, Rd, b0, b1, Catm, o2, theta, StomataWS, water_stress_approach, electrons_per_carboxylation, electrons_per_oxygenation);
+        double AssIdir = temp_photo_results.Assim;
+        double GAssIdir = temp_photo_results.GrossAssim;
 
         IDiff = light_profile.diffuse_irradiance[current_layer];
-        pLeafshade = light_profile.shaded_fraction[current_layer];
-        Leafshade = LAIc * pLeafshade;
+        double pLeafshade = light_profile.shaded_fraction[current_layer];
+        double Leafshade = LAIc * pLeafshade;
 
-        tmp6_ET = c3EvapoTrans(IDiff, Itot, Temp, rh, layerWindSpeed, LAIc, CanHeight,
+        struct ET_Str et_diffuse = c3EvapoTrans(IDiff, Itot, temperature, relative_humidity, layer_wind_speed, LAIc, CanHeight,
 				vmax1, Jmax, Rd, b0, b1, Catm, o2, theta, StomataWS, water_stress_approach, electrons_per_carboxylation, electrons_per_oxygenation);
-        TempIdiff = Temp + tmp6_ET.Deltat;
+        double leaf_temperature_Idiffuse = temperature + et_diffuse.Deltat;
 
-        temp_photo_results = c3photoC(IDiff, TempIdiff, rh, vmax1, Jmax, Rd, b0, b1, Catm, o2, theta, StomataWS, water_stress_approach, electrons_per_carboxylation, electrons_per_oxygenation);
-        AssIdiff = temp_photo_results.Assim;
-        GAssIdiff = temp_photo_results.GrossAssim;
+        temp_photo_results = c3photoC(IDiff, leaf_temperature_Idiffuse, relative_humidity, vmax1, Jmax, Rd, b0, b1, Catm, o2, theta, StomataWS, water_stress_approach, electrons_per_carboxylation, electrons_per_oxygenation);
+        double AssIdiff = temp_photo_results.Assim;
+        double GAssIdiff = temp_photo_results.GrossAssim;
 
         CanopyA += Leafsun * AssIdir + Leafshade * AssIdiff;
         GCanopyA += Leafsun * GAssIdir + Leafshade * GAssIdiff;
-        CanopyT += Leafsun * tmp5_ET.TransR + Leafshade * tmp6_ET.TransR;
+        CanopyT += Leafsun * et_direct.TransR + Leafshade * et_diffuse.TransR;
     }
 
-    /*## These are micro mols of CO2 per m2 per sec for Assimilation
-## and mili mols of H2O per m2 per sec for Transpiration
-## Need to convert to 
-## 3600 converts seconds to hours
-## 10^-6 converts micro mols to mols
-## 30 converts mols of CO2 to grams
-## (1/10^6) converts grams to Mg
-## 10000 scales up to ha */
+    /* These are micro mols of CO2 per m2 per sec for Assimilation
+       and mili mols of H2O per m2 per sec for Transpiration
+       Need to convert to 
+       3600 converts seconds to hours
+       10^-6 converts micro mols to mols
+       30 converts mols of CO2 to grams
+       (1/10^6) converts grams to Mg
+       10000 scales up to ha */
     /* A similar conversion is made for water but
        replacing 30 by 18 and mili mols are converted to
        mols (instead of micro) */
 
+    struct Can_Str ans;
     ans.Assim = cf * CanopyA;
     ans.Trans = cf2 * CanopyT; 
     ans.GrossAssim = cf * GCanopyA;
