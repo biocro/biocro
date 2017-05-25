02dfe436b469b3830ebb13fa44afe7cf271fe23c
justinmcgrath <mcgrathjustin+github@gmail.com>
2017-05-25 16:14:13 -0500

    Remove unpublished files

--- data/sorghum_modules.r
+++
@@ -1,5 +0,0 @@
-sorghum_modules = list(canopy_module_name='c4_canopy',
-    soil_module_name='one_layer_soil_profile',
-    growth_module_name='partitioning_growth',
-    senescence_module_name='thermal_time_senescence')
-
