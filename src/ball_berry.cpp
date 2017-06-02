42d3c69f8baa3d39f582c554ad01847e6d8462d1
justinmcgrath <mcgrathjustin+github@gmail.com>
2017-06-02 12:15:51 -0500

    Use SI units in ballBerry().

--- src/ball_berry.cpp
+++ src/ball_berry.cpp
@@ -10,23 +10,21 @@
 #include "ball_berry.h"
 
 /* Ball Berry stomatal conductance function */
-double ballBerry(double Amu, double Cappm, double Temp, double RelH, double beta0, double beta1)
+double ballBerry(double assimilation, double Ca, double Temp, double RelH, double beta0, double beta1)
 {
     const double gbw = 1.2;  // micromole / m^2 / s.  Collatz et al. (1992) Aust. J. Plant Physiol. pg. 526.
-    const double Camf = Cappm * 1e-6;  // mol / m^2 / s.
-    const double assimn = Amu * 1e-6;  // mol / m^2 / s.
     double gswmol;
 
-    if (assimn < 0.0) {
+    if (assimilation < 0.0) {
         /* Set stomatal conductance to the minimum value, beta0 */
         gswmol = beta0;
     } else {
-        double Cs = Camf - (1.4 / gbw) * assimn;  // mol / mol.
+        double Cs = Ca - (1.4 / gbw) * assimilation;  // mol / mol.
         if (Cs < 0.0) {
             Cs = 1;
         }
 
-        double acs = assimn / Cs;
+        double acs = assimilation / Cs;
         if (acs < 1e-6) {
             acs = 1e-6;
         }
@@ -36,8 +34,8 @@ double ballBerry(double Amu, double Cappm, double Temp, double RelH, double beta
          * 
          * These coefficients come from personal communication with Joe Berry to Steve Long.
          */
-        double a = beta1 * acs;  // Equivalent to a = beta1 * assimn / cs
-        double b = beta0 + gbw - beta1 * acs;  // Equivalent to b = beta0 + gbw - beta1 * assimn / cs
+        double a = beta1 * acs;  // Equivalent to a = beta1 * assimilation / cs
+        double b = beta0 + gbw - beta1 * acs;  // Equivalent to b = beta0 + gbw - beta1 * assimilation / cs
         double c = -RelH * gbw - beta0;
 
         double root_term = b * b - 4 * a * c;
