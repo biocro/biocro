07eace32f2dd69227a8527fcf1589f5ce801afc9
justinmcgrath <mcgrathjustin+github@gmail.com>
2017-06-02 09:18:28 -0500

    Rearrange files.

--- src/c3photo.cpp
+++ src/c3photo.cpp
@@ -3,24 +3,12 @@
  *
  */
 
-#include <R.h>
 #include <math.h>
-#include <boost/units/quantity.hpp>
-#include <boost/units/systems/si.hpp>
-#include <boost/units/systems/si/codata/physico-chemical_constants.hpp>
-#include <boost/units/pow.hpp>
-#include <boost/units/cmath.hpp>
 #include "c3photo.h"
 #include "ball_berry.h"
+#include "biocro_units.h"
 
-using namespace boost::units;
-using namespace boost::units::si;
-using namespace boost::units::si::constants::codata;
-
-typedef divide_typeof_helper<energy, amount>::type energy_over_amount;
-typedef divide_typeof_helper<amount, boost::units::si::time>::type amount_over_time;  // 'time' must be fully qualified because it is already defined elsewhere.
-typedef divide_typeof_helper<amount_over_time, area>::type flux;
-typedef divide_typeof_helper<amount, amount>::type mole_fraction;
+using namespace biocro_units;
 
 /*! Calculate the exponential term of the Arrhenius function.
  *
