19d3cea4ab68726296e7e05f9ce3692b45537ecc
justinmcgrath <mcgrathjustin+github@gmail.com>
2017-11-13 10:48:13 -0600

    Add soil bucket module and soil evaporation module.

--- src/modules.h
+++ src/modules.h
@@ -397,22 +397,55 @@ class one_layer_soil_profile_derivatives : public IModule {
         }
 };
 
+class soil_evaporation : public IModule {
+    public:
+        soil_evaporation()
+            : IModule(std::vector<std::string> {"lai", "temp", "solar", "soil_field_capacity", "soil_wilting_point", "soil_water_content",
+                    "soil_sand_content", "soil_saturated_conductivity", "soil_air_entry", "soil_reflectance", "soil_transmission",
+                    "soil_clod_size", "rsec"},
+                    std::vector<std::string> {})
+            {}
+    private:
+        state_map do_operation(state_map const &s) const
+        {
+            double soilEvap = SoilEvapo(s.at("lai"), 0.68, s.at("temp"), s.at("solar"), s.at("soil_water_content"),
+                        s.at("soil_field_capacity"), s.at("soil_wilting_point"), s.at("windspeed"), s.at("rh"), s.at("rsec"), 
+                        s.at("soil_clod_size"), s.at("soil_reflectance"), s.at("soil_transmission"),
+                        s.at("specific_heat"), s.at("stefan_boltzman"));  // kg / m^2 / s.
+            state_map result { {"soil_evaporation_rate", soilEvap } };
+            return result;
+        }
+};
+
 class bucket_soil_drainage : public IModule {
     public:
         bucket_soil_drainage()
-            : IModule(std::vector<std::string> {"precipitation_rate", "evapotranspiration_rate", "soil_water_content", "soil_field_capacity", "soil_wilting_point",
-                    "soil_depth", "soil_sand_content", "evapotranspiration", "soil_saturated_conductivity", "soil_air_entry",
-                    "soil_b_coefficient"},
+            : IModule(std::vector<std::string> {"precipitation_rate", "soil_evaporation_rate", "soil_water_content", "soil_field_capacity", "soil_wilting_point",
+                    "soil_saturation_capacity", "soil_depth", "soil_sand_content", "transpiration_rate", "soil_saturated_conductivity",
+                    "soil_air_entry", "soil_b_coefficient"},
                     std::vector<std::string> {})
         {}
     private:
         state_map do_operation(state_map const &s) const
         {
-            state_map result;
-            /* TODO:
-             * Need surface water and infiltration rate. Page 129 of Norman and Campbell
-             * gives an equation for drainage, and someone this can be applied to infiltration.
-             */
+            constexpr double g = 9.8; // m / s^2. Acceleration due to gravity.
+            constexpr double density_of_water_at_20_celcius = 998.2;  // kg m^-3.
+            constexpr double seconds_per_hour = 3600;
+
+            double field_capacity = s.at("soil_field_capacity");  // m^3 / m^3.
+            double wilting_point = s.at("soil_wilting_point");  // m^3 / m^3.
+            double water_capacity = s.at("soil_water_content");  // m^3 / m^3.
+            double saturation_capacity = s.at("soil_saturation_capacity");  // m^3 / m^3.
+            double soil_depth = s.at("soil_depth"); // meters;
+
+            /* soil_matric_potential is calculated as per "Dynamic Simulation of Water Deficit Effects upon Maize Yield" R. F. Grant Agricultural Systems. 33(1990) 13-39. */
+            double soil_matric_potential = -exp(log(0.033) + log(field_capacity / awc) / log(field_capacity / wilting_point) * log(1.5 / 0.033)) * 1e3;  // This last term converts from MPa to kPa.
+            double hydraulic_conductivity = s.at("soil_saturated_conductivity") * pow(s.at("soil_air_entry") / soil_matric_potential, 2 + 3 / s.at("soil_b_coefficient");  // kg s / m^3.
+
+            double drainage = - hydraulic_conductivity * g / density_of_water_at_20_celcius;  // m / s.
+            double runoff = std::min(0, (water_capacity - saturation_capacity) / second_per_hour * soil_depth); // m / s. Get rid of any water in excess of the saturation point in one hour.
+
+            state_map result { {"soil_water_content", s.at("precipitation_rate") - s.at("soil_evaporation_rate") - s.at("transpiration_rate") - runoff - drainage } };  // m / s;
             return result;
         };
 };
