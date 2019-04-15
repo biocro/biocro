8ba7fee527928eb185eb4ea4325a3c16e9a6f2d1
eloch216 <48919455+eloch216@users.noreply.github.com>
2019-04-15 16:26:29 -0500

    Rearranged utilization_growth_and_senescence_module

--- src/modules.h
+++ src/modules.h
@@ -140,24 +140,6 @@ class IGrowth_module : public IModule {
         {}
 };
 
-class utilization_growth_and_senescence_module: public ISenescence_module {
-    public:
-        utilization_growth_and_senescence_module()
-            : ISenescence_module("utilization_growth_and_senescence_module",
-                    std::vector<std::string> {"TTc", "seneLeaf", "seneStem", "seneRoot", "seneRhizome",
-                    "Leaf", "Stem", "Root", "Rhizome", "Grain",
-                    "kLeaf", "kStem", "kRoot", "kRhizome", "kGrain",
-                    "KmLeaf", "KmStem", "KmRoot", "KmRhizome", "KmGrain",
-                    "resistance_leaf_to_stem", "resistance_stem_to_grain", "resistance_stem_to_root", "resistance_stem_to_rhizome",
-                    "rate_constant_leaf", "rate_constant_stem", "rate_constant_root", "rate_constant_rhizome",
-                    "remobilization_fraction", "grain_TTc"},
-                    std::vector<std::string> {})
-        {}
-    private:
-        virtual state_map do_operation(state_vector_map const &s_history, state_vector_map const &d_history, state_map const &parameters) const;
-        virtual state_map do_operation(state_map const &s) const;
-};
-
 class utilization_growth_flowering: public ISenescence_module {
     public:
         utilization_growth_flowering()
