e587fdeef230e033ce0528a6f40ecc35159af7ad
Scott Rohde <srohde@igb.illinois.edu>
2019-12-13 14:04:22 -0600

    Testing module creation.

--- src/module_factory_test.cpp
+++ src/module_factory_test.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include "module_factory.h"
 #include "module_wrapper.h"
+#include "module_library/c3_canopy.hpp"
 
 using namespace std;
 
 int main() {
 
-    
+
     auto list = ModuleFactory::get_modules();
 
+    cout << "\nAvailable modules: \n" << endl;
+    
     for (auto s : list) {
         cout << s << endl;
     }
@@ -16,21 +19,34 @@ int main() {
     module_wrapper_base*  w = ModuleFactory::create("c3_canopy");
     auto inputs = w->get_inputs();
 
-    cout << "Inputs: " << endl;
-    
+    cout << "\nInputs: \n" << endl;
+
+    std::unordered_map<std::string, double> input;
+    for (auto s : inputs) {
+        cout << s << endl;
+        input.insert({ s, 1.0 });
+    }
+
+    cout << "\nOutputs: \n" << endl;
+
+    auto outputs = w->get_outputs();
+
+    std::unordered_map<std::string, double> output;
+    for (auto s : outputs) {
+        cout << s << endl;
+        output.insert({s, 1});
+    }
+
+    auto m = unique_ptr<c3_canopy>(dynamic_cast<c3_canopy*>((w->createModule(input, output)).get()));
+
+    inputs = m->get_inputs();
+
+    cout << "Inputs Again!!!: " << endl;
+
     for (auto s : inputs) {
         cout << s << endl;
     }
 
-    cout << "Outputs: " << endl;
+    cout << "\ngot here\n" << endl;
 
-    auto outputs = w->get_outputs();
-
-    for (auto s : outputs) {
-        cout << s << endl;
-    }
-
-    //auto m = w->createModule(inputs, outputs);
-
-    
 }
