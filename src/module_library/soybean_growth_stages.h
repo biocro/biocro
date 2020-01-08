dbdd5880677bddec54df7a9f865cc81c559a068f
Megan L. Matthews <53279181+leighmatth@users.noreply.github.com>
2020-01-08 13:55:17 -0600

    Update description in soybean_growth_stages.h

--- src/module_library/soybean_growth_stages.h
+++ src/module_library/soybean_growth_stages.h
@@ -6,13 +6,20 @@
 #include "../modules.h"
 
 /**
- * \brief
+ * \brief This module should be used in conjunction with the soybean_growth_stages_calculator module. The soybean_development_rate_per_hour calculated in that module is used in this module to update the development index (DVI) describing soybean growth. The DVI can be used to determine how carbon is partitioned between the different plant parts and when senescence starts (see partitioning_coefficient_logistic and senescence_coefficient_logistic modules).
+ *
+ * We define the DVI similarly to how its defined in Osborne et al., 2015, with:
+ *  -1 <= DVI < 0  :  period between sowing and soybean emergence
+ *  0 <= DVI < 1 : emergence to beginning of flowering (R1)
+ *  1 <= DVI < 2 : the reproductive stages flowering (R1) to maturity (R7)
+ *
+ * The DVI is allowed to continue accumulating above 2, indicating the crop has reached maturity before the final given timepoint.
+ *
+ * For more information on how the DVI and soybean_development_rate_per_hour are calculated, see soybean_growth_stages_calculator.h.
  *
  *  References:
- *  [1] Setiyono, T.D. et al. 2007. “Understanding and Modeling the Effect of Temperature and Daylength on Soybean Phenology under High-Yield Conditions.” Field Crops Research 100(2–3): 257–71.
- *  [2] Osborne, T. et al. 2015. “JULES-Crop: A Parametrisation of Crops in the Joint UK Land Environment Simulator.” Geoscientific Model Development 8(4): 1139–55.
-
-
+ *  Osborne, T. et al. 2015. “JULES-Crop: A Parametrisation of Crops in the Joint UK Land Environment Simulator.” Geoscientific Model Development 8(4): 1139–55. https://doi.org/10.5194/gmd-8-1139-2015
+ *
  */
 
 class soybean_growth_stages : public DerivModule
