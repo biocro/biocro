d27008755b4a8c823e5c6f23d8bf18e421c48d91
eloch216 <48919455+eloch216@users.noreply.github.com>
2020-02-26 12:47:23 -0600

    Removed commented code from R_Gro.cpp and applied clang-format

--- src/R_Gro.cpp
+++ src/R_Gro.cpp
@@ -11,21 +11,22 @@
 #include "solver_library/SystemSolverFactory.hpp"
 
 using std::string;
-using std::vector;
 using std::unique_ptr;
+using std::vector;
 
 extern "C" {
 
-SEXP R_Gro_solver(SEXP initial_state,
-                  SEXP parameters,
-                  SEXP varying_parameters,
-                  SEXP steady_state_module_names,
-                  SEXP derivative_module_names,
-                  SEXP solver_type,
-                  SEXP solver_output_step_size,
-                  SEXP solver_adaptive_error_tol,
-                  SEXP solver_adaptive_max_steps,
-                  SEXP verbose)
+SEXP R_Gro_solver(
+    SEXP initial_state,
+    SEXP parameters,
+    SEXP varying_parameters,
+    SEXP steady_state_module_names,
+    SEXP derivative_module_names,
+    SEXP solver_type,
+    SEXP solver_output_step_size,
+    SEXP solver_adaptive_error_tol,
+    SEXP solver_adaptive_max_steps,
+    SEXP verbose)
 {
     try {
         state_map s = map_from_list(initial_state);
@@ -43,7 +44,7 @@ SEXP R_Gro_solver(SEXP initial_state,
         string solver_type_string = CHAR(STRING_ELT(solver_type, 0));
         double output_step_size = REAL(solver_output_step_size)[0];
         double adaptive_error_tol = REAL(solver_adaptive_error_tol)[0];
-        int adaptive_max_steps = (int) REAL(solver_adaptive_max_steps)[0];
+        int adaptive_max_steps = (int)REAL(solver_adaptive_max_steps)[0];
 
         biocro_simulation gro(s, ip, vp, ss_names, deriv_names,
                               solver_type_string, output_step_size,
@@ -55,22 +56,21 @@ SEXP R_Gro_solver(SEXP initial_state,
         }
 
         return list_from_map(result);
-    }
-    catch (std::exception const& e) {
+    } catch (std::exception const& e) {
         Rf_error(string(string("Caught exception in R_Gro_solver: ") + e.what()).c_str());
-    }
-    catch (...) {
+    } catch (...) {
         Rf_error("Caught unhandled exception in R_Gro_solver.");
     }
 }
 
-SEXP R_Gro_deriv(SEXP state,
-                 SEXP time,
-                 SEXP parameters,
-                 SEXP varying_parameters,
-                 SEXP steady_state_module_names,
-                 SEXP derivative_module_names,
-                 SEXP verbose)
+SEXP R_Gro_deriv(
+    SEXP state,
+    SEXP time,
+    SEXP parameters,
+    SEXP varying_parameters,
+    SEXP steady_state_module_names,
+    SEXP derivative_module_names,
+    SEXP verbose)
 {
     try {
         // Convert the inputs into the proper format
@@ -117,10 +117,11 @@ SEXP R_Gro_deriv(SEXP state,
     }
 }
 
-SEXP R_Gro_ode(SEXP state,
-               SEXP steady_state_module_names,
-               SEXP derivative_module_names,
-               SEXP verbose)
+SEXP R_Gro_ode(
+    SEXP state,
+    SEXP steady_state_module_names,
+    SEXP derivative_module_names,
+    SEXP verbose)
 {
     try {
         // Get the input state
@@ -397,11 +398,12 @@ SEXP R_test_standalone_ss(SEXP module_name_input, SEXP input_parameters, SEXP ve
     }
 }
 
-SEXP R_test_system(SEXP initial_state,
-                   SEXP parameters,
-                   SEXP varying_parameters,
-                   SEXP steady_state_module_names,
-                   SEXP derivative_module_names)
+SEXP R_test_system(
+    SEXP initial_state,
+    SEXP parameters,
+    SEXP varying_parameters,
+    SEXP steady_state_module_names,
+    SEXP derivative_module_names)
 {
     try {
         state_map s = map_from_list(initial_state);
@@ -454,172 +456,5 @@ SEXP R_get_all_solvers()
         Rf_error("Caught unhandled exception in R_get_all_solvers.");
     }
 }
-
-/*
-SEXP R_Gro(SEXP initial_state,
-        SEXP invariate_parameters,
-        SEXP varying_parameters,
-        SEXP canopy_photosynthesis_module,
-        SEXP soil_water_module,
-        SEXP growth_module,
-        SEXP senescence_module,
-        SEXP stomata_water_stress_module,
-        SEXP leaf_water_stress_module)
-{
-    try {
-
-        state_map s = map_from_list(initial_state);
-        state_map ip = map_from_list(invariate_parameters);
-        state_vector_map vp = map_vector_from_list(varying_parameters);
-
-        if (vp.begin()->second.size() == 0) {
-            return R_NilValue;
-        }
-
-        vector<unique_ptr<IModule>> steady_state_modules;
-        vector<unique_ptr<IModule>> derivative_modules;
-
-        steady_state_modules.push_back(module_factory("soil_type_selector"));
-        steady_state_modules.push_back(module_factory(CHAR(STRING_ELT(stomata_water_stress_module, 0))));
-        steady_state_modules.push_back(module_factory(CHAR(STRING_ELT(leaf_water_stress_module, 0))));
-        steady_state_modules.push_back(module_factory("parameter_calculator"));
-        steady_state_modules.push_back(module_factory("partitioning_coefficient_selector"));
-        steady_state_modules.push_back(module_factory("soil_evaporation"));
-        steady_state_modules.push_back(module_factory(CHAR(STRING_ELT(canopy_photosynthesis_module, 0))));
-
-        derivative_modules.push_back(module_factory(CHAR(STRING_ELT(senescence_module, 0))));
-        derivative_modules.push_back(module_factory(CHAR(STRING_ELT(growth_module, 0))));
-        derivative_modules.push_back(module_factory("thermal_time_accumulator"));
-        derivative_modules.push_back(module_factory(CHAR(STRING_ELT(soil_water_module, 0))));
-
-        vector<string> required_state = {"iSp", "doy", "Leaf",
-            "LeafN_0", "vmax_n_intercept", "vmax1", "alphab1",
-            "alpha1", "TTc", "temp", "tbase", "timestep",
-            "mrc1", "seneLeaf", "Stem", "seneStem",
-            "mrc2", "Root", "seneRoot", "Rhizome", "seneRhizome", "kln", "growth_respiration_fraction"};
-
-        state_map all_state = combine_state(combine_state(ip, at(vp, 0)), s);
-
-        vector<string> missing_state;
-        for (auto it = required_state.begin(); it != required_state.end(); ++it) {
-            if (all_state.find(*it) == all_state.end()) {
-                missing_state.push_back(*it);
-            }
-        }
-
-        if (!missing_state.empty()) {
-            std::ostringstream message;
-            message << "The following state variables are required but are missing: ";
-            for(vector<string>::iterator it = missing_state.begin(); it != missing_state.end() - 1; ++it) {
-                message << *it << ", ";
-            }
-            message << missing_state.back() << ".";
-            error(message.str().c_str());
-        }
-
-        state_vector_map result = Gro(s, ip, vp, steady_state_modules, derivative_modules);
-        return (list_from_map(result));
-
-    } catch (std::exception const &e) {
-        error(string(string("Caught exception in R_Gro: ") + e.what()).c_str());
-    } catch (...) {
-        error("Caught unhandled exception in R_Gro.");
-    }
-}
-
-SEXP R_Gro_ode(SEXP state,
-        SEXP steady_state_modules_list,
-        SEXP derivative_modules_list)
-{
-    try {
-        state_map s = map_from_list(state);
-
-        if (s.size() == 0) {
-            return R_NilValue;
-        }
-
-        //ModuleFactory module_factory;
-
-        vector<string> steady_state_names_vector = make_vector(steady_state_modules_list);
-        vector<unique_ptr<IModule>> steady_state_modules;
-        steady_state_names_vector.reserve(steady_state_names_vector.size());
-        for (auto it = steady_state_names_vector.begin(); it != steady_state_names_vector.end(); ++it) {
-            steady_state_modules.push_back(module_factory(*it));
-        }
-
-        vector<string> derivative_names_vector = make_vector(derivative_modules_list);
-        vector<unique_ptr<IModule>> derivative_modules;
-        derivative_modules.reserve(derivative_names_vector.size());
-        for (auto it = derivative_names_vector.begin(); it != derivative_names_vector.end(); ++it) {
-            derivative_modules.push_back(module_factory(*it));
-        }
-
-        state_map result = Gro(s, steady_state_modules, derivative_modules);
-        return list_from_map(result);
-
-    } catch (std::exception const &e) {
-        error(string(string("Caught exception in R_Gro_ode: ") + e.what()).c_str());
-    } catch (...) {
-        error("Caught unhandled exception in R_Gro_ode.");
-    }
-}
-
-SEXP R_run_modules(SEXP state,
-        SEXP modules_list)
-{
-    try {
-        state_map s = map_from_list(state);
-
-        if (s.size() == 0) {
-            return R_NilValue;
-        }
-
-        //ModuleFactory module_factory;
-
-        vector<string> names_vector = make_vector(modules_list);
-        vector<unique_ptr<IModule>> modules;
-        names_vector.reserve(names_vector.size());
-        for (auto it = names_vector.begin(); it != names_vector.end(); ++it) {
-            modules.push_back(module_factory(*it));
-        }
-
-        for (auto it = modules.begin(); it != modules.end(); ++it) {
-            state_map temp = (*it)->run(s);
-            s.insert(temp.begin(), temp.end());
-        }
-
-        return list_from_map(s);
-
-    } catch (std::exception const &e) {
-        error(string(string("Caught exception in R_run_module: ") + e.what()).c_str());
-    } catch (...) {
-        error("Caught unhandled exception in R_run_module.");
-    }
-}
-
-SEXP R_get_module_requirements(SEXP modules_list)
-{
-    try {
-        vector<string> names_vector = make_vector(modules_list);
-        vector<unique_ptr<IModule>> modules;
-        for (auto it = names_vector.begin(); it != names_vector.end(); ++it) {
-            modules.push_back(module_factory(*it));
-        }
-
-        vector<string> parameters;
-        for (auto it = modules.begin(); it != modules.end(); ++it) {
-            std::vector<string> temp = (*it)->list_required_state();
-            parameters.insert(parameters.end(), temp.begin(), temp.end());
-        }
-
-        return r_string_vector_from_vector(parameters);
-
-    } catch (std::exception const &e) {
-        error(string(string("Caught exception in R_run_module: ") + e.what()).c_str());
-    } catch (...) {
-        error("Caught unhandled exception in R_run_module.");
-    }
-}
-*/
 
 }  // extern "C"
