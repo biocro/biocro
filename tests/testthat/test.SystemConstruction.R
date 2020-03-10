9031ea39dada327da58bef706836c0c513871227
eloch216 <48919455+eloch216@users.noreply.github.com>
2020-03-10 14:29:56 -0500

    Added a test for non-existent module names

--- tests/testthat/test.SystemConstruction.R
+++ tests/testthat/test.SystemConstruction.R
@@ -5,7 +5,31 @@ context("Test error conditions during system validation and construction")
 MAX_INDEX <- 100
 SILENT <- TRUE
 
-# Criterion 1
+test_that("All modules must exist", {
+	# Set up some bad inputs
+	
+	initial_state <- list(
+		unused_state_variable = 0
+	)
+	
+	parameters <- list(
+		unused_parameter = 0
+	)
+	
+	varying_parameters <- list(
+		unused_varying_parameter=rep(0, MAX_INDEX)
+	)
+	
+	steady_state_module_names <- c("module_that_does_not_exist")
+	
+	derivative_module_names <- c()
+	
+	# This should throw an error
+	
+	expect_error(validate_system_inputs(initial_state, parameters, varying_parameters, steady_state_module_names, derivative_module_names, silent=SILENT))
+	
+})
+
 test_that("Duplicated quantities produce an error during validation", {
 	# Set up some bad inputs for a harmonic oscillator
 	
@@ -36,7 +60,6 @@ test_that("Duplicated quantities produce an error during validation", {
 	
 })
 
-# Criterion 2
 test_that("Missing inputs produce an error during validation", {
 	# Set up some bad inputs for a harmonic oscillator
 	
@@ -66,7 +89,6 @@ test_that("Missing inputs produce an error during validation", {
 	
 })
 
-# Criterion 3
 test_that("Derivative modules only supply derivatives for quantities in the initial state", {
 	# Set up some bad inputs for a harmonic oscillator
 	
@@ -96,7 +118,6 @@ test_that("Derivative modules only supply derivatives for quantities in the init
 	
 })
 
-# Criterion 4
 test_that("Steady state modules must be supplied in the correct order", {
 	# Set up some bad inputs
 	
