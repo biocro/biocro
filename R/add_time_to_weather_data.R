e9d77a2cf82774a985d2859f09b0ea99d396eba9
Scott Rohde <srohde@igb.illinois.edu>
2020-04-27 12:33:58 -0500

    Fixes tests by changing the way add_time_to_weather_data() works.

--- R/add_time_to_weather_data.R
+++ R/add_time_to_weather_data.R
@@ -1,5 +1,34 @@
-add_time_to_weather_data <- function(weather_data)
+#' Add a doy_dbl component to input
+#'
+#' @description Ensure, if possible, that input data that varies over
+#'     time has a "doy_dbl" component.
+#'
+#' @param varying_parameters A list or dataframe representing known
+#'     system parameters that vary over time, such as weather data.
+#'
+#' @return If varying_parameters has no doy_dbl component, then one is
+#'     added, provided it _does_ have components for doy and
+#'     hour. (The doy_dbl values will be equal to the doy values plus
+#'     the fractional portion of a day represented by the hour
+#'     values.)  Otherwise varying_parameters is returned as is.
+#'
+#' @note Preconditions: If varying_parameters is a list, the values
+#'     should be vectors of equal length.  Moreover, if it already
+#'     contains a doy_dbl component, then it shouldn't contain either
+#'     a doy or an hour component unless it contains both of them and
+#'     the values are mutually consistent.  Furthermore, the time
+#'     represented by (doy, hour), if given, or by doy_dbl, if given,
+#'     should increase as the vector or row index increases.
+#' @md
+add_time_to_weather_data <- function(varying_parameters)
 {
-	weather_data$doy_dbl <- weather_data$doy + weather_data$hour / 24.0
-	return(weather_data)
+    if ("doy" %in% names(varying_parameters) &&
+        "hour" %in% names(varying_parameters) &&
+        !"doy_dbl" %in% names(varying_parameters))
+    {
+        day_fraction <- varying_parameters$hour / 24.0
+        varying_parameters$doy_dbl <- varying_parameters$doy + day_fraction
+    }
+
+    varying_parameters
 }
--- R/system.R
+++ R/system.R
@@ -1,19 +1,17 @@
 validate_system_inputs <- function(initial_state, parameters, varying_parameters, steady_state_module_names, derivative_module_names, silent = FALSE)
-{	
-	# If the varying parameter input doesn't have a doy_dbl column, add one
-	if (!"doy_dbl" %in% colnames(varying_parameters)) {
-		varying_parameters <- add_time_to_weather_data(varying_parameters)
-	}
+{
+    # If the varying parameter input doesn't have a doy_dbl column, add one
+    varying_parameters <- add_time_to_weather_data(varying_parameters)
 
-	# C++ requires that all the variables have type `double`
-	initial_state = lapply(initial_state, as.numeric)
-	parameters = lapply(parameters, as.numeric)
-	varying_parameters = lapply(varying_parameters, as.numeric)
-	
-	# Make sure silent is a logical variable
-	silent = lapply(silent, as.logical)
-	
-	# Run the C++ code
-	result = .Call(R_validate_system_inputs, initial_state, parameters, varying_parameters, steady_state_module_names, derivative_module_names, silent)
-	return(result)
+    # C++ requires that all the variables have type `double`
+    initial_state = lapply(initial_state, as.numeric)
+    parameters = lapply(parameters, as.numeric)
+    varying_parameters = lapply(varying_parameters, as.numeric)
+
+    # Make sure silent is a logical variable
+    silent = lapply(silent, as.logical)
+
+    # Run the C++ code
+    result = .Call(R_validate_system_inputs, initial_state, parameters, varying_parameters, steady_state_module_names, derivative_module_names, silent)
+    return(result)
 }
