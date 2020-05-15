937a7c4e931a2982976b7e67695467a3487c55e7
eloch216 <48919455+eloch216@users.noreply.github.com>
2020-05-15 15:58:07 -0500

    Update constants.h

--- src/constants.h
+++ src/constants.h
@@ -3,19 +3,36 @@
 
 #include <boost/math/constants/constants.hpp>
 
-namespace math_constants {
-    using boost::math::double_constants::pi;
-    using boost::math::double_constants::e;
-}
+namespace math_constants
+{
+using boost::math::double_constants::e;
+using boost::math::double_constants::pi;
+}  // namespace math_constants
 
-namespace calculation_constants {
-    const double eps_deriv = 1e-11;
-}
+namespace calculation_constants
+{
+const double eps_deriv = 1e-11;
+}  // namespace calculation_constants
 
-namespace physical_constants {
-    const double stefan_boltzmann = 5.67e-8;        // W / m^2 / K^4
-    const double celsius_to_kelvin = 273.15;        // deg. C or deg. K
-    const double pressure_at_sea_level = 101325;    // Pa
-}
+namespace physical_constants
+{
+/**
+ * These physical constants are truly constant everywhere
+ * in the universe in all possible conditions, as far as
+ * anybody knows.
+ */
+const double stefan_boltzmann = 5.670374419e-8;      // W / m^2 / K^4
+const double celsius_to_kelvin = 273.15;             // deg. C or deg. K
+const double ideal_gas_constant = 8.31446261815324;  // J / K / mol
+const double molar_mass_of_water = 18.01528e-3;      // kg / mol
+
+/**
+ * These water properties vary with temperature
+ * and pressure, but the changes are not significant
+ * across the range of conditions that a plant
+ * might encounter.
+ */
+const double specific_heat_of_water = 1010;  // J / kg / K
+}  // namespace physical_constants
 
 #endif
