46b5036bdac0b387b962232238e277edee20e07d
Megan L. Matthews <53279181+leighmatth@users.noreply.github.com>
2020-09-11 09:51:24 -0500

    added absorptivity params to CanAC header line

--- src/module_library/BioCro.h
+++ src/module_library/BioCro.h
@@ -17,7 +17,9 @@ struct Can_Str CanAC(double LAI, int DOY, double hr, double solarR, double Temp,
 		     double Kparm, double beta, double Rd, double Catm, double b0, 
 		     double b1, double theta, double kd, double chil, double heightf,
 		     double leafN, double kpLN, double lnb0, double lnb1, int lnfun, double upperT,
-		     double lowerT, const struct nitroParms &nitroP, double leafwidth, int eteq, double StomataWS, int water_stress_approach);
+		     double lowerT, const struct nitroParms &nitroP, double leafwidth, int eteq,
+             double StomataWS, int water_stress_approach, double absorptivity_par,
+             double absorptivity_nir);
          
 struct Can_Str c3CanAC(double LAI, int DOY, double hr, double solarR, double Temp,
                        double RH, double WindSpeed, double lat, int nlayers, double Vmax,
