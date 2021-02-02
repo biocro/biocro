f048c87ee9e7caeba5ff9a7efc982046d874950b
eloch216 <48919455+eloch216@users.noreply.github.com>
2021-02-02 11:41:01 -0600

    Get the cassava example to run by supplying missing parameter values.
    New values are taken from soybean, so are unlikely to be realistic in most cases. It was also necessary to change the senescence module, since the original one (thermal_time_senescence) is incompatible with adaptive step size solvers, but the growth module (utilization) requires adaptive step size solvers.

--- data/manihot_esculenta_initial_state.r
+++ data/manihot_esculenta_initial_state.r
@@ -6,10 +6,9 @@ manihot_esculenta_initial_state = with(list(), {
     Leaf                     0.02
     Stem                     0.99
     Root                     1
-    Grain                    0
-    waterCont                0.32
+    Grain                    0.0001
+    soil_water_content       0.32
     LeafN                    2
-    TTc                      0
     LeafLitter               0
     RootLitter               0
     RhizomeLitter            0
@@ -18,11 +17,12 @@ manihot_esculenta_initial_state = with(list(), {
     stem_senescence_index    0
     root_senescence_index    0
     rhizome_senescence_index 0
-    substrate_pool_leaf      0
-    substrate_pool_grain     0
-    substrate_pool_stem      0
-    substrate_pool_root      0
-    substrate_pool_rhizome   0"
+    substrate_pool_leaf      0.03       # No substrate pool can be zero with the utilization growth module
+    substrate_pool_grain     0.00005    # No substrate pool can be zero with the utilization growth module
+    substrate_pool_stem      0.005      # No substrate pool can be zero with the utilization growth module
+    substrate_pool_root      0.005      # No substrate pool can be zero with the utilization growth module
+    substrate_pool_rhizome   0.00005    # No substrate pool can be zero with the utilization growth module
+    TTc                      0"
 
     data_frame = utils::read.table(textConnection(datalines), header=TRUE)
     values = as.list(data_frame$value)
