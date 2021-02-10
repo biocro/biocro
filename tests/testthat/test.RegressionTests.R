4d04da05947a4f0e60bcbf26df9f7f4f5a09eba1
eloch216 <48919455+eloch216@users.noreply.github.com>
2021-02-09 19:18:20 -0600

    Specify a set of quantities to ignore during the "regression test"

--- tests/testthat/test.RegressionTests.R
+++ tests/testthat/test.RegressionTests.R
@@ -40,14 +40,19 @@ test_plant_model <- function(test_info) {
         )
     })
 
-    # Remove the 'ncalls' column from consideration since its value may depend
-    # on operating system or other factors. Eventually, 'ncalls' might not be
-    # part of the simulation output. If it disppears, remind the developers to
-    # update this test.
-    if ('ncalls' %in% names(result)) {
-        result[['ncalls']] <- NULL
-    } else {
-        stop("The regression test reports that 'ncalls' is no longer included in the simulation result. Consider updating this test.")
+    # Some variables may need to be ignored, possibly because their values
+    # depend on the operating system or other factors that may change between
+    # simulation runs. Remove these from the results. If a variable is flagged
+    # to be ignored but is not in the simulation result, this could indicate
+    # that one of the default modules has been changed, and the list of ignored
+    # variables should probably be revisited, so warn the user.
+    for (variable in test_info[['ignored_variables']]) {
+        if (variable %in% names(result)) {
+            result[[variable]] <- NULL
+        } else {
+            msg <- paste("The regression test reports that '", variable, "' is no longer included in the ", test_info[['plant_name']], " simulation result. Did a default module change?", sep="")
+            warning(msg)
+        }
     }
 
     # Make sure some important variables are included in the output
@@ -88,24 +93,40 @@ test_plant_model <- function(test_info) {
 }
 
 # Make a helping function for specifying plant information
-specify <- function(plant_name, initial_state, parameters, modules, stored_result_file) {
+specify <- function(plant_name, initial_state, parameters, modules, stored_result_file, ignored_variables) {
     list(
         plant_name = plant_name,
         initial_state = initial_state,
         parameters = parameters,
         modules = modules,
-        stored_result_file = stored_result_file
+        stored_result_file = stored_result_file,
+        ignored_variables = ignored_variables
     )
 }
 
+# Define lists of species-specific variables to ignore
+glycine_max_ignore <- c(
+    "ncalls",
+    "transport_leaf_to_stem", "transport_stem_to_root", "transport_stem_to_grain", "transport_stem_to_rhizome",
+    "utilization_leaf", "utilization_stem", "utilization_root", "utilization_rhizome", "utilization_grain",
+    "mass_fraction_leaf", "mass_fraction_stem", "mass_fraction_root", "mass_fraction_rhizome", "mass_fraction_grain",
+    "kGrain_scale"
+)
+manihot_esculenta_ignore <- glycine_max_ignore
+
+miscanthus_x_giganteus_ignore <- c("ncalls")
+sorghum_ignore <- miscanthus_x_giganteus_ignore
+willow_ignore <- miscanthus_x_giganteus_ignore
+zea_mays_ignore <- miscanthus_x_giganteus_ignore
+
 # Define the plants to test
 plant_testing_info <- list(
-    specify("glycine_max",            glycine_max_initial_state,            glycine_max_parameters,            glycine_max_modules,            "../test_data/glycine_max_simulation.rda"),
-    specify("manihot_esculenta",      manihot_esculenta_initial_state,      manihot_esculenta_parameters,      manihot_esculenta_modules,      "../test_data/manihot_esculenta_simulation.rda"),
-    specify("miscanthus_x_giganteus", miscanthus_x_giganteus_initial_state, miscanthus_x_giganteus_parameters, miscanthus_x_giganteus_modules, "../test_data/miscanthus_x_giganteus_simulation.rda"),
-    specify("sorghum",                sorghum_initial_state,                sorghum_parameters,                sorghum_modules,                "../test_data/sorghum_simulation.rda"),
-    specify("willow",                 willow_initial_state,                 willow_parameters,                 willow_modules,                 "../test_data/willow_simulation.rda"),
-    specify("zea_mays",               zea_mays_initial_state,               zea_mays_parameters,               zea_mays_modules,               "../test_data/zea_mays_simulation.rda")
+    specify("glycine_max",            glycine_max_initial_state,            glycine_max_parameters,            glycine_max_modules,            "../test_data/glycine_max_simulation.rda",            glycine_max_ignore),
+    specify("manihot_esculenta",      manihot_esculenta_initial_state,      manihot_esculenta_parameters,      manihot_esculenta_modules,      "../test_data/manihot_esculenta_simulation.rda",      manihot_esculenta_ignore),
+    specify("miscanthus_x_giganteus", miscanthus_x_giganteus_initial_state, miscanthus_x_giganteus_parameters, miscanthus_x_giganteus_modules, "../test_data/miscanthus_x_giganteus_simulation.rda", miscanthus_x_giganteus_ignore),
+    specify("sorghum",                sorghum_initial_state,                sorghum_parameters,                sorghum_modules,                "../test_data/sorghum_simulation.rda",                sorghum_ignore),
+    specify("willow",                 willow_initial_state,                 willow_parameters,                 willow_modules,                 "../test_data/willow_simulation.rda",                 willow_ignore),
+    specify("zea_mays",               zea_mays_initial_state,               zea_mays_parameters,               zea_mays_modules,               "../test_data/zea_mays_simulation.rda",               zea_mays_ignore)
 )
 
 # Run all the tests
