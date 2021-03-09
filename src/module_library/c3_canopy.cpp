1093e5f5aeefa41d66b2acccca5abedd56cba8bb
eloch216 <48919455+eloch216@users.noreply.github.com>
2021-03-09 10:15:09 -0600

    Remove some unnecessary c-style casts

--- src/module_library/c3_canopy.cpp
+++ src/module_library/c3_canopy.cpp
@@ -48,17 +48,17 @@ std::vector<std::string> c3_canopy::get_outputs() {
 
 void c3_canopy::do_operation() const {
 	// Collect inputs and make calculations
-	int doy = int(*doy_dbl_ip);					// Round doy_dbl down to get the day of year
+	double doy = floor(*doy_dbl_ip);			// Round doy_dbl down to get the day of year
 	double hour = 24.0 * ((*doy_dbl_ip) - doy);	// Get the fractional part as the hour
 
 	// c3CanAC is located in c3CanAc.cpp
 	struct Can_Str can_result = c3CanAC(*lai_ip, doy, hour, *solar_ip, *temp_ip,
-            *rh_ip, *windspeed_ip, *lat_ip, (int)(*nlayers_ip), *vmax_ip,
+            *rh_ip, *windspeed_ip, *lat_ip, *nlayers_ip, *vmax_ip,
             *jmax_ip, *tpu_rate_max_ip, *Rd_ip, *Catm_ip, *O2_ip, *b0_ip,
             *b1_ip, *Gs_min_ip, *theta_ip, *kd_ip, *heightf_ip, *LeafN_ip,
-            *kpLN_ip, *lnb0_ip, *lnb1_ip, (int)(*lnfun_ip), *chil_ip,
+            *kpLN_ip, *lnb0_ip, *lnb1_ip, *lnfun_ip, *chil_ip,
             *StomataWS_ip, *specific_heat_of_air_ip, *atmospheric_pressure_ip,
-            *growth_respiration_fraction_ip, (int)(*water_stress_approach_ip),
+            *growth_respiration_fraction_ip, *water_stress_approach_ip,
             *electrons_per_carboxylation_ip, *electrons_per_oxygenation_ip);
 
 	// Update the output parameter list
