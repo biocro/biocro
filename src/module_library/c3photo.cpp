fed3b1b5c0fa2d35c3ab53a349ab02ee7391419f
Scott Rohde <srohde@illinois.edu>
2021-03-11 11:55:17 -0600

    Update c3photo.cpp
    Reformatted reference.

--- src/module_library/c3photo.cpp
+++ src/module_library/c3photo.cpp
@@ -43,7 +43,7 @@ struct c3_str c3photoC(double _Qp, double _Tleaf, double RH, double _Vcmax0, dou
     const quantity<flux> maximum_tpu_rate = _TPU_rate_max * 1e-6 * mole / square_meter / second;
 
 
-    /* From Bernacchi et al. 2003 Plant, Cell and Environment 26, 14191430 doi: 10.1046/j.0016- 8025.2003.01050.x */
+    /* From Bernacchi et al. (2003) Plant, Cell and Environment, 26, 1419-1430. doi:10.1046/j.0016-8025.2003.01050.x */
     /* Note: Values in Dubois and Bernacchi are incorrect. */
     const quantity<mole_fraction> Kc = 1e-6 * arrhenius_exponent(38.05, 79.43e3 * joule / mole, leaf_temperature);
     const quantity<mole_fraction> Ko = 1e-3 * arrhenius_exponent(20.30, 36.38e3 * joule / mole, leaf_temperature);
