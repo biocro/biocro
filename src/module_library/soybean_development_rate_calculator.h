b1b417cd4e19b3df0cf7a8ae3893b4a173a9b25a
Megan L. Matthews <53279181+leighmatth@users.noreply.github.com>
2021-03-24 13:58:26 -0500

    cleaned up formatting of soybean development rate module

--- src/module_library/soybean_development_rate_calculator.h
+++ src/module_library/soybean_development_rate_calculator.h
@@ -6,12 +6,17 @@
 #include <cmath>
 
 /**
- * \brief This module uses photothermal functions to determine the hourly development rate for soybean growth. 
+ * \brief This module uses photothermal functions to determine the hourly development rate for soybean
+ * growth.
  *
  * References:
- *  Setiyono, T.D. et al. 2007. “Understanding and Modeling the Effect of Temperature and Daylength on Soybean Phenology under High-Yield Conditions.” Field Crops Research 100(2–3): 257–71. https://doi.org/10.1016/j.fcr.2006.07.011
+ *  Setiyono, T.D. et al. 2007. “Understanding and Modeling the Effect of Temperature and Daylength on
+ *  Soybean Phenology under High-Yield Conditions.” Field Crops Research 100(2–3): 257–71.
+ *  https://doi.org/10.1016/j.fcr.2006.07.011
  *
- *  Osborne, T. et al. 2015. “JULES-Crop: A Parametrisation of Crops in the Joint UK Land Environment Simulator.” Geoscientific Model Development 8(4): 1139–55. https://doi.org/10.5194/gmd-8-1139-2015
+ *  Osborne, T. et al. 2015. “JULES-Crop: A Parametrisation of Crops in the Joint UK Land Environment
+ *  Simulator.” Geoscientific Model Development 8(4): 1139–55.
+ *  https://doi.org/10.5194/gmd-8-1139-2015
  */
 
 double photoFunc(double P, double Popt, double Pcrit);
@@ -111,43 +116,62 @@ void soybean_development_rate_calculator::do_operation() const {
     if (DVI >= -1 && DVI < 0) { // 1. Sowing to emergence
         
         double temp_diff = temp - Tbase_emr; // degrees C
-        double soybean_development_rate = temp_diff / TTemr_threshold; // day^-1
+        soybean_development_rate = temp_diff / TTemr_threshold; // day^-1
         
-    } else if (DVI >= 0 && DVI < 1) { // 2. Vegetative stages
+    } else if (DVI >= 0 && DVI < 1) {
+        // 2. Vegetative stages: Emergence to R1 (flowering)
         
-        if (DVI < 0.333) { // 2a. Emr - V0; r = Rmax * f(T)
+        if (DVI < 0.333) {
+            // 2a. Emergence - V0 (cotyledon stage); r = Rmax * f(T)
+            soybean_development_rate = Rmax_emrV0 * tempFunc(temp,
+                Tmin_emrV0, Topt_emrV0, Tmax_emrV0) / 3.0; // day^-1
             
-            double soybean_development_rate = Rmax_emrV0 * tempFunc(temp, Tmin_emrV0, Topt_emrV0, Tmax_emrV0) / 3.0; // day^-1
-            
-        } else if (DVI >= 0.333 && DVI < 0.667) { // 2b. V0 - R0; r = Rmax * f(P)
-            
-            const double Rmax_V0R0 = 0.0294 + 0.1561 / maturity_group; // day^-1; Setiyono et al., 2007, Fig. 12a
-            const double Popt_V0R0 = 12.759 - 0.388 * maturity_group - 0.058 * pow(maturity_group,2); // hours; Setiyono et al., 2007, Fig 4
-            const double Pcrit_V0R0 = 27.275 - 0.493 * maturity_group - 0.066 * pow(maturity_group,2); // hours; Setiyono et al., 2007, Fig 4
-            double day_length_civil = 1.072 * day_length; // hours; converts day length calculated based on solar elevation = -0.83 degrees to civil day length (based on solar elevation angle = -6 degrees)
-            double soybean_development_rate = Rmax_V0R0 * photoFunc(day_length_civil, Popt_V0R0, Pcrit_V0R0) / 3.0; // day^-1
-        } else { // 2c. R0 - R1; r = Rmax * f(T)
-            
-            const double Rmax_R0R1 = (0.2551 + 0.0965 * maturity_group) / (1 + 2.1107 * maturity_group); // day^-1; Setiyono et al., 2007, Fig 11a
-            double soybean_development_rate = Rmax_R0R1 * tempFunc(temp, Tmin_R0R1, Topt_R0R1, Tmax_R0R1) / 3.0; // day^-1
+        } else if (DVI >= 0.333 && DVI < 0.667) {
+            // 2b. V0 (cotyledon) - R0 (end of floral induction); r = Rmax * f(P)
+            const double Rmax_V0R0 = 0.0294 + 0.1561 / maturity_group; // day^-1;
+                // Setiyono et al., 2007, Fig. 12a
+            const double Popt_V0R0 = 12.759 - 0.388 * maturity_group -
+                0.058 * pow(maturity_group,2); // hours; Setiyono et al., 2007, Fig 4
+            const double Pcrit_V0R0 = 27.275 - 0.493 * maturity_group -
+                0.066 * pow(maturity_group,2); // hours; Setiyono et al., 2007, Fig 4
+            double day_length_civil = 1.072 * day_length; // hours; Converts
+                // day length calculated based on solar
+                // elevation = -0.83 degrees to civil day length (based on
+                // solar elevation angle = -6 degrees)
+            soybean_development_rate = Rmax_V0R0 *
+                photoFunc(day_length_civil, Popt_V0R0, Pcrit_V0R0) / 3.0; // day^-1
+        } else {
+            // 2c. R0 (end of floral induction) - R1 (flowering); r = Rmax * f(T)
+            const double Rmax_R0R1 = (0.2551 + 0.0965 * maturity_group) /
+                (1 + 2.1107 * maturity_group); // day^-1; Setiyono et al., 2007, Fig 11a
+            soybean_development_rate = Rmax_R0R1 *
+                tempFunc(temp, Tmin_R0R1, Topt_R0R1, Tmax_R0R1) / 3.0; // day^-1
         }
-    } else if (DVI >= 1) { // 3. Reproductive stages, R1 - R7; r = Rmax * f(T) * f(P)
+    } else if (DVI >= 1) {
+        // 3. Reproductive stages, R1 (flowering) - R7 (maturity);
+        // r = Rmax * f(T) * f(P)
+        const double Rmax_R1R7 = (0.0563 + 0.0228 * maturity_group) /
+            (1 + 1.9683 * maturity_group); // day^-1; Setiyono et al., 2007, Fig 11d
+        const double Popt_R1R7 = 10.6595 + 2.9706 / maturity_group; // hrs;
+            // Setiyono et al., 2007, Fig 12c
+        const double Pcrit_R1R7 = 16.1257 + 4.3143 / maturity_group; // hrs;
+            // Setiyono et al., 2007, Fig 12c
         
-        const double Rmax_R1R7 = (0.0563 + 0.0228 * maturity_group) / (1 + 1.9683 * maturity_group); // day^-1; Setiyono et al., 2007, Fig 11d
-        const double Popt_R1R7 = 10.6595 + 2.9706 / maturity_group; // hrs; Setiyono et al., 2007, Fig 12c
-        const double Pcrit_R1R7 = 16.1257 + 4.3143 / maturity_group; // hrs; Setiyono et al., 2007, Fig 12c
-        
-        double soybean_development_rate = Rmax_R1R7 * tempFunc(temp, Tmin_R1R7, Topt_R1R7, Tmax_R1R7) * photoFunc(day_length, Popt_R1R7, Pcrit_R1R7); // day^-1
+        soybean_development_rate = Rmax_R1R7 *
+            tempFunc(temp, Tmin_R1R7, Topt_R1R7, Tmax_R1R7) *
+            photoFunc(day_length, Popt_R1R7, Pcrit_R1R7); // day^-1
         
     } else {
-        // error, DVI out of bounds, this should never occur, but prevents warning messages when compiling biocro
-        double soybean_development_rate = 0; //
+        // error, DVI out of bounds, this should never occur,
+        // but prevents warning messages when compiling biocro
+        soybean_development_rate = 0; // day^-1
     }
     
     double development_rate_per_hour = soybean_development_rate / 24.0; // hr^-1
     
     // Update the output parameter list
-    update(development_rate_per_hour_op, (development_rate_per_hour > eps) ? development_rate_per_hour : 0);
+    update(development_rate_per_hour_op, (development_rate_per_hour > eps) ?
+           development_rate_per_hour : 0);
 }
 
 double tempFunc(double T, double Tmin, double Topt, double Tmax) {
