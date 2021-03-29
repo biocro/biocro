cf35afd19ff56e16d44107a1b1e3851fbb8ae33b
Megan L. Matthews <53279181+leighmatth@users.noreply.github.com>
2021-03-28 20:12:14 -0500

    minor edit to comments

--- src/module_library/soybean_development_rate_calculator.h
+++ src/module_library/soybean_development_rate_calculator.h
@@ -79,7 +79,7 @@ public:
     static string_vector get_outputs();
     
     private:
-    // Pointers to input parameters
+    // References to input parameters
     const double& maturity_group;
     const double& DVI;
     const double& day_length;
