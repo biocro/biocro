1a1c39f116d0a539545121b89dd21150dade6ec2
eloch216 <48919455+eloch216@users.noreply.github.com>
2021-04-01 17:49:31 -0500

    Consolidate partitioning growth derivative modules

--- src/module_library/module_wrapper_factory.cpp
+++ src/module_library/module_wrapper_factory.cpp
@@ -101,7 +101,6 @@
 #include "soybean_development_rate_calculator.h"
 #include "thermal_time_development_rate_calculator.h"
 #include "no_leaf_resp_neg_assim_partitioning_growth_calculator.h"
-#include "partitioning_growth_neg_assim_partitioned.h"
 #include "rasmussen_specific_heat.h"
 #include "buck_swvp.h"
 #include "rh_to_mole_fraction.h"
@@ -238,7 +237,6 @@ module_wrapper_factory::module_wrapper_creator_map module_wrapper_factory::modul
      {"golden_ratio_hyperbola",                                &create_wrapper<golden_ratio_hyperbola>},
      {"hyperbola_2d",                                          &create_wrapper<hyperbola_2d>},
      {"no_leaf_resp_neg_assim_partitioning_growth_calculator", &create_wrapper<no_leaf_resp_neg_assim_partitioning_growth_calculator>},
-     {"partitioning_growth_neg_assim_partitioned",             &create_wrapper<partitioning_growth_neg_assim_partitioned>},
      {"rasmussen_specific_heat",                               &create_wrapper<rasmussen_specific_heat>},
      {"buck_swvp",                                             &create_wrapper<buck_swvp>},
      {"rh_to_mole_fraction",                                   &create_wrapper<rh_to_mole_fraction>}
