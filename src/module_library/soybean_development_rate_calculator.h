24406a6e3ea932b20ce44d48de8ea78ff87d705f
Megan L. Matthews <53279181+leighmatth@users.noreply.github.com>
2021-04-02 18:42:39 -0500

    fixed formatting in soybean development rate module

--- src/module_library/soybean_development_rate_calculator.h
+++ src/module_library/soybean_development_rate_calculator.h
@@ -3,7 +3,7 @@
 
 #include "../modules.h"
 #include "../state_map.h"
-#include <cmath>
+#include <cmath> // for pow
 
 double photoFunc(double P, double Popt, double Pcrit);
 double tempFunc(double T, double Tmin, double Topt, double Tmax);
@@ -13,34 +13,39 @@ double tempFunc(double T, double Tmin, double Topt, double Tmax);
  *
  * @brief Determines hourly soybean developments rate  using photothermal functions.
  *
- * This module is designed to be used with the development_index module
+ * This module is designed to be used with the `development_index` module
  *
- * This module calculates soybean growth rate using photothermal functions across 5 different stages.
+ * This module calculates soybean growth rate using photothermal functions
+ * across 5 different stages.
  *
- * | Development index (DVI)                                    | Growth stages                                             |
- * | :----------------------------------------------------: | :-----------------------------------------------: |
- * | \f$-1\le\f$ DVI \f$ <0\f$                             | sowing \f$ \to\f$ VE (emergence)           |
- * | \f$0\le\f$ DVI \f$ <\frac{1}{3}\f$                | VE \f$ \to\f$ V0 (cotyledon)                     |
- * | \f$ \frac{1}{3}\le\f$ DVI \f$ <\frac{2}{3}\f$ | V0 \f$ \to\f$ R0 (end of floral indcution)  |
- * | \f$ \frac{2}{3}\le\f$ DVI \f$ <1\f$               | R0 \f$ \to\f$ R1 (flowering)                      |
- * | \f$1\le\f$ DVI                                                 | R1 \f$ \to\f$ R7 (maturity)                        |
+ * | Development index (DVI)          | Growth stages                          |
+ * | :------------------------------: | :------------------------------------: |
+ * | \f$  -1 \le \text{DVI} < 0   \f$ | sowing to VE (emergence)               |
+ * | \f$   0 \le \text{DVI} < 1/3 \f$ |     VE to V0 (cotyledon)               |
+ * | \f$ 1/3 \le \text{DVI} < 2/3 \f$ |     V0 to R0 (end of floral induction) |
+ * | \f$ 2/3 \le \text{DVI} < 1   \f$ |     R0 to R1 (flowering)               |
+ * | \f$   1 \le \text{DVI}       \f$ |     R1 to R7 (maturity)                |
  *
- * The growth rate (\f$ r \f$) is calculated using accumulated thermal time for the first stage from from
- * sowing to VE. For the remaining stages after VE, \f$ r \f$ is calculated using a subset of the photothermal
- * functions from the SOYDEV model: \f$r=R_{max}f_Tf_P\f$ (Setiyono et al. 2007).
+ * The growth rate (\f$ r \f$) is calculated using accumulated thermal time for
+ * the first stage from from sowing to VE. For the remaining stages after VE,
+ * \f$ r \f$ is calculated using a subset of the photothermal functions from the
+ *  SOYDEV model: \f$ r = R_{max} f_T f_P \f$ (Setiyono et al. 2007).
  *
- * See Matthews et al. for a more detailed description of the functions used in this module.
+ * See Matthews et al. for a more detailed description of the functions used in
+ * this module.
  *
  * References:
  *
- *  Matthews, M.L. et al. 2021. in preparation. "Soybean-BioCro: A semi-mechanistic model of soybean growth"
+ *  Matthews, M.L. et al. 2021. in preparation. "Soybean-BioCro: A
+ *  semi-mechanistic model of soybean growth"
  *
- *  [Setiyono, T.D. et al. 2007. “Understanding and Modeling the Effect of Temperature and Daylength on
- *  Soybean Phenology under High-Yield Conditions.” Field Crops Research 100(2–3): 257–71.]
+ *  [Setiyono, T.D. et al. 2007. “Understanding and Modeling the Effect of
+ *  Temperature and Daylength on Soybean Phenology under High-Yield Conditions.”
+ *  Field Crops Research 100(2–3): 257–71.]
  *  (https://doi.org/10.1016/j.fcr.2006.07.011)
  *
- *  [Osborne, T. et al. 2015. “JULES-Crop: A Parametrisation of Crops in the Joint UK Land Environment
- *  Simulator.” Geoscientific Model Development 8(4): 1139–55.]
+ *  [Osborne, T. et al. 2015. “JULES-Crop: A Parametrisation of Crops in the Joint
+ *  UK Land Environment Simulator.” Geoscientific Model Development 8(4): 1139–55.]
  *  (https://doi.org/10.5194/gmd-8-1139-2015)
  */
 class soybean_development_rate_calculator : public SteadyModule
@@ -136,58 +141,66 @@ void soybean_development_rate_calculator::do_operation() const
 {
     double soybean_development_rate; // day^-1
 
-    if (DVI >= -1 && DVI < 0) { // 1. Sowing to emergence
+
+    if (DVI < -1) {
+        // error, DVI out of bounds, this should never occur unless initial DVI
+        // state is less than -1.
+        soybean_development_rate = 0; // day^-1
+
+    } else if (DVI < 0) {
+        // 1. Sowing to emergence
 
         double temp_diff = temp - Tbase_emr; // degrees C
         soybean_development_rate = temp_diff / TTemr_threshold; // day^-1
 
-    } else if (DVI >= 0 && DVI < 1) {
-        // 2. Vegetative stages: Emergence to R1 (flowering)
+    } else if (DVI < 0.333) {
+        // 2a. Emergence - V0 (cotyledon stage); r = Rmax * f(T)
 
-        if (DVI < 0.333) {
-            // 2a. Emergence - V0 (cotyledon stage); r = Rmax * f(T)
-            soybean_development_rate = Rmax_emrV0 * tempFunc(temp,
-                Tmin_emrV0, Topt_emrV0, Tmax_emrV0) / 3.0; // day^-1
+        soybean_development_rate = Rmax_emrV0 * tempFunc(temp, Tmin_emrV0,
+                                   Topt_emrV0, Tmax_emrV0) / 3.0; // day^-1
+    } else if (DVI < 0.667) {
+        // 2b. V0 (cotyledon) - R0 (end of floral induction); r = Rmax * f(P)
 
-        } else if (DVI >= 0.333 && DVI < 0.667) {
-            // 2b. V0 (cotyledon) - R0 (end of floral induction); r = Rmax * f(P)
-            const double Rmax_V0R0 = 0.0294 + 0.1561 / maturity_group; // day^-1;
-                // Setiyono et al., 2007, Fig. 12a
-            const double Popt_V0R0 = 12.759 - 0.388 * maturity_group -
-                0.058 * pow(maturity_group,2); // hours; Setiyono et al., 2007, Fig 4
-            const double Pcrit_V0R0 = 27.275 - 0.493 * maturity_group -
-                0.066 * pow(maturity_group,2); // hours; Setiyono et al., 2007, Fig 4
-            double day_length_civil = 1.072 * day_length; // hours; Converts
-                // day length calculated based on solar
-                // elevation = -0.83 degrees to civil day length (based on
-                // solar elevation angle = -6 degrees)
-            soybean_development_rate = Rmax_V0R0 *
-                photoFunc(day_length_civil, Popt_V0R0, Pcrit_V0R0) / 3.0; // day^-1
-        } else {
-            // 2c. R0 (end of floral induction) - R1 (flowering); r = Rmax * f(T)
-            const double Rmax_R0R1 = (0.2551 + 0.0965 * maturity_group) /
-                (1 + 2.1107 * maturity_group); // day^-1; Setiyono et al., 2007, Fig 11a
-            soybean_development_rate = Rmax_R0R1 *
-                tempFunc(temp, Tmin_R0R1, Topt_R0R1, Tmax_R0R1) / 3.0; // day^-1
-        }
-    } else if (DVI >= 1) {
+        const double Rmax_V0R0 = 0.0294 + 0.1561 / maturity_group; // day^-1;
+                                // Setiyono et al., 2007, Fig. 12a
+
+        const double Popt_V0R0 = 12.759 - 0.388 * maturity_group -
+            0.058 * pow(maturity_group,2); // hours; Setiyono et al., 2007, Fig 4
+        const double Pcrit_V0R0 = 27.275 - 0.493 * maturity_group -
+            0.066 * pow(maturity_group,2); // hours; Setiyono et al., 2007, Fig 4
+
+        double day_length_civil = 1.072 * day_length; // hours;
+            // Converts day length calculated based on solar elevation = -0.83 degrees
+            // to civil day length (based on solar elevation angle = -6 degrees)
+
+        soybean_development_rate = Rmax_V0R0 *
+            photoFunc(day_length_civil, Popt_V0R0, Pcrit_V0R0) / 3.0; // day^-1
+
+    } else if (DVI < 1) {
+        // 2c. R0 (end of floral induction) - R1 (flowering); r = Rmax * f(T)
+
+        const double Rmax_R0R1 = (0.2551 + 0.0965 * maturity_group) /
+            (1 + 2.1107 * maturity_group); // day^-1; Setiyono et al., 2007, Fig 11a
+
+        soybean_development_rate = Rmax_R0R1 *
+            tempFunc(temp, Tmin_R0R1, Topt_R0R1, Tmax_R0R1) / 3.0; // day^-1
+
+    } else {
         // 3. Reproductive stages, R1 (flowering) - R7 (maturity);
         // r = Rmax * f(T) * f(P)
+
         const double Rmax_R1R7 = (0.0563 + 0.0228 * maturity_group) /
             (1 + 1.9683 * maturity_group); // day^-1; Setiyono et al., 2007, Fig 11d
+
         const double Popt_R1R7 = 10.6595 + 2.9706 / maturity_group; // hrs;
             // Setiyono et al., 2007, Fig 12c
+
         const double Pcrit_R1R7 = 16.1257 + 4.3143 / maturity_group; // hrs;
             // Setiyono et al., 2007, Fig 12c
 
         soybean_development_rate = Rmax_R1R7 *
             tempFunc(temp, Tmin_R1R7, Topt_R1R7, Tmax_R1R7) *
             photoFunc(day_length, Popt_R1R7, Pcrit_R1R7); // day^-1
-
-    } else {
-        // error, DVI out of bounds, this should never occur,
-        // but prevents warning messages when compiling biocro
-        soybean_development_rate = 0; // day^-1
     }
 
     double development_rate_per_hour = soybean_development_rate / 24.0; // hr^-1
@@ -201,10 +214,10 @@ double tempFunc(double T, double Tmin, double Topt, double Tmax)
     double fT;  // dimensionless
 
     if (T > Tmin && T < Tmax) {
-        double alpha = log(2.0) / log((Tmax - Tmin) / (Topt - Tmin));              // dimensionless
+        double alpha = log(2.0) / log((Tmax - Tmin) / (Topt - Tmin)); // dimensionless
         double fT_num_pt1 = 2.0 * pow(T - Tmin, alpha) * pow(Topt - Tmin, alpha);  // dimensionless
-        double fT_num_pt2 = pow(T - Tmin, 2.0 * alpha);                            // dimensionless
-        double fT_denom = pow(Topt - Tmin, 2.0 * alpha);                           // dimensionless
+        double fT_num_pt2 = pow(T - Tmin, 2.0 * alpha); // dimensionless
+        double fT_denom = pow(Topt - Tmin, 2.0 * alpha); // dimensionless
 
         fT = (fT_num_pt1 - fT_num_pt2) / fT_denom;  // dimensionless
     } else {
