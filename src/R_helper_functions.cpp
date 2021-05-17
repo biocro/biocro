3164b1464eeb42e0407b501a217d119bf78dce0f
eloch216 <48919455+eloch216@users.noreply.github.com>
2021-05-17 10:30:13 -0500

    Reformat R helper functions (apply `clang-format`, use aliases, split long lines)

--- src/R_helper_functions.cpp
+++ src/R_helper_functions.cpp
@@ -9,28 +9,43 @@ using std::vector;
 
 /*
  * !!!!! IMPORTANT NOTE !!!!!:
- * This is a C++ interface to the C library in R. C++ supports exceptions, but C does not.
- * R's C library requires you to explicitly PROTECT() and UNPROTECT() memory for garbage collection.
- * If care is not taken, and an exception is thrown after memory has been PROTECTed, but before it has been UNPROTECTed, UNPROTECT will not be called.
+ * This is a C++ interface to the C library in R. C++ supports exceptions, but C
+ * does not. R's C library requires you to explicitly PROTECT() and UNPROTECT()
+ * memory for garbage collection. If care is not taken, and an exception is
+ * thrown after memory has been PROTECTed, but before it has been UNPROTECTed,
+ * UNPROTECT will not be called.
  *
- * Every effort should be made to not use exception-throwing functions between PROTECT and UNPROTECT pairs.
- * If an exception-throwing function is used between pairs, a means of ensuring that UNPROTECT is called when exceptions are thrown is required.
+ * Every effort should be made to not use exception-throwing functions between
+ * PROTECT and UNPROTECT pairs. If an exception-throwing function is used
+ * between pairs, a means of ensuring that UNPROTECT is called when exceptions
+ * are thrown is required.
  *
- * The ScopeGuard pattern could be used which would store protect_count internally, and call UNPROTECT(protect_count) in the destructor.
- * It would be used as follows:
+ * The ScopeGuard pattern could be used which would store protect_count
+ * internally, and call UNPROTECT(protect_count) in the destructor. It would be
+ * used as follows:
  *
- * guard = ProtectGuard();  // ProtectGuard is an implementation of the ScopeGuard pattern.
+ * guard = ProtectGuard();
  * PROTECT(something);
- * guard.increment_protect_count(1);  //  Call this immediately after each set of PROTECT calls using the appropriate number of items to protect.
+ * guard.increment_protect_count(1);
  *
- * The ProtectGuard destructor will be called when the function exists, regardless whether exceptions were thrown.
+ * ProtectGuard is an implementation of the ScopeGuard pattern. Its
+ * increment_protect_count method should be be called immediately after each set
+ * of PROTECT calls using the appropriate number of items to process. The
+ * ProtectGuard destructor will be called when the function exists, regardless
+ * of whether exceptions were thrown.
  *
- * Reference:
- * For std::unordered_map and std::vector, begin(), end(), size() and operator[() are guaranteed to not throw exceptions.
- *
+ * For reference: the following methods are guaranteed to not throw exceptions:
+ * - std::unordered_map::begin()
+ * - std::unordered_map::end()
+ * - std::unordered_map::size()
+ * - std::unordered_map::operator[()
+ * - std::vector::begin()
+ * - std::vector::end()
+ * - std::vector::size()
+ * - std::vector::operator[()
  */
 
-state_map map_from_list(SEXP const &list)
+state_map map_from_list(SEXP const& list)
 {
     SEXP names = Rf_getAttrib(list, R_NamesSymbol);
     size_t n = Rf_length(list);
@@ -42,7 +57,7 @@ state_map map_from_list(SEXP const &list)
     return m;
 }
 
-state_vector_map map_vector_from_list(SEXP const &list)
+state_vector_map map_vector_from_list(SEXP const& list)
 {
     SEXP names = Rf_getAttrib(list, R_NamesSymbol);
     size_t n = Rf_length(list);
@@ -55,12 +70,14 @@ state_vector_map map_vector_from_list(SEXP const &list)
         for (size_t j = 0; j < p; ++j) {
             temporary.emplace_back(REAL(VECTOR_ELT(list, i))[j]);
         }
-        m.insert(state_vector_map::value_type(CHAR(STRING_ELT(names, i)), temporary));
+        m.insert(state_vector_map::value_type(
+            CHAR(STRING_ELT(names, i)), temporary));
     }
     return m;
 }
 
-vector<string> make_vector(SEXP const &r_string_vector) {
+vector<string> make_vector(SEXP const& r_string_vector)
+{
     vector<string> v;
     size_t n = Rf_length(r_string_vector);
     v.reserve(n);
@@ -70,7 +87,8 @@ vector<string> make_vector(SEXP const &r_string_vector) {
     return v;
 }
 
-SEXP r_string_vector_from_vector(vector<string> const &v) {
+SEXP r_string_vector_from_vector(vector<string> const& v)
+{
     auto n = v.size();
     SEXP r_string_vector = PROTECT(Rf_allocVector(STRSXP, n));
     for (size_t i = 0; i < n; ++i) {
@@ -80,10 +98,10 @@ SEXP r_string_vector_from_vector(vector<string> const &v) {
     return r_string_vector;
 }
 
-SEXP list_from_map(state_map const &m)
+SEXP list_from_map(state_map const& m)
 {
     auto n = m.size();
-    SEXP list =  PROTECT(Rf_allocVector(VECSXP, n));
+    SEXP list = PROTECT(Rf_allocVector(VECSXP, n));
     SEXP names = PROTECT(Rf_allocVector(STRSXP, n));
     size_t i = 0;
     for (auto it = m.begin(); it != m.end(); ++it, ++i) {
@@ -95,10 +113,10 @@ SEXP list_from_map(state_map const &m)
     return list;
 }
 
-SEXP list_from_map(state_vector_map const &m)
+SEXP list_from_map(state_vector_map const& m)
 {
     auto n = m.size();
-    SEXP list =  PROTECT(Rf_allocVector(VECSXP, n));
+    SEXP list = PROTECT(Rf_allocVector(VECSXP, n));
     SEXP names = PROTECT(Rf_allocVector(STRSXP, n));
     size_t i = 0;
     for (auto it = m.begin(); it != m.end(); ++it, ++i) {
@@ -106,10 +124,10 @@ SEXP list_from_map(state_vector_map const &m)
         auto second = it->second;
         auto p = second.size();
         SEXP values = PROTECT(Rf_allocVector(REALSXP, p));
-        double* vp = REAL(values);  // It's a litte faster if you get a pointer and reuse it.
+        double* vp = REAL(values);  // It's a litte faster to reuse a pointer
         for (auto vit = second.begin(); vit != second.end(); ++vit, ++j) {
             vp[j] = second[j];
-        } 
+        }
         SET_VECTOR_ELT(list, i, values);
         SET_STRING_ELT(names, i, Rf_mkChar(it->first.c_str()));
         UNPROTECT(1);
@@ -119,10 +137,10 @@ SEXP list_from_map(state_vector_map const &m)
     return list;
 }
 
-SEXP list_from_map(std::unordered_map<std::string, std::vector<std::string>> const &m)
+SEXP list_from_map(std::unordered_map<string, vector<string>> const& m)
 {
-	auto n = m.size();
-    SEXP list =  PROTECT(Rf_allocVector(VECSXP, n));
+    auto n = m.size();
+    SEXP list = PROTECT(Rf_allocVector(VECSXP, n));
     SEXP names = PROTECT(Rf_allocVector(STRSXP, n));
     size_t i = 0;
     for (auto it = m.begin(); it != m.end(); ++it, ++i) {
@@ -131,8 +149,8 @@ SEXP list_from_map(std::unordered_map<std::string, std::vector<std::string>> con
         auto p = second.size();
         SEXP values = PROTECT(Rf_allocVector(STRSXP, p));
         for (auto vit = second.begin(); vit != second.end(); ++vit, ++j) {
-			SET_STRING_ELT(values, j, Rf_mkChar((second[j]).c_str()));
-        } 
+            SET_STRING_ELT(values, j, Rf_mkChar((second[j]).c_str()));
+        }
         SET_VECTOR_ELT(list, i, values);
         SET_STRING_ELT(names, i, Rf_mkChar(it->first.c_str()));
         UNPROTECT(1);
@@ -142,12 +160,12 @@ SEXP list_from_map(std::unordered_map<std::string, std::vector<std::string>> con
     return list;
 }
 
-SEXP vector_from_map(state_map const &m)
+SEXP vector_from_map(state_map const& m)
 {
     auto n = m.size();
     SEXP vector = PROTECT(Rf_allocVector(REALSXP, n));
     SEXP names = PROTECT(Rf_allocVector(STRSXP, n));
-    double* vp = REAL(vector);  // It's a little faster if you get a pointer and reuse it.
+    double* vp = REAL(vector);  // It's a little faster to reuse a pointer
     size_t i = 0;
     for (auto it = m.begin(); it != m.end(); ++it, ++i) {
         vp[i] = it->second;
@@ -161,17 +179,19 @@ SEXP vector_from_map(state_map const &m)
 SEXP r_logical_from_boolean(bool b)
 {
     SEXP logical_output = PROTECT(Rf_allocVector(LGLSXP, 1));
-    LOGICAL(logical_output)[0] = b;
+    LOGICAL(logical_output)
+    [0] = b;
     UNPROTECT(1);
     return logical_output;
 }
 
-void output_map(state_map const &m) {
+void output_map(state_map const& m)
+{
     if (!m.empty()) {
         size_t i = 0;
         auto it = m.begin();
         Rprintf("The map contains the following items: ");
-        for(; std::next(it) != m.end(); ++it) {
+        for (; std::next(it) != m.end(); ++it) {
             Rprintf("%s, %0.04f; ", it->first.c_str(), it->second);
             if (++i % 5 == 0) Rprintf("\n");
         }
@@ -181,15 +201,21 @@ void output_map(state_map const &m) {
     }
 }
 
-void output_list(SEXP const &list) {
+void output_list(SEXP const& list)
+{
     size_t n = Rf_length(list);
     SEXP names = PROTECT(Rf_allocVector(STRSXP, n));
     names = Rf_getAttrib(list, R_NamesSymbol);
     Rprintf("The list contains the following items: ");
     for (size_t i = 0; i < n - 1; ++i) {
-        Rprintf("%s, %0.04f; ", CHAR(STRING_ELT(names, i)), REAL(VECTOR_ELT(list, i))[0]);
+        Rprintf(
+            "%s, %0.04f; ",
+            CHAR(STRING_ELT(names, i)),
+            REAL(VECTOR_ELT(list, i))[0]);
     }
-    Rprintf("%s, %0.04f.\n\n", CHAR(STRING_ELT(names, n - 1)), REAL(VECTOR_ELT(list, n - 1))[0]);
+    Rprintf(
+        "%s, %0.04f.\n\n",
+        CHAR(STRING_ELT(names, n - 1)),
+        REAL(VECTOR_ELT(list, n - 1))[0]);
     UNPROTECT(1);
 }
-
