04acd3cfbf13f7fccc75596017045c38cdfcc7d2
eloch216 <48919455+eloch216@users.noreply.github.com>
2021-05-26 13:35:47 -0500

    Remove the `Gro` and `partial_gro` functions since they can be fully replicated with `Gro_solver` and `partial_gro_solver`

--- R/Gro.R
+++ R/Gro.R
@@ -1,138 +1,3 @@
-Gro <- function(initial_values, parameters, drivers, modules, solver=list(type='Gro', output_step_size=1.0, adaptive_rel_error_tol=1e-4, adaptive_abs_error_tole=1e-4, adaptive_max_steps=200), verbose = FALSE)
-{
-    # This function runs a full crop growth simulation, automatically choosing the Rosenbrock integration method when possible
-    #
-    # initial_values: a list of named parameters representing state variables
-    # parameters: a list of named parameters that don't change with time
-    # drivers: a dataframe of parameters defined at equally spaced time intervals
-    #  Note: the time interval should be specified as a parameter called "timestep" in the list of constant parameters
-    #  Note: the drivers must include "doy" and "hour"
-    # modules: a list of named strings specifying which modules to use for the simulation which must include:
-    #  'canopy'
-    #  'soil'
-    #  'growth'
-    #  'senescence'
-    #  'stomata_water_stress'
-    #  'leaf_water_stress'
-    # verbose: a logical variable indicating whether or not to print system startup information
-    #
-    # Example: running a sorghum simulation using weather data from 2005
-    #
-    #  result = Gro(sorghum_initial_values, sorghum_parameters, get_growing_season_climate(weather05), sorghum_modules, TRUE)
-    #  xyplot(Leaf + Stem + Root ~ TTc, data=result, type='l', auto=TRUE)
-    #
-    # The result is a data frame showing all time-dependent variables as they change throughout the growing season.
-    # When Gro is run in verbose mode (as in this example, where verbose = TRUE), information about the input and output parameters
-    # will be printed to the R console before the simulation runs.
-    #
-    # In the sorghum example, the simulation is performed using the fixed-step size Euler method for numerical integration. One of its modules (thermal_time_senescence)
-    # requires a history of all parameters, making it incompatible with any other integration method.
-    #
-    # Example 2: running a soybean simulation using weather data from 2005
-    #
-    #  result = Gro(glycine_max_initial_values, glycine_max_parameters, get_growing_season_climate(weather05), glycine_max_modules, TRUE)
-    #  xyplot(Leaf + Stem + Root + Grain ~ TTc, data=result, type='l', auto=TRUE)
-    #
-    # In the soybean simulation, Gro automatically detects that all modules are compatible with adapative step size integration methods. In this case, it uses
-    # ODEINT's implementation of an implicit Rosenbrock solver to run the simulation.
-
-
-    # Check to make sure the modules are properly defined
-    module_names = paste(c('canopy', 'soil', 'growth', 'senescence', 'stomata_water_stress', 'leaf_water_stress'), '_module_name', sep='')
-    if (any(unused_modules_ind <- !names(modules) %in% module_names)) {
-        message = 'The following modules were supplied, but are not used by this function: '
-        unused_modules = paste(names(modules)[unused_modules_ind], collaspe=', ')
-        stop(paste(message, unused_modules, ',', sep=''))
-    }
-
-    if (any(null_ind <- unlist(lapply(modules[module_names], is.null)))) {
-        message = 'The following modules names are NULL, but they must be defined: '
-        missing_modules = paste(module_names[null_ind], collapse=', ')
-        stop(paste(message, missing_modules, '.', sep=''))
-    }
-
-    # Get info about the growth module
-    if(modules$growth_module_name == "partitioning_growth") {
-        # The partitioning growth model implementation requires several modules:
-        #  Deriv: partitioning_growth
-        #  SS: partitioning_coefficient_selector and partitioning_growth_calculator
-        growth_module <- "partitioning_growth"
-        growth_module_support <- c("partitioning_coefficient_selector", "partitioning_growth_calculator")
-    }
-    else if(modules$growth_module_name == "no_leaf_resp_partitioning_growth") {
-        # The no leaf respiration partitioning growth model implementation requires several modules:
-        #  Deriv: partitioning_growth
-        #  SS: partitioning_coefficient_selector and no_leaf_resp_partitioning_growth_calculator
-        growth_module <- "partitioning_growth"
-        growth_module_support <- c("partitioning_coefficient_selector", "no_leaf_resp_partitioning_growth_calculator")
-    }
-    else if(modules$growth_module_name == "utilization_growth") {
-        # The utilization growth model implementation requires several modules:
-        #  Deriv: utilization_growth
-        #  SS: utilization_growth_calculator
-        growth_module <- "utilization_growth"
-        growth_module_support <- "utilization_growth_calculator"
-    }
-    else stop("The growth module you chose is not supported by Gro. Use one of the supported modules (partitioning_growth, no_leaf_resp_partitioning_growth, or utilization_growth) or use a different Gro variant such as Gro_auto.")
-
-    # Get info about the senescence module
-    if(modules$senescence_module_name == "thermal_time_senescence") {
-        # The thermal time senescence model implementation requires several modules:
-        #  Deriv: thermal_time_senescence
-        #  SS: partitioning_coefficient_selector
-        senescence_module <- "thermal_time_senescence"
-        senescence_module_support <- "partitioning_coefficient_selector"
-    }
-    else if(modules$senescence_module_name == "thermal_time_and_frost_senescence") {
-        # The thermal time and frost senescence model implementation requires several modules:
-        #  Deriv: thermal_time_and_frost_senescence
-        #  SS: partitioning_coefficient_selector
-        # We need to be careful since the partitioning_coefficient_selector may already have been defined
-        senescence_module <- "thermal_time_and_frost_senescence"
-        senescence_module_support <- "partitioning_coefficient_selector"
-    }
-    else if(modules$senescence_module_name == "utilization_senescence") {
-        # The utilization senescence model implementation requires several modules:
-        #  Deriv: utilization_senescence
-        #  SS: utilization_senescence_calculator
-        senescence_module <- "utilization_senescence"
-        senescence_module_support <- "utilization_senescence_calculator"
-    }
-    else stop("The senescence module you chose is not supported by Gro. Use one of the supported modules (thermal_time_senescence, thermal_time_and_frost_senescence, or utilization_senescence) or use a different Gro variant such as Gro_auto.")
-
-    # Build the module lists
-    # Note that the included modules depend on the choice of soil module, since one_layer_soil_profile requires a few supporting modules while two_layer_soil_profile does not
-    #  In fact, the leaf and stomata water stress module choices will be disregarded when two_layer_soil_profile is chosen. This module relies on a parameter (wsFun) to indicate
-    #   the appropriate methods to calculate LeafWS and StomataWS.
-    if(modules$soil_module_name == "one_layer_soil_profile") {
-        steady_state_module_names <- c("soil_type_selector", modules$stomata_water_stress_module_name, modules$leaf_water_stress_module_name, "parameter_calculator", "soil_evaporation", modules$canopy_module_name, growth_module_support, senescence_module_support)
-        derivative_module_names <- c(senescence_module, growth_module, "thermal_time_linear", modules$soil_module_name)
-    }
-    else if(modules$soil_module_name == "two_layer_soil_profile") {
-        steady_state_module_names <- c("soil_type_selector", modules$stomata_water_stress_module_name, modules$leaf_water_stress_module_name, "parameter_calculator", modules$canopy_module_name, growth_module_support, senescence_module_support)
-        derivative_module_names <- c(senescence_module, growth_module, "thermal_time_linear", modules$soil_module_name)
-    }
-    else stop("The soil profile module you chose is not supported by Gro. Use one of the supported modules (one_layer_soil_profile or two_layer_soil_profile) or use a different Gro variant such as Gro_auto.")
-
-    # Remove any duplicates in the lists
-    steady_state_module_names = unique(steady_state_module_names)
-    derivative_module_names = unique(derivative_module_names)
-
-    # Use Gro_solver to get the result
-    result = Gro_solver(
-        initial_values = initial_values,
-        parameters = parameters,
-        drivers = drivers,
-        steady_state_module_names = steady_state_module_names,
-        derivative_module_names = derivative_module_names,
-        solver = solver,
-        verbose = verbose
-    )
-
-    # Return the result
-    return(result)
-}
-
 Gro_solver <- function(initial_values, parameters, drivers, steady_state_module_names, derivative_module_names, solver=list(type='Gro', output_step_size=1.0, adaptive_rel_error_tol=1e-4, adaptive_abs_error_tol=1e-4, adaptive_max_steps=200), verbose=FALSE)
 {
     # This function runs a full crop growth simulation with a user-specified solver
@@ -261,19 +126,19 @@ Gro_solver <- function(initial_values, parameters, drivers, steady_state_module_
     return(result)
 }
 
-partial_gro <- function(initial_values, parameters, drivers, modules, arg_names, verbose = FALSE)
+partial_gro_solver <- function(initial_values, parameters, drivers, steady_state_module_names, derivative_module_names, arg_names, solver=list(type='Gro', output_step_size=1.0, adaptive_rel_error_tol=1e-4, adaptive_abs_error_tol=1e-4, adaptive_max_steps=200), verbose=FALSE)
 {
-    # Accepts the same parameters as Gro() with an additional 'arg_names' parameter, which is a vector of character variables.
-    # Returns a function that runs Gro() with all of the parameters, except 'arg_names
+    # Accepts the same parameters as Gro_solver() with an additional 'arg_names' parameter, which is a vector of character variables.
+    # Returns a function that runs Gro_solver() with all of the parameters, except 'arg_names'
     # set as default. The only parameter in the new function is the value of 'arg_names'.
     # This technique is called partial application, hence the name partial_gro.
     #
-    # initial_values: same as Gro()
-    # parameters: same as Gro()
-    # drivers: same as Gro()
-    # steady_state_module_names: same as Gro()
-    # derivative_module_names: same as Gro()
-    # verbose: same as Gro()
+    # initial_values: same as Gro_solver()
+    # parameters: same as Gro_solver()
+    # drivers: same as Gro_solver()
+    # steady_state_module_names: same as Gro_solver()
+    # derivative_module_names: same as Gro_solver()
+    # verbose: same as Gro_solver()
     # arg_names: vector of character variables. The names of the arguments that the new function accepts.
     #  Note: 'arg_names' must contain the names of parameters in 'initial_values', 'parameters', or 'drivers'.
     #
@@ -281,53 +146,17 @@ partial_gro <- function(initial_values, parameters, drivers, modules, arg_names,
     #
     # Example: varying the TTc values at which senescence starts for a sorghum simulation
     #
-    #  sorghum_ss_modules <- c("soil_type_selector", "stomata_water_stress_linear", "leaf_water_stress_exponential", "parameter_calculator", "partitioning_coefficient_selector", "soil_evaporation", "c4_canopy", "partitioning_growth_calculator")
-    #  sorghum_deriv_modules <- c("thermal_time_senescence", "partitioning_growth", "thermal_time_linear", "one_layer_soil_profile")
-    #  senescence_gro <- partial_gro(sorghum_initial_values, sorghum_parameters, get_growing_season_climate(weather05), sorghum_ss_modules, sorghum_deriv_modules, c('seneLeaf', 'seneStem', 'seneRoot', 'seneRhizome'), TRUE)
+    #  senescence_gro_solver <- partial_gro_solver(sorghum_initial_values, sorghum_parameters, get_growing_season_climate(weather05), sorghum_steady_state_modules, sorghum_derivative_modules, sorghum_solver, c('seneLeaf', 'seneStem', 'seneRoot', 'seneRhizome'), TRUE)
     #  result = senescence_gro(c(2000, 2000, 2000, 2000))
     #
     # Note that in this example, typing 'sorghum_parameters$seneStem' returns the original default value for seneStem: 3500.
-    # However, after running senescence_gro, the system startup messages (in verbose mode) indicate a value of 2000 for seneStem, as desired.
+    # However, after running senescence_gro_solver, the system startup messages (in verbose mode) indicate a value of 2000 for seneStem, as desired.
     # To remove system startup messages, change verbose to FALSE in the partial_gro command or omit it altogether to use the default value of FALSE, i.e.
     #
-    #  senescence_gro <- partial_gro(sorghum_initial_values, sorghum_parameters, get_growing_season_climate(weather05), sorghum_ss_modules, sorghum_deriv_modules, c('seneLeaf', 'seneStem', 'seneRoot', 'seneRhizome'), FALSE)
+    #  senescence_gro_solver <- partial_gro_solver(sorghum_initial_values, sorghum_parameters, get_growing_season_climate(weather05), sorghum_steady_state_modules, sorghum_derivative_modules, sorghum_solver, c('seneLeaf', 'seneStem', 'seneRoot', 'seneRhizome'), FALSE)
     #  OR
-    #  senescence_gro <- partial_gro(sorghum_initial_values, sorghum_parameters, get_growing_season_climate(weather05), sorghum_ss_modules, sorghum_deriv_modules, c('seneLeaf', 'seneStem', 'seneRoot', 'seneRhizome'))
+    #  senescence_gro_solver <- partial_gro_solver(sorghum_initial_values, sorghum_parameters, get_growing_season_climate(weather05), sorghum_steady_state_modules, sorghum_derivative_modules, sorghum_solver, c('seneLeaf', 'seneStem', 'seneRoot', 'seneRhizome'))
 
-    # Form the argument list to pass to Gro
-    arg_list = list(initial_values=initial_values, parameters=parameters, drivers=drivers, modules=modules, verbose=verbose)
-
-    # Make a data frame containing the names of all parameters in the initial_values, parameters, and drivers inputs
-    df = data.frame(control=character(), arg_name=character(), stringsAsFactors=FALSE)
-    for (i in seq_along(arg_list)) {
-        if (length(names(arg_list[[i]])) > 0) {
-            df = rbind(df, data.frame(control = names(arg_list)[i], arg_name=names(arg_list[[i]]), stringsAsFactors=FALSE))
-        }
-    }
-
-    # Find the locations of the parameters specified in arg_names and check for errors
-    controls = df[match(arg_names, df$arg_name), ]
-    if (any(is.na(controls))) {
-        missing = arg_names[which(is.na(controls$control))]
-        stop(paste('The following arguments in "arg_names" are not in any of the paramter lists:', paste(missing, collapse=', ')))
-    }
-
-    # Make a function that calls Gro with new values for the parameters specified in arg_names
-    function(x)
-    {
-        if (length(x) != length(arg_names)) stop("The length of x does not match the length of arguments when this function was defined.")
-        x = unlist(x)
-        temp_arg_list = arg_list
-        for (i in seq_along(arg_names)) {
-            c_row = controls[i, ]
-            temp_arg_list[[c_row$control]][[c_row$arg_name]] = x[i]
-        }
-        do.call(Gro, temp_arg_list)
-    }
-}
-
-partial_gro_solver <- function(initial_values, parameters, drivers, steady_state_module_names, derivative_module_names, arg_names, solver=list(type='Gro', output_step_size=1.0, adaptive_rel_error_tol=1e-4, adaptive_abs_error_tol=1e-4, adaptive_max_steps=200), verbose=FALSE)
-{
     arg_list = list(initial_values=initial_values, parameters=parameters, drivers=drivers, steady_state_module_names=steady_state_module_names, derivative_module_names=derivative_module_names, solver=solver, verbose=verbose)
 
     df = data.frame(control=character(), arg_name=character(), stringsAsFactors=FALSE)
