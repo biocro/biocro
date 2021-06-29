5f237d2d8e146376d2d62ebff4620826c880dc2c
eloch216 <48919455+eloch216@users.noreply.github.com>
2021-06-29 16:17:49 -0500

    Update `validate_system_inputs` to accept the same argument classes as `run_biocro`

--- R/system.R
+++ R/system.R
@@ -1,7 +1,65 @@
-validate_system_inputs <- function(initial_values, parameters, drivers, steady_state_module_names, derivative_module_names, silent = FALSE)
+# `validate_system_inputs` checks a set of parameters, drivers, modules, and
+# initial values to see if they can properly define a dynamical system and can
+# therefore be used as inputs to `run_biocro`.
+#
+# The return value from this function is a boolean indicating whether the inputs
+# can form a valid system.
+#
+# The `run_biocro` function performs all the same checks. However,
+# `validate_system_inputs` returns additional information, such as a list of all
+# quantities that are defined in the parameters, initial values, and drivers
+# that are not used as inputs to any modules.
+#
+# When silent is TRUE, no information will be printed to the R console.
+validate_system_inputs <- function(
+    initial_values = list(),
+    parameters = list(),
+    drivers,
+    steady_state_module_names = list(),
+    derivative_module_names = list(),
+    silent = FALSE
+)
 {
+    # Check to make sure the initial values are properly defined
+    if (!is.list(initial_values)) {
+        stop('"initial_values" must be a list')
+    }
+
+    if (length(initial_values) != length(unlist(initial_values))) {
+        item_lengths = unlist(lapply(initial_values, length))
+        error_message = sprintf("The following initial_values members have lengths other than 1, but all parameters must have a length of exactly 1: %s.\n", paste(names(item_lengths)[which(item_lengths > 1)], collapse=', '))
+        stop(error_message)
+    }
+
+    # Check to make sure the parameters are properly defined
+    if (!is.list(parameters)) {
+        stop('"parameters" must be a list')
+    }
+
+    if (length(parameters) != length(unlist(parameters))) {
+        item_lengths = unlist(lapply(parameters, length))
+        error_message = sprintf("The following parameters members have lengths other than 1, but all parameters must have a length of exactly 1: %s.\n", paste(names(item_lengths)[which(item_lengths > 1)], collapse=', '))
+        stop(error_message)
+    }
+
+    # Check to make sure the drivers are properly defined
+    if (!is.list(drivers)) {
+        stop('"drivers" must be a list')
+    }
+
     # If the drivers input doesn't have a time column, add one
     drivers <- add_time_to_weather_data(drivers)
+
+    # Check to make sure the module names are vectors or lists of strings
+    steady_state_module_names <- unlist(steady_state_module_names)
+    if (length(steady_state_module_names) > 0 & !is.character(steady_state_module_names)) {
+        stop('"steady_state_module_names" must be a vector or list of strings')
+    }
+
+    derivative_module_names <- unlist(derivative_module_names)
+    if (length(derivative_module_names) > 0 & !is.character(derivative_module_names)) {
+        stop('"derivative_module_names" must be a vector or list of strings')
+    }
 
     # C++ requires that all the variables have type `double`
     initial_values = lapply(initial_values, as.numeric)
@@ -12,6 +70,15 @@ validate_system_inputs <- function(initial_values, parameters, drivers, steady_s
     silent = lapply(silent, as.logical)
 
     # Run the C++ code
-    result = .Call(R_validate_system_inputs, initial_values, parameters, drivers, steady_state_module_names, derivative_module_names, silent)
+    result = .Call(
+        R_validate_system_inputs,
+        initial_values,
+        parameters,
+        drivers,
+        steady_state_module_names,
+        derivative_module_names,
+        silent
+    )
+
     return(result)
 }
