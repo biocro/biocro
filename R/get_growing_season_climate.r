95a620a69a286198cbfe45cf96951145984318fd
eloch216 <48919455+eloch216@users.noreply.github.com>
2022-04-19 14:00:24 -0500

    Document `get_growing_season_climate`

--- R/get_growing_season_climate.r
+++ R/get_growing_season_climate.r
@@ -1,18 +1,35 @@
-get_growing_season_climate = function(climate, threshold_temperature=0) {
-    doy_range = guess_growing_season_start_and_end_doy(climate, threshold_temperature)
+get_growing_season_climate <- function(climate, threshold_temperature=0) {
+    doy_range <-
+        guess_growing_season_start_and_end_doy(climate, threshold_temperature)
+
     return(climate[with(climate, doy >= doy_range[1] & doy <= doy_range[2]), ])
 }
 
-guess_growing_season_start_and_end_doy = function(climate, threshold_temperature) {
-    # Within the first half of the year, get the days on which the temperature was below 0.
+# This is a helper function for `get_growing_season_climate`.
+#
+# Within the first half of the year, get the days on which the temperature was
+# below 0. The first day of the season is the last freezing day, or day 90 at
+# the earliest.
+#
+# Within the second half of the year, get the days on which the temperature was
+# below 0. The last day of the season is the first freezing day, or day 330 at
+# the latest.
+#
+# This function returns the first and last days as a two-element numeric vector.
+guess_growing_season_start_and_end_doy <- function(
+    climate,
+    threshold_temperature
+)
+{
+    early_season_freezing_days <-
+        climate[with(climate, doy <= 183 & temp <= threshold_temperature), 'doy']
 
-    early_season_freezing_days = climate[with(climate, doy <= 183 & temp <= threshold_temperature), 'doy']
-    day1 = max(c(early_season_freezing_days, 90))  # The first day of the season is the last freezing day, or day 90 at the earliest.
+    day1 = max(c(early_season_freezing_days, 90))
 
-    # Within the second half of the year, get the days on which the temperature was below 0.
-    late_season_freezing_days = climate[with(climate, doy > 183 & temp <= threshold_temperature), 'doy']
-    dayn = min(c(late_season_freezing_days, 330))  # The last day of the season is the first freezing day, or day 330 at the latest.
+    late_season_freezing_days <-
+        climate[with(climate, doy > 183 & temp <= threshold_temperature), 'doy']
+
+    dayn = min(c(late_season_freezing_days, 330))
 
     return(c(day1=day1, dayn=dayn))
 }
-
