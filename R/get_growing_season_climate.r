d1029d73cbd0ec1cb22155cf08c6b7eee371dc34
eloch216 <48919455+eloch216@users.noreply.github.com>
2022-04-21 11:01:29 -0500

    Improve working in documentation

--- R/get_growing_season_climate.r
+++ R/get_growing_season_climate.r
@@ -1,4 +1,4 @@
-get_growing_season_climate <- function(climate, threshold_temperature=0) {
+get_growing_season_climate <- function(climate, threshold_temperature = 0) {
     doy_range <-
         guess_growing_season_start_and_end_doy(climate, threshold_temperature)
 
@@ -8,12 +8,12 @@ get_growing_season_climate <- function(climate, threshold_temperature=0) {
 # This is a helper function for `get_growing_season_climate`.
 #
 # Within the first half of the year, get the days on which the temperature was
-# below 0. The first day of the season is the last freezing day, or day 90 at
-# the earliest.
+# below (or equal to) the threshold temperature. The first day of the season is
+# the last day meeting that criterion, or day 90 at the earliest.
 #
 # Within the second half of the year, get the days on which the temperature was
-# below 0. The last day of the season is the first freezing day, or day 330 at
-# the latest.
+# below (or equal to) the threshold temperature. The last day of the season is
+# the first day meeting that criterion, or day 330 at the latest.
 #
 # This function returns the first and last days as a two-element numeric vector.
 guess_growing_season_start_and_end_doy <- function(
@@ -21,15 +21,15 @@ guess_growing_season_start_and_end_doy <- function(
     threshold_temperature
 )
 {
-    early_season_freezing_days <-
+    early_season_cold_days <-
         climate[with(climate, doy <= 183 & temp <= threshold_temperature), 'doy']
 
-    day1 = max(c(early_season_freezing_days, 90))
+    day1 = max(c(early_season_cold_days, 90))
 
-    late_season_freezing_days <-
+    late_season_cold_days <-
         climate[with(climate, doy > 183 & temp <= threshold_temperature), 'doy']
 
-    dayn = min(c(late_season_freezing_days, 330))
+    dayn = min(c(late_season_cold_days, 330))
 
-    return(c(day1=day1, dayn=dayn))
+    return(c(day1, dayn))
 }
