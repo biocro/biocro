ab7ebbf9f5f9f046ca63890428a9b7aab3d9d1b0
eloch216 <48919455+eloch216@users.noreply.github.com>
2022-07-14 06:46:19 -0500

    Make `run_pkgdown.R` more forgiving about case

--- documentation/script/run_pkgdown.R
+++ documentation/script/run_pkgdown.R
@@ -1,14 +1,14 @@
 #! /usr/bin/env Rscript
 
 if (!requireNamespace("pkgdown", quietly = TRUE)) {
-    cat("pkgdown must be installed to run this script.  Install now (~170M with dependencies)? (Y/n) ")
+    cat("pkgdown must be installed to run this script.  Install now (~170M with dependencies)? (y/n) ")
     while (TRUE) {
         ans <- readLines(con = file("stdin"), n = 1)
 
-        if (ans == "y" || ans == "") {
+        if (tolower(ans) == "y" || ans == "") {
             install.packages("pkgdown")
             break
-        } else if (ans == "n") {
+        } else if (tolower(ans) == "n") {
             stop("Stopping.  pkgdown must be installed to run this script.")
         } else {
             cat(sprintf('"%s" is not a valid answer.  Please answer "y" or "n": ', ans))
