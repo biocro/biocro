450b2f672f325efb2ca56c5d442f56bf70b55d40
eloch216 <48919455+eloch216@users.noreply.github.com>
2023-07-11 17:49:59 -0500

    Finish documentation - whoops!

--- src/module_library/lightME.cpp
+++ src/module_library/lightME.cpp
@@ -4,24 +4,23 @@
 using physical_constants::atmospheric_pressure_at_sea_level;
 
 /**
- *
- *  The return value from this function is a customized structure
- *  (`light_model`) with the following members:
- *  - `light_model.direct_irradiance_fraction`: The fraction of irradiance that
- *    is direct radiation (dimensionless)
- *  - `light_model.diffuse_irradiance_fraction`: The fraction of irradiance
- *    that is diffuse radiation (dimensionless)
- *
- *
  * @brief Calculates the "light macro environment"; in other words, the amount
  * of sunlight scattered out of the direct beam by the atmosphere.
  *
- * The result is expressed in two ways: as atmospheric transmittances (which
- * would be appropriate for calculating the intensity of direct and diffuse
- * light at the surface given a value for the solar constant) or as fractions
- * (which would be appropriate for calculating the intensity of direct and
- * diffuse light given an experimental measurement of light intensity at the
- * surface).
+ * The result is expressed in two ways:
+
+ * 1. As atmospheric transmittances, which are the ratios of direct light or
+ *    diffuse light at the Earth's surface to incident light at the upper
+ *    atmosphere. These would be appropriate for calculating the intensity of
+ *    direct and diffuse light at the surface given a value for the solar
+ *    constant. Note that these transmittances are different than the input
+ *    parameter `atmospheric_transmittance`, which represents the transmittance
+ *    of either type of light through a small volume of atmosphere.
+ *
+ * 2. As fractions of direct and diffuse light at the Earth's surface, which
+ *    would be appropriate for calculating the intensity of direct and
+ *    diffuse light given an experimental measurement of light intensity at the
+ *    surface.
  *
  * The basis for this function is given in chapter 11 of Norman and Campbell,
  *  _An Introduction to Environmental Biophysics_, 2nd edition.
@@ -33,9 +32,11 @@ using physical_constants::atmospheric_pressure_at_sea_level;
  *
  * @param [in] atmospheric_pressure Local atmospheric pressure in Pa
  *
- * @param [in] atmospheric_transmittance
+ * @param [in] atmospheric_transmittance Fraction of light transmitted through a
+               small volume of atmosphere (dimensionless)
  *
- * @param [in] atmospheric_scattering
+ * @param [in] atmospheric_scattering Atmospheric scattering factor
+ *             (dimensionless)
  *
  * @return A structure containing values of the transmittances and fractions of
  *         direct and diffuse light just above the canopy.
--- src/module_library/lightME.h
+++ src/module_library/lightME.h
@@ -1,6 +1,6 @@
 struct Light_model {
     double direct_transmittance;   //!< Atmospheric transmittance to direct radiation (dimensionless)
-    double diffuse_transmittance;  //!< Atmospheric transmittance to direct radiation (dimensionless)
+    double diffuse_transmittance;  //!< Atmospheric transmittance to diffuse radiation (dimensionless)
     double direct_fraction;        //!< Fraction of direct irradiance at the Earth's surface (dimensionless)
     double diffuse_fraction;       //!< Fraction of diffuse irradiance at the Earth's surface (dimensionless)
 };
