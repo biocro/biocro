ba55eca5f7758bac38ef37e17a4839ebdf3decdd
Scott Rohde <srohde@illinois.edu>
2023-09-15 15:09:34 -0500

    Added a comment to the tag-generation script.

--- script/generate_tags.sh
+++ script/generate_tags.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# This is a simple script for Emacs users that will generate a tags
+# table for all C++ src files under the "src" directory.
+
 pushd ../src &&
     rm -f TAGS &&
     find . \( -name "*.cpp" -o -name "*.h" \) -exec etags --language=c++ --append {} \; &&
