450c95685930f5406b8bac14abf2ff399db9c669
uphone87 <yufenghe87@gmail.com>
2024-01-15 09:51:37 -0600

    format comments

--- data/soybean.R
+++ data/soybean.R
@@ -116,7 +116,12 @@ soybean <- list(
 
         # incident_shortwave_from_ground_par module
         par_energy_fraction         = 0.5,
-        par_energy_content          = 0.219,       # W * s / micromole. Also J / micromole. Conversion from photon flux density to to irradiance for PAR. Equals 1/4.57. Plant Growth Chamber Handbook. CHAPTER 1 – RADIATION– John C. Sager and J. Craig McFarlane. Table 2, Pg 3 (https://www.controlledenvironments.org/wp-content/uploads/sites/6/2017/06/Ch01.pdf)
+        par_energy_content          = 0.219,       # W * s / micromole. Also J / micromole. Conversion
+                                                   # from photon flux density to to irradiance for
+                                                   # PAR. Equals 1/4.57. Plant Growth Chamber
+                                                   # Handbook. CHAPTER 1 – RADIATION– John C. Sager and
+                                                   # J. Craig McFarlane. Table 2, Pg 3
+                                                   # (https://www.controlledenvironments.org/wp-content/uploads/sites/6/2017/06/Ch01.pdf)
 
         # ten_layer_canopy_properties module
         absorptivity_par            = 0.8,         # Campbell and Norman, An Introduction to Environmental Biophysics, 2nd Edition
