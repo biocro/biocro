f2153f37c1f934ed669c313666ef8dae8f803520
Scott Rohde <srohde@illinois.edu>
2024-01-16 11:59:56 -0600

    Factored out some common parameter values into a separate file.

---
+++ data/common_parameters.R
@@ -0,0 +1,6 @@
+common_parameters <- list(
+    absorptivity_par            = 0.8,
+    atmospheric_transmittance   = 0.6, # Campbell and Norman, An Introduction to Environmental Biophysics, 2nd Edition, Pg 173
+    par_energy_content          = 0.219,  # W * s / micromole. Also J / micromole. Conversion from photon flux density to to irradiance for PAR. Equals 1/4.57. Plant Growth Chamber Handbook. CHAPTER 1 – RADIATION– John C. Sager and J. Craig McFarlane. Table 2, Pg 3 (https://www.controlledenvironments.org/wp-content/uploads/sites/6/2017/06/Ch01.pdf)
+    par_energy_fraction         = 0.5
+)
