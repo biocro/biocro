49e316e02905331e99f72b49856eaae6567594ef
eloch216 <48919455+eloch216@users.noreply.github.com>
2024-05-16 11:17:55 -0500

    Split some sunML calculations into separate functions
    All tests pass

--- src/module_library/sunML.cpp
+++ src/module_library/sunML.cpp
@@ -86,15 +86,36 @@ double thick_layer_absorption(
 }
 
 /**
+ *  @brief Computes energy flux in the near-infrared band (in J / m^2 / s) from
+ *  the photosynthetically active photon flux density (PPFD; in
+ *  micromol / m^2 / s).
+ *
+ *  We can calculate the energy in the photosynthetically active radiation (PAR)
+ *  band from the PPFD using the average energy per photon in this band. Then,
+ *  we can calculate the energy in the NIR band using the ratio of
+ *  `(PAR energy) / (total energy)`, which we call `par_energy_fraction`. If all
+ *  shortwave energy is split between the PAR and NIR bands, then
+ *  `par_energy_fraction = PAR / (PAR + NIR)`, so
+ *  `NIR / PAR = (1 - par_energy_fraction) / par_energy_fraction`.
+ */
+double nir_from_ppfd(
+    double ppfd,                // micromol / m^2 / s
+    double par_energy_content,  // J / micromol
+    double par_energy_fraction  // dimensionless
+)
+{
+    return ppfd * par_energy_content *
+           (1 - par_energy_fraction) / par_energy_fraction;  // J / m^2 / s
+}
+
+/**
  *  @brief Computes total absorbed shortwave radiation from the
  *  photosynthetically active photon flux density (PPFD) incident on a leaf.
  *
  *  The total absorbed shortwave radiation is determined using the following
  *  steps:
- *  - determine the incident photosynthetically active radiation (PAR) using the
- *    energy content of PAR
- *  - determine the incident near-infrared radiation (NIR) from the incident PAR
- *    using the PAR energy fraction
+ *  - determine the incident near-infrared radiation (NIR) from the incident
+ *    PPFD using the PAR energy content and PAR energy fraction
  *  - determine the total incident radiation by adding the incident PAR and NIR
  *  - determine the total absorbed radiation using the leaf's reflection and
  *    transmission coefficients
@@ -129,9 +150,8 @@ double absorbed_shortwave_from_incident_ppfd(
 {
     double incident_par = incident_ppfd * par_energy_content;  // J / m^2 / s
 
-    double incident_nir = incident_par *
-                          (1 - par_energy_fraction) /
-                          par_energy_fraction;  // J / m^2 /s
+    double incident_nir = nir_from_ppfd(
+        incident_ppfd, par_energy_content, par_energy_fraction);  // J / m^2 /s
 
     double incident_shortwave = incident_par + incident_nir;  // J / m^2 / s
 
@@ -139,6 +159,186 @@ double absorbed_shortwave_from_incident_ppfd(
         leaf_reflectance,
         leaf_transmittance,
         incident_shortwave);  // J / m^2 / s
+}
+
+/**
+ *  @brief Computes total radiation (direct and downscattered) using
+ *  Equation 15.15 from Campbell & Norman (1998).
+ *
+ *  The total radiation `Q_t` is given by
+ *
+ *  `Q_t = tau * Q_o`                       [Equation (1)]
+ *
+ *  where `Q_o` is the radiation just above the canopy and `tau` is a
+ *  transmission factor whose value decreases deeper into the canopy. `tau`
+ *  is given by Equation 15.6:
+ *
+ *  `tau = exp(-sqrt(alpha) * k * ell)`       [Equation (2)]
+ *
+ *  where `alpha` is the leaf absorptivity, `k` is the canopy extinction
+ *  coefficient, and `ell` is the cumulative leaf area index. Combining
+ *  Equations (1) and (2), we have
+ *
+ *  `Q_t = Q_o * exp(-sqrt(alpha) * k * ell)`  [Equation (3)]
+ *
+ *  which is implemented in the code below.
+ *
+ *  Note that `Q_o` and `Q_t` are expressed on a ground area basis; in other
+ *  words, they are fluxes of photons or energy passing through a horizontal
+ *  surface.
+ *
+ *  Also note that this equation can be used for direct or diffuse radiation,
+ *  provided appropriate values of `Q_o`, `alpha` and `k` are used.
+ *
+ *  @param [in] Q_o The radiation just above the canopy expressed on a ground
+ *              area basis using either quantum flux density
+ *              (micromol / m^2 / s) or energy density (J / m^2 / s).
+ *
+ *  @param [in] k The (dimensionless) canopy extinction coefficient.
+ *
+ *  @param [in] alpha The (dimensionless) leaf absorptivity.
+ *
+ *  @param [in] ell The cumulative leaf area index, which is dimensionless from
+ *              m^2 leaf / m^2 ground.
+ *
+ *  @return The total radiation (direct and downscattered) at depth `ell` in the
+ *          canopy, expressed in the same units as `Q_o`.
+ */
+double total_radiation(
+    double Q_o,    // Light units such as `micromol / m^2 / s` or `J / m^2 / s`
+    double k,      // dimensionless
+    double alpha,  // dimensionless
+    double ell     // dimensionless from m^2 leaf / m^2 ground
+)
+{
+    return Q_o * exp(-k * sqrt(alpha) * ell);  // same units as `Q_ob`
+}
+
+/**
+ *  @brief Computes downscattered radiation using Equation 15.20 from Campbell &
+ *  Norman (1998).
+ *
+ *  The downscattered radiation `Q_sc` is given by
+ *
+ *  `Q_sc = Q_bt - Q_b`             [Equation (1)]
+ *
+ *  where `Q_bt` is the total beam radiation (direct and downscattered) and
+ *  `Q_b` is the direct beam radiation. `Q_bt` can be calculated using the
+ *  total_radiation() function, while `Q_b` is given by Equation 15.16:
+ *
+ *  `Q_b = tau_b * Q_ob`            [Equation (2)]
+ *
+ *  where `Q_ob` is the beam radiation just above the canopy and `tau_b` is a
+ *  transmission factor whose value decreases deeper into the
+ *  canopy. `tau_b` is given by an un-numbered equation in the text which can be
+ *  found just after Equation 15.17:
+ *
+ *  `tau_b = exp(-k_direct * ell)`  [Equation (3)]
+ *
+ *  where `k_direct` is the canopy extinction coefficient for direct radiation,
+ *  and `ell` is the cumulative leaf area index.
+ *
+ *  Combining Equations (1-3) above, we arrive at
+ *
+ *  `Q_sc = Q_bt - Q_ob * exp(-sqrt(alpha) * k_direct * ell))`
+ *
+ *  which is implemented in the code below.
+ *
+ *  Note that `Q_ob` and `Q_bt` are expressed on a ground area basis; in other
+ *  words, they are fluxes of photons or energy passing through a horizontal
+ *  surface. However, `Q_sc` is diffuse radiation; it is not directional, and
+ *  its value represents the flux through _any_ surface.
+ *
+ *  @param [in] Q_ob The beam radiation just above the canopy expressed on a
+ *              ground area basis using either quantum flux density
+ *              (micromol / m^2 / s) or energy density (J / m^2 / s).
+ *
+ *  @param [in] k_direct The (dimensionless) canopy extinction coefficient for
+ *              direct radiation.
+ *
+ *  @param [in] alpha_direct The (dimensionless) leaf absorptivity for direct
+ *              radiation.
+ *
+ *  @param [in] ell The cumulative leaf area index, which is dimensionless from
+ *              m^2 leaf / m^2 ground.
+ *
+ *  @return The amount of downscattered radiation at depth `ell` in the canopy,
+ *          expressed in the same units as `Q_ob`.
+ */
+double downscattered_radiation(
+    double Q_ob,          // Light units such as `micromol / m^2 / s` or `J / m^2 / s`
+    double k_direct,      // dimensionless
+    double alpha_direct,  // dimensionless
+    double ell            // dimensionless from m^2 leaf / m^2 ground
+)
+{
+    return total_radiation(Q_ob, k_direct, alpha_direct, ell) -
+           Q_ob * exp(-k_direct * ell);  // same units as `Q_ob`
+}
+
+/**
+ *  @brief Computes the radiation incident on shaded leaves using Equation 15.19
+ *  from Campbell & Norman (1998).
+ *
+ *  The radiation incident on shaded leaves (`Q_sh`) is given by
+ *
+ *  `Q_sh = Q_d + Q_sc`  [Equation (1)]
+ *
+ *  where `Q_d` is diffuse flux from the sky and `Q_sc` is downscattered
+ *  radiation that is produced as the direct beam is scattered by leaves. `Q_sc`
+ *  can be calculated using the downscattered_radiation() function, while `Q_d`
+ *  is given by Equation 15.17:
+ *
+ *  `Q_d = tau_dt * Q_od`  [Equation (2)]
+ *
+ *  where `Q_od` is the diffuse radiation just above the canopy and `tau_dt` is
+ *  a transmission factor whose value decreases deeper into the canopy. `tau_dt`
+ *  is given by Equation 15.6, so `Q_d` can be calculated using the
+ *  total_radiation() function.
+ *
+ *  Note that `Q_ob` and `Q_od` are expressed on a ground area basis; in other
+ *  words, they are fluxes of photons or energy passing through a horizontal
+ *  surface. However, `Q_sh`, `Q_d`, and `Q_sc` are diffuse radiation; they are
+ *  not directional, and their values represent the flux through _any_ surface.
+ *
+ *  @param [in] Q_ob The direct radiation just above the canopy expressed on a
+ *              ground area basis using either quantum flux density
+ *              (micromol / m^2 / s) or energy density (J / m^2 / s).
+ *
+ *  @param [in] Q_od The diffuse radiation just above the canopy expressed on a
+ *              ground area basis using either quantum flux density
+ *              (micromol / m^2 / s) or energy density (J / m^2 / s).
+ *
+ *  @param [in] k_direct The (dimensionless) canopy extinction coefficient for
+ *              direct radiation.
+ *
+ *  @param [in] k_diffuse The (dimensionless) canopy extinction coefficient for
+ *              diffuse radiation.
+ *
+ *  @param [in] alpha_direct The (dimensionless) leaf absorptivity for direct
+ *              radiation.
+ *
+ *  @param [in] alpha_diffuse The (dimensionless) leaf absorptivity for diffuse
+ *              radiation.
+ *
+ *  @param [in] ell The cumulative leaf area index, which is dimensionless from
+ *              m^2 leaf / m^2 ground.
+ *
+ *  @return The amount radiation incident on shaded leaves at depth `ell` in the
+ *          canopy, expressed in the same units as `Q_ob`.
+ */
+double shaded_radiation(
+    double Q_ob,           // Light units such as `micromol / m^2 / s` or `J / m^2 / s`
+    double Q_od,           // same units as `Q_ob`
+    double k_direct,       // dimensionless
+    double k_diffuse,      // dimensionless
+    double alpha_direct,   // dimensionless
+    double alpha_diffuse,  // dimensionless
+    double ell             // dimensionless from m^2 leaf / m^2 ground
+)
+{
+    return total_radiation(Q_od, k_diffuse, alpha_diffuse, ell) +
+           downscattered_radiation(Q_ob, k_direct, alpha_direct, ell);  // same units as `Q_ob`
 }
 
 /**
@@ -259,20 +459,17 @@ Light_profile sunML(
         // leaf area above this layer
         const double cumulative_lai = lai_per_layer * (i + 0.5);
 
-        // Calculate the amount of PPFD scattered out of the direct beam using
-        // Equations 15.6 and 15.1 from Campbell & Norman (1998), following
-        // example 15.2. This is a diffuse flux density representing the flux
-        // through any surface.
-        const double scattered_ppfd =
-            ambient_ppfd_beam_ground * (exp(-k * sqrt(absorptivity) * cumulative_lai) -
-                                        exp(-k * cumulative_lai));  // micromol / m^2 / s
+        // Calculate the PPFD incident on shaded leaves
+        double shaded_ppfd = shaded_radiation(
+            ambient_ppfd_beam_ground, ambient_ppfd_diffuse,
+            k, kd,
+            absorptivity, 1.0,
+            cumulative_lai);  // micromol / m^2 / s
 
-        // Calculate the total flux of diffuse photosynthetically active light
-        // in this layer by combining the scattered PPFD with the ambient
-        // diffuse PPFD. Here we use Equation 15.6 with `alpha` = 1 and
-        // `kbe(phi)` = kd.
-        double diffuse_ppfd =
-            ambient_ppfd_diffuse * exp(-kd * cumulative_lai) + scattered_ppfd;  // micromol / m^2 / s
+        // Calculate the amount of PPFD scattered out of the direct beam. This
+        // is a diffuse flux density representing the flux through any surface.
+        const double scattered_ppfd = downscattered_radiation(
+            ambient_ppfd_beam_ground, k, absorptivity, cumulative_lai);  // micromol / m^2 / s
 
         // Calculate the fraction of sunlit and shaded leaves in this canopy
         // layer using Equation 15.22.
@@ -284,36 +481,36 @@ Light_profile sunML(
         // expressions as cosine_zenith_angle approaches 0 from the right:
         if (cosine_zenith_angle <= 1E-10) {
             ambient_ppfd_beam_leaf = ambient_ppfd_beam / k1;
-            diffuse_ppfd = ambient_ppfd_diffuse * exp(-kd * cumulative_lai);
+            shaded_ppfd = ambient_ppfd_diffuse * exp(-kd * cumulative_lai);
             sunlit_fraction = 0;
             shaded_fraction = 1;
         }
 
         // Store values of incident PPFD
-        light_profile.sunlit_incident_ppfd[i] = ambient_ppfd_beam_leaf + diffuse_ppfd;  // micromol / (m^2 leaf) / s
-        light_profile.incident_ppfd_scattered[i] = scattered_ppfd;                      // micromol / m^2 / s
-        light_profile.shaded_incident_ppfd[i] = diffuse_ppfd;                           // micromol / (m^2 leaf) / s
-        light_profile.sunlit_fraction[i] = sunlit_fraction;                             // dimensionless from m^2 / m^2
-        light_profile.shaded_fraction[i] = shaded_fraction;                             // dimensionless from m^2 / m^2
-        light_profile.height[i] = (lai - cumulative_lai) / heightf;                     // m
+        light_profile.sunlit_incident_ppfd[i] = ambient_ppfd_beam_leaf + shaded_ppfd;  // micromol / (m^2 leaf) / s
+        light_profile.incident_ppfd_scattered[i] = scattered_ppfd;                     // micromol / m^2 / s
+        light_profile.shaded_incident_ppfd[i] = shaded_ppfd;                           // micromol / (m^2 leaf) / s
+        light_profile.sunlit_fraction[i] = sunlit_fraction;                            // dimensionless from m^2 / m^2
+        light_profile.shaded_fraction[i] = shaded_fraction;                            // dimensionless from m^2 / m^2
+        light_profile.height[i] = (lai - cumulative_lai) / heightf;                    // m
 
         // Store values of absorbed PPFD
         light_profile.sunlit_absorbed_ppfd[i] =
             thin_layer_absorption(
                 leaf_reflectance,
                 leaf_transmittance,
-                ambient_ppfd_beam_leaf + diffuse_ppfd);  // micromol / m^2 / s
+                ambient_ppfd_beam_leaf + shaded_ppfd);  // micromol / m^2 / s
 
         light_profile.shaded_absorbed_ppfd[i] =
             thin_layer_absorption(
                 leaf_reflectance,
                 leaf_transmittance,
-                diffuse_ppfd);  // micromol / m^2 / s
+                shaded_ppfd);  // micromol / m^2 / s
 
         // Store values of absorbed solar energy (including PAR and NIR)
         light_profile.sunlit_absorbed_shortwave[i] =
             absorbed_shortwave_from_incident_ppfd(
-                ambient_ppfd_beam_leaf + diffuse_ppfd,
+                ambient_ppfd_beam_leaf + shaded_ppfd,
                 par_energy_content,
                 par_energy_fraction,
                 leaf_reflectance,
@@ -321,7 +518,7 @@ Light_profile sunML(
 
         light_profile.shaded_absorbed_shortwave[i] =
             absorbed_shortwave_from_incident_ppfd(
-                diffuse_ppfd,
+                shaded_ppfd,
                 par_energy_content,
                 par_energy_fraction,
                 leaf_reflectance,
--- src/module_library/sunML.h
+++ src/module_library/sunML.h
@@ -4,17 +4,17 @@
 #include "AuxBioCro.h"  // for MAXLAY
 
 struct Light_profile {
-    double sunlit_incident_ppfd[MAXLAY];        // micromol / (m^2 leaf) / s
-    double incident_ppfd_scattered[MAXLAY];     // micromol / m^2 / s
-    double shaded_incident_ppfd[MAXLAY];        // micromol / (m^2 leaf) / s
-    double sunlit_absorbed_ppfd[MAXLAY];        // micromol / (m^2 leaf) / s
-    double shaded_absorbed_ppfd[MAXLAY];        // micromol / (m^2 leaf) / s
-    double sunlit_absorbed_shortwave[MAXLAY];   // J / (m^2 leaf) / s
-    double shaded_absorbed_shortwave[MAXLAY];   // J / (m^2 leaf) / s
-    double sunlit_fraction[MAXLAY];             // dimensionless
-    double shaded_fraction[MAXLAY];             // dimensionless
-    double height[MAXLAY];                      // m
-    double canopy_direct_transmission_fraction; // dimensionless
+    double sunlit_incident_ppfd[MAXLAY];         // micromol / (m^2 leaf) / s
+    double incident_ppfd_scattered[MAXLAY];      // micromol / m^2 / s
+    double shaded_incident_ppfd[MAXLAY];         // micromol / (m^2 leaf) / s
+    double sunlit_absorbed_ppfd[MAXLAY];         // micromol / (m^2 leaf) / s
+    double shaded_absorbed_ppfd[MAXLAY];         // micromol / (m^2 leaf) / s
+    double sunlit_absorbed_shortwave[MAXLAY];    // J / (m^2 leaf) / s
+    double shaded_absorbed_shortwave[MAXLAY];    // J / (m^2 leaf) / s
+    double sunlit_fraction[MAXLAY];              // dimensionless
+    double shaded_fraction[MAXLAY];              // dimensionless
+    double height[MAXLAY];                       // m
+    double canopy_direct_transmission_fraction;  // dimensionless
 };
 
 double thin_layer_absorption(
@@ -31,6 +31,12 @@ double thick_layer_absorption(
                                 //   `J / m^2 / s`
 );
 
+double nir_from_ppfd(
+    double ppfd,                // micromol / m^2 / s
+    double par_energy_content,  // J / micromol
+    double par_energy_fraction  // dimensionless
+);
+
 double absorbed_shortwave_from_incident_ppfd(
     double incident_ppfd,        // micromol / m^2 / s
     double par_energy_content,   // J / micromol
@@ -38,6 +44,31 @@ double absorbed_shortwave_from_incident_ppfd(
     double leaf_reflectance,     // dimensionless
     double leaf_transmittance    // dimensionless
 );
+
+double total_radiation(
+    double Q_o,    // Light units such as `micromol / m^2 / s` or `J / m^2 / s`
+    double k,      // dimensionless
+    double alpha,  // dimensionless
+    double ell     // dimensionless from m^2 leaf / m^2 ground
+);
+
+double downscattered_radiation(
+    double Q_ob,   // Light units such as `micromol / m^2 / s` or `J / m^2 / s`
+    double k,      // dimensionless
+    double alpha,  // dimensionless
+    double ell     // dimensionless from m^2 leaf / m^2 ground
+);
+
+double shaded_radiation(
+    double Q_ob,           // Light units such as `micromol / m^2 / s` or `J / m^2 / s`
+    double Q_od,           // same units as `Q_ob`
+    double k_direct,       // dimensionless
+    double k_diffuse,      // dimensionless
+    double alpha_direct,   // dimensionless
+    double alpha_diffuse,  // dimensionless
+    double ell             // dimensionless from m^2 leaf / m^2 ground
+);
+
 struct Light_profile sunML(
     double ambient_ppfd_beam,     // micromol / (m^2 beam) / s
     double ambient_ppfd_diffuse,  // micromol / m^2 / s
