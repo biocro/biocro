1b6554fc23143d15954ab8a33143b1ea08477c16
leavesandlemmas <60299100+leavesandlemmas@users.noreply.github.com>
2024-07-17 09:06:31 -0500

    Changes in the name's of functions; removed default arguments.
    Changes in the name's of functions; removed default arguments. Necessary changes to documentation and tests.

--- R/module_writer.R
+++ R/module_writer.R
@@ -1,9 +1,7 @@
 module.write = function(
-        name = 'my_module',
-        lib = 'my_library',
-        type = 'direct',
-        inputs = 'x', 
-        outputs = 'y'
+        name = 'my_module', lib = 'my_library', type = 'direct',
+        inputs = 'x',  outputs = 'y',    
+        input.units = NULL, output.units = NULL
 ){
     name_caps = toupper(name)
     lib_caps = toupper(lib)
@@ -20,19 +18,17 @@ module.write = function(
         stop(e)
     }
     
-    input.field = input_declare(inputs)
-    input.ptr = input_pointer_list(inputs)
-    input.units = names(inputs) # might be null
-    input.get = get_list(inputs, input.units)
+    input.field = make_input_initializations(inputs)
+    input.ptr = make_input_reference_list(inputs)
+    input.get = make_get(inputs, input.units)
     
-    output.field = output_declare(outputs)
-    output.ptr = output_pointer_list(outputs)
-    output.units = names(outputs) # might be null
-    output.get = get_list(outputs, output.units)
+    output.field = make_output_initializations(outputs)
+    output.ptr = make_output_pointer_list(outputs)
+    output.get = make_get(outputs, output.units)
 
 
     
-    update.template = update_template(outputs)
+    update.template = make_update_template(outputs)
     
     sprintf(template, 
             name, lib, type, 
@@ -65,11 +61,11 @@ namespace %2$s
                 state_map* output_quantities)
         : %3$s_module(),
         
-        // Get references to input quantities 
-        %6$s
-        
-        // Get pointers to output quantities 
-        %9$s
+            // Get references to input quantities 
+            %6$s
+            
+            // Get pointers to output quantities 
+            %9$s
         {
         }
         static string_vector get_inputs();
@@ -77,11 +73,11 @@ namespace %2$s
         static std::string get_name() { return \"%1$s\"; }
         
         private:
-        // References to input quantities 
-        %7$s
-    
-        // Pointers to output quantities 
-        %10$s
+            // References to input quantities 
+            %7$s
+        
+            // Pointers to output quantities 
+            %10$s
         
         // Main operation
         void do_operation() const;
@@ -90,14 +86,14 @@ namespace %2$s
     string_vector %1$s::get_inputs()
     {
         return {
-        %8$s
+            %8$s
         };
     }
     
     string_vector %1$s::get_outputs()
     {
         return {
-        %11$s
+            %11$s
         };
     }
     
@@ -112,39 +108,42 @@ namespace %2$s
 "
 
 # newline and indent
-endl = "\n\t" 
-comma_endl = ",\n\t" 
+indent = "    " 
+endl_dbl = paste0("\n",indent,indent)
+endl_tpl = paste0("\n",indent,indent,indent)
+comma_tpl = paste0(",",end_tpl)
 
-input_declare = function(x){
+
+make_input_initializations = function(x){
     paste0(x, "{get_input(input_quantities, \"", x, "\")},",  
-           collapse=endl)    
+           collapse=endl_tpl)    
 }
 
-input_pointer_list = function(x) {
+make_input_reference_list = function(x) {
     paste0('double const& ', x, ';',
-           collapse=endl)
+           collapse=endl_tpl)
 }
 
-output_declare = function(x){
+make_output_initializations = function(x){
     paste0(x, "_op{get_op(output_quantities, \"", x, "\")}", 
-           collapse=comma_endl)    
+           collapse=comma_tpl)    
 }
 
-output_pointer_list = function(x) {
+make_output_pointer_list = function(x) {
     paste0('double* ', x, '_op;', 
-           collapse=endl)    
+           collapse=endl_tpl)    
 }
 
-get_list = function(x, units=NULL) {
+make_get = function(x, units=NULL) {
     if(is.null(units)){units_string = ''}
     else{
-        units_string = paste("\t\t//", units)
+        units_string = paste(indent, indent, "//", units)
     }
     paste0('\"', x, '\",', units_string,
-           collapse=endl)    
+           collapse=endl_tpl)    
 }
 
-update_template = function(x){
+make_update_template = function(x){
     paste0('update(', x, '_op, 0);', 
-           collapse=endl)    
+           collapse=endl_dbl)    
 }
--- tests/testthat/test.module_writer.R
+++ tests/testthat/test.module_writer.R
@@ -1,10 +1,3 @@
-
-default_out = "\n#ifndef MY_LIBRARY_MY_MODULE_H\n#define MY_LIBRARY_MY_MODULE_H\n    \n#include \"../framework/module.h\"\n#include \"../framework/state_map.h\"\n\nnamespace my_library\n{\n    /**\n    * @class my_module\n    *\n    * @brief Put Documentation Here.\n    *\n    */\n    class my_module : public direct_module\n    {\n        public:\n            my_module(\n                state_map const& input_quantities,\n                state_map* output_quantities)\n        : direct_module(),\n        \n        // Get references to input quantities \n        x{get_input(input_quantities, \"x\")},\n        \n        // Get pointers to output quantities \n        y_op{get_op(output_quantities, \"y\")}\n        {\n        }\n        static string_vector get_inputs();\n        static string_vector get_outputs();\n        static std::string get_name() { return \"my_module\"; }\n        \n        private:\n        // References to input quantities \n        double const& x;\n    \n        // Pointers to output quantities \n        double* y_op;\n        \n        // Main operation\n        void do_operation() const;\n    };\n    \n    string_vector my_module::get_inputs()\n    {\n        return {\n        \"x\",\n        };\n    }\n    \n    string_vector my_module::get_outputs()\n    {\n        return {\n        \"y\",\n        };\n    }\n    \n    void my_module::do_operation() const\n    {\n        //use update to set outputs as function of inputs\n        update(y_op, 0);\n    }\n    \n}  // namespace my_library\n#endif\n"
-
-test_that("Default output hasn't changed", {
-    expect_true(BioCro:::module.write() == default_out)
-})
-
 test_that("Duplicate input variables raise error",
     {
         expect_error(BioCro:::module.write(inputs = c('x', 'x')))
