5a0b9ee067f6bee1a1594da45731411deeef0ba3
leavesandlemmas <60299100+leavesandlemmas@users.noreply.github.com>
2024-07-18 18:57:55 -0500

    Update module_writer.R

--- R/module_writer.R
+++ R/module_writer.R
@@ -1,47 +1,47 @@
 module.write = function(
         name , lib, type,
-        inputs,  outputs,    
+        inputs,  outputs,
         input.units = NULL, output.units = NULL
 ){
     name_caps = toupper(name)
     lib_caps = toupper(lib)
-    
+
     # check for duplicate inputs
     if(anyDuplicated(inputs)){
         e <- simpleError("Duplicate inputs detected.")
         stop(e)
     }
-    
+
     # check for duplicate outputs
     if(anyDuplicated(outputs)){
         e <- simpleError("Duplicate outputs detected.")
         stop(e)
     }
-    
+
     input.field = make_input_initializations(inputs)
     input.ptr = make_input_reference_list(inputs)
     input.get = make_get(inputs, input.units)
-    
+
     output.field = make_output_initializations(outputs)
     output.ptr = make_output_pointer_list(outputs)
     output.get = make_get(outputs, output.units)
 
 
-    
+
     update.template = make_update_template(outputs)
-    
-    sprintf(template, 
-            name, lib, type, 
+
+    sprintf(template,
+            name, lib, type,
             name_caps, lib_caps,
             input.field, input.ptr, input.get,
             output.field, output.ptr, output.get,
-            update.template )   
+            update.template )
 }
 
 template = "
 #ifndef %5$s_%4$s_H
 #define %5$s_%4$s_H
-    
+
 #include \"../framework/module.h\"
 #include \"../framework/state_map.h\"
 
@@ -60,63 +60,63 @@ namespace %2$s
                 state_map const& input_quantities,
                 state_map* output_quantities)
         : %3$s_module(),
-        
-            // Get references to input quantities 
+
+            // Get references to input quantities
             %6$s
-            
-            // Get pointers to output quantities 
+
+            // Get pointers to output quantities
             %9$s
         {
         }
         static string_vector get_inputs();
         static string_vector get_outputs();
         static std::string get_name() { return \"%1$s\"; }
-        
+
         private:
-            // References to input quantities 
+            // References to input quantities
             %7$s
-        
-            // Pointers to output quantities 
+
+            // Pointers to output quantities
             %10$s
-        
+
         // Main operation
         void do_operation() const;
     };
-    
+
     string_vector %1$s::get_inputs()
     {
         return {
             %8$s
         };
     }
-    
+
     string_vector %1$s::get_outputs()
     {
         return {
             %11$s
         };
     }
-    
+
     void %1$s::do_operation() const
     {
         //use update to set outputs as function of inputs
         %12$s
     }
-    
+
 }  // namespace %2$s
 #endif
 "
 
 # newline and indent
-indent = "    " 
+indent = "    "
 endl_dbl = paste0("\n",indent,indent)
 endl_tpl = paste0("\n",indent,indent,indent)
-comma_tpl = paste0(",",end_tpl)
+comma_tpl = paste0(",",endl_tpl)
 
 
 make_input_initializations = function(x){
-    paste0(x, "{get_input(input_quantities, \"", x, "\")},",  
-           collapse=endl_tpl)    
+    paste0(x, "{get_input(input_quantities, \"", x, "\")},",
+           collapse=endl_tpl)
 }
 
 make_input_reference_list = function(x) {
@@ -125,13 +125,13 @@ make_input_reference_list = function(x) {
 }
 
 make_output_initializations = function(x){
-    paste0(x, "_op{get_op(output_quantities, \"", x, "\")}", 
-           collapse=comma_tpl)    
+    paste0(x, "_op{get_op(output_quantities, \"", x, "\")}",
+           collapse=comma_tpl)
 }
 
 make_output_pointer_list = function(x) {
-    paste0('double* ', x, '_op;', 
-           collapse=endl_tpl)    
+    paste0('double* ', x, '_op;',
+           collapse=endl_tpl)
 }
 
 make_get = function(x, units=NULL) {
@@ -140,10 +140,10 @@ make_get = function(x, units=NULL) {
         units_string = paste(indent, indent, "//", units)
     }
     paste0('\"', x, '\",', units_string,
-           collapse=endl_tpl)    
+           collapse=endl_tpl)
 }
 
 make_update_template = function(x){
-    paste0('update(', x, '_op, 0);', 
-           collapse=endl_dbl)    
+    paste0('update(', x, '_op, 0);',
+           collapse=endl_dbl)
 }
