bbece4d6de3bab2abb70d404652a6c99fd7adb21
eloch216 <48919455+eloch216@users.noreply.github.com>
2025-02-03 10:34:01 -0600

    Relax tolerance
    There are test failures on other operating systems

--- R/model_testing.R
+++ R/model_testing.R
@@ -111,7 +111,7 @@ model_test_case <- function(
     quantities_to_ignore = character(),
     row_interval = 24,
     digits = 5,
-    relative_tolerance = sqrt(.Machine$double.eps)
+    relative_tolerance = 1e-5
 )
 {
     # Check over the inputs arguments for possible issues
