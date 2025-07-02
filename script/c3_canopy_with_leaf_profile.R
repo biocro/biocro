940491138affc23f5519b7e94f89fb635ba27790
Scott Oswald <soswald77@gmail.com>
2025-07-02 17:08:12 -0500

    Update c3_canopy_with_leaf_profile.R

--- script/c3_canopy_with_leaf_profile.R
+++ script/c3_canopy_with_leaf_profile.R
@@ -80,17 +80,25 @@ optimize <- function(drivers, par0=rep(0.1, 20)){
     print(drivers[['year']][1])
     flow <- partial_run_biocro(
         initial_values = soybean$initial_values,
-        parameters = params,
+        parameters = within(params,{chil <- chil * 0.1}),
         drivers = drivers,
         direct_module_names = soybean_direct,
         differential_module_names = soybean$differential_modules,
         arg_names = kronecker(c("jmax_fraction_", "vcmax_fraction_"), 0:9, paste0)
     )    
     
+    project_to_simplex <- function(x){ 
+        x[1:10] <- x[1:10]/sum(x[1:10])
+        x[11:20] <- x[11:20]/sum(x[11:20]) 
+        x
+    }
+    
     objective_function <- function(x){
+        #c <- equality_constraint(x)
+        # u <- project_to_simplex(x)
         out <- flow_by_jv(x)
         n <- nrow(out)
-        -out[n,'Grain'] # sum(x^2)
+        -out[n,'Grain']  # +  1e-2 * sum(c^2)
     }
     
     equality_constraint <- function(x){
@@ -132,8 +140,9 @@ process_optimizations <- function(result){
         layer = rep(0:9, 2 * nyear), 
         year = years,
         param = rep(rep(c("Jmax", "Vcmax"), each = 10), nyear),
-        fraction = lapply(result, \(x){x$solution}) |> unlist()
+        fraction = lapply(result, \(x){project_to_simplex(x$solution)}) |> unlist()
         )
+    
     df['value'] <-  10 * rep(rep( c(Jmax,Vcmax), each=10), nyear) *  df[['fraction']]
     list(messages = msgs, results = df )
 }
@@ -147,10 +156,11 @@ cut_weather_data <- function(data, start_doy = 150, end_doy = 270, days = NULL){
 }
 
 
-t1 <- Sys.time()
+
 growing_season <- lapply(weather, cut_weather_data)
-# soybean_weather_result <- lapply(soybean_weather, optimize)
-weather_result <- lapply(growing_season, optimize)
+t1 <- Sys.time()
+soybean_weather_result <- lapply(soybean_weather, optimize)
+# weather_result <- lapply(growing_season, optimize)
 t2 <- Sys.time()
 t2-t1
 save(weather_result,file=".rdata")
@@ -166,30 +176,58 @@ save(weather_result,file=".rdata")
 # )
 
 # 
-# opt <- nloptr::nloptr(
-#     x0 = par,
-#     eval_f = objective_function,
-#     eval_g_eq = equality_constraint,
-#     lb = rep(0, 20),
-#     opts = list(
-#         algorithm = "NLOPT_LN_COBYLA",
-#         # algorithm = "NLOPT_GN_ISRES",
-#         # algorithm = "NLOPT_LN_AUGLAG_EQ",
-#         # local_opts = list(algorithm = "NLOPT_LN_COBYLA",
-#         #                   maxeval = 2000,
-#         #                   xtol_rel   = 1.0e-7
-#         # ),
-#         maxeval = 2000,
-#         xtol_rel   = 1.0e-6,
-#         print_level = 3)
-# )
-# 
+opt <- nloptr::nloptr(
+    x0 = rep(0.1,20),
+    eval_f = objective_function,
+    eval_g_ineq = equality_constraint,
+    lb = rep(0, 20),
+    opts = list(
+        algorithm = "NLOPT_LN_BOBYQA",
+        # algorithm = "NLOPT_GN_ISRES",
+        # algorithm = "NLOPT_LN_AUGLAG_EQ",
+        # local_opts = list(algorithm = "NLOPT_LN_COBYLA",
+        #                   maxeval = 2000,
+        #                   xtol_rel   = 1.0e-7
+        # ),
+        maxeval = 200,
+        xtol_rel   = 1.0e-6,
+        print_level = 3)
+)
+
+get_ratio <- function(result){
+    msgs <- lapply(result, function(x)x$message)
+    ys <- names(result)
+    nyear <- length(ys)
+    years <- rep(ys, each=10)
+    
+    Jmax <- params$Jmax_at_25
+    Vcmax <- params$Vcmax_at_25 
+    
+    df= data.frame(
+        layer = rep(0:9, nyear), 
+        year = years,
+        param = rep("Jmax/Vcmax", 10 * nyear),
+        fraction_ratio = lapply(result, \(x){x$solution[1:10]/x$solution[11:20]}) |> unlist()
+    )
+    
+    df['ratio'] <-  Jmax /Vcmax *  df[['fraction_ratio']]
+    list(messages = msgs, results = df )
+    
+} 
 # result <- flow_by_jv(opt$solution)
 plt + geom_line(data=result, mapping = aes(x=time,y=Leaf), color = 'red') + theme_bw()
 
-ggplot(soyres$results, aes(layer, value, color = year)) + geom_point() + geom_line() + theme_bw() + facet_grid(cols = vars(param)) + labs(y="micromol / m^2 / s")
-# cmat <- rbind(rep(c(1,0),each=10), rep(c(0,1), each=10))
-# opt <- stats::constrOptim(par, objective_function, ui = cmat, ci = rep(1,2))
+soyres <- rbind(soyres_neldermead$results,soyres_cobyla$results)
+soyres$method <- rep(c("neldermead","cobyla"),40)
+
+qlt <- ggplot(soyres_neldermead$results, aes(layer, value, color = year)) + geom_point() + geom_line() + theme_bw() + facet_grid(cols = vars(param)) + labs(y="micromol / m^2 / s")
+ggplot(soyres, aes(layer, value, color = method)) + geom_point() + geom_line() + theme_bw() + facet_grid(rows=vars(year), cols = vars(param)) + labs(y="micromol / m^2 / s")
+
+
+
+
+cmat <- rbind(rep(c(1,0),each=10), rep(c(0,1), each=10))
+opt <- stats::constrOptim(rep(0.1, 20), objective_function, ui = -cmat, ci = rep(-1,2), method= "Nelder-Mead")
 
 
 
