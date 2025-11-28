20de8b000108ce4372f5db8d7a4285757784ebb2
leavesandlemmas <soswald77@gmail.com>
2025-11-28 08:19:21 -0600

    multilayer_soil_profile_avg.h

--- src/module_library/multi_layer_soil_profile.h
+++ src/module_library/multi_layer_soil_profile.h
@@ -19,32 +19,26 @@ class multi_layer_soil_profile : public differential_module
     static const size_t num_layers = 6;
 
     struct soil_layer {
-        const double& soil_depth;
-        const double& soil_water_content;
+        const double& depth;
+        const double& water_content;
         const double& deltaS;
         const double& deltaU;
         const double& deltaT;
         const double& uptake;
         
-        double* soil_water_content_op;
+        double* water_content_op;
         
         soil_layer(state_map const& input_quantities, state_map* output_quantities, size_t layer_num) :
-            soil_depth{get_input(input_quantities, "soil_depth_" + std::to_string(layer_num))},
-            soil_water_content{get_input(input_quantities, "soil_water_content_" + std::to_string(layer_num))},
+            depth{get_input(input_quantities, "soil_depth_" + std::to_string(layer_num))},
+            water_content{get_input(input_quantities, "soil_water_content_" + std::to_string(layer_num))},
             deltaS{get_input(input_quantities, "deltaS_" + std::to_string(layer_num))},
             deltaU{get_input(input_quantities, "deltaU_" + std::to_string(layer_num))},
             deltaT{get_input(input_quantities, "deltaT_" + std::to_string(layer_num))},
             uptake{get_input(input_quantities, "uptake_layer_" + std::to_string(layer_num))},
-            soil_water_content_op{get_op(output_quantities, "soil_water_content_" + std::to_string(layer_num))} {
+            water_content_op{get_op(output_quantities, "soil_water_content_" + std::to_string(layer_num))} {
                 
 
             }
-
-       inline double delta_soil_water_content() const {
-            return out;
-       }       
-        
-      
     }; 
 
    public:
@@ -54,10 +48,7 @@ class multi_layer_soil_profile : public differential_module
         : differential_module(),
 
           // Get references to input quantities
-
-          // soil_reflectance{get_input(input_quantities, "soil_reflectance")}, //Albedo
-          soil_evaporation_rate{get_input(input_quantities, "soil_evaporation_rate")},
-          surface_runoff{get_input(input_quantities, "surface_runoff")}
+          soil_evaporation_rate{get_input(input_quantities, "soil_evaporation_rate")}
 
     {   
         layers.reserve(num_layers);
@@ -71,7 +62,6 @@ class multi_layer_soil_profile : public differential_module
 
    private:
     // References to input quantities
-    // double const& soil_reflectance;
     double const& soil_evaporation_rate;
     std::vector <soil_layer> layers;
    
@@ -81,20 +71,23 @@ class multi_layer_soil_profile : public differential_module
 
 string_vector multi_layer_soil_profile::get_inputs()
 {
-    string_vector out;
-    constexpr size_t size = 2 + 6 * num_layers;
-    out.reserve(size);
-    out.push_back("soil_evaporation_rate");
+    string_vector names = {
+        "soil_depth",
+        "soil_water_content",
+        "deltaS",
+        "deltaU",
+        "deltaT",
+        "uptake_layer"
+    };
+    
+    string_vector inputs;    
+    inputs.reserve(1 + names.size() * num_layers);
+    inputs.push_back("soil_evaporation_rate");
     for (size_t i = 1; i < num_layers + 1; ++i) { // index by 1
-        out.push_back("soil_depth_" + std::to_string(i));
-        out.push_back("soil_water_content_" + std::to_string(i));
-        out.push_back("deltaS_" + std::to_string(i));
-        out.push_back("deltaU_" + std::to_string(i));
-        out.push_back("deltaT_" + std::to_string(i));
-        out.push_back("uptake_layer_" + std::to_string(i));
+        for (auto name : names) 
+            inputs.push_back(name + "_" + std::to_string(i));
     }
-    return out;    
-
+    return inputs;    
 }
 
 string_vector multi_layer_soil_profile::get_outputs()
@@ -114,23 +107,22 @@ void multi_layer_soil_profile::do_operation() const
     for (int l = 0; l < layers.size(); l++) {
      // Calculate total change in soil water content
 
-
         constexpr double cm_per_m = 100;
         // adding uptake because value is negative
-        ds = layers[l].deltaS + layers[l].deltaU + layers[l].deltaT + (layers[l].uptake / (cm_per_m * layers[l].soil_depth));
+        ds = layers[l].deltaS + layers[l].deltaU + layers[l].deltaT + (layers[l].uptake / (cm_per_m * layers[l].depth));
         // ensure output is not negative; this is an Euler method step.     
-        if (layers[l].soil_water_content + ds < 0) { 
-            ds = -layers[l].soil_water_content;
+        if (layers[l].water_content + ds < 0) { 
+            ds = -layers[l].water_content;
         }
             
         if (l == 0) {
-            ds -= soil_evaporation_rate / (10.0 * layers[l].soil_depth);
+            ds -= soil_evaporation_rate / (10.0 * layers[l].depth);
         }      
         // Mg/ha/hr
     // double soil_evap = soil_evaporation_rate/ 10.0; // Mg/ha/hr to mm/hr
     // remove soil evaporation from first layer, but don't let water content go
     // negative. This is a crude, and hopefully temporary, fix. -mlm/    
-        update(layers[l].soil_water_content_op, ds);
+        update(layers[l].water_content_op, ds);
     }
  
 }
--- src/module_library/multilayer_soil_profile_avg.h
+++ src/module_library/multilayer_soil_profile_avg.h
@@ -15,6 +15,28 @@ namespace standardBML
 class multilayer_soil_profile_avg : public direct_module
 
 {
+    static const size_t num_layers = 6;
+
+    struct soil_layer {
+        const double& depth;
+        const double& water_content;
+        const double& saturated_conductivity;
+        const double& saturation_capacity;
+        const double& field_capacity;
+        const double& wilting_point;
+        
+        soil_layer(state_map const& input_quantities, state_map* output_quantities, size_t layer_num) :
+            depth{get_input(input_quantities, "soil_depth_" + std::to_string(layer_num))},
+            water_content{get_input(input_quantities, "soil_water_content_" + std::to_string(layer_num))},
+            saturated_conductivity{get_input(input_quantities, "soil_saturated_conductivity_" + std::to_string(layer_num))},
+            saturation_capacity{get_input(input_quantities, "soil_saturation_capacity_" + std::to_string(layer_num))},
+            field_capacity{get_input(input_quantities, "soil_field_capacity_" + std::to_string(layer_num))},
+            wilting_point{get_input(input_quantities, "soil_wilting_point_" + std::to_string(layer_num))}
+           {
+                
+
+            }
+    }; 
    public:
     multilayer_soil_profile_avg(
         state_map const& input_quantities,
@@ -25,54 +47,6 @@ class multilayer_soil_profile_avg : public direct_module
 
           max_rooting_layer{get_input(input_quantities, "max_rooting_layer")},
 
-          // Inputs for layer 1
-          soil_depth_1{get_input(input_quantities, "soil_depth_1")},
-          soil_water_content_1{get_input(input_quantities, "soil_water_content_1")},
-          soil_saturated_conductivity_1{get_input(input_quantities, "soil_saturated_conductivity_1")},
-          soil_saturation_capacity_1{get_input(input_quantities, "soil_saturation_capacity_1")},
-          soil_field_capacity_1{get_input(input_quantities, "soil_field_capacity_1")},
-          soil_wilting_point_1{get_input(input_quantities, "soil_wilting_point_1")},
-
-          // Inputs for layer 2
-          soil_depth_2{get_input(input_quantities, "soil_depth_2")},
-          soil_water_content_2{get_input(input_quantities, "soil_water_content_2")},
-          soil_saturated_conductivity_2{get_input(input_quantities, "soil_saturated_conductivity_2")},
-          soil_saturation_capacity_2{get_input(input_quantities, "soil_saturation_capacity_2")},
-          soil_field_capacity_2{get_input(input_quantities, "soil_field_capacity_2")},
-          soil_wilting_point_2{get_input(input_quantities, "soil_wilting_point_2")},
-
-          // Inputs for layer 3
-          soil_depth_3{get_input(input_quantities, "soil_depth_3")},
-          soil_water_content_3{get_input(input_quantities, "soil_water_content_3")},
-          soil_saturated_conductivity_3{get_input(input_quantities, "soil_saturated_conductivity_3")},
-          soil_saturation_capacity_3{get_input(input_quantities, "soil_saturation_capacity_3")},
-          soil_field_capacity_3{get_input(input_quantities, "soil_field_capacity_3")},
-          soil_wilting_point_3{get_input(input_quantities, "soil_wilting_point_3")},
-
-          // Inputs for layer 4
-          soil_depth_4{get_input(input_quantities, "soil_depth_4")},
-          soil_water_content_4{get_input(input_quantities, "soil_water_content_4")},
-          soil_saturated_conductivity_4{get_input(input_quantities, "soil_saturated_conductivity_4")},
-          soil_saturation_capacity_4{get_input(input_quantities, "soil_saturation_capacity_4")},
-          soil_field_capacity_4{get_input(input_quantities, "soil_field_capacity_4")},
-          soil_wilting_point_4{get_input(input_quantities, "soil_wilting_point_4")},
-
-          // Inputs for layer 5
-          soil_depth_5{get_input(input_quantities, "soil_depth_5")},
-          soil_water_content_5{get_input(input_quantities, "soil_water_content_5")},
-          soil_saturated_conductivity_5{get_input(input_quantities, "soil_saturated_conductivity_5")},
-          soil_saturation_capacity_5{get_input(input_quantities, "soil_saturation_capacity_5")},
-          soil_field_capacity_5{get_input(input_quantities, "soil_field_capacity_5")},
-          soil_wilting_point_5{get_input(input_quantities, "soil_wilting_point_5")},
-
-          // Inputs for layer 6
-          soil_depth_6{get_input(input_quantities, "soil_depth_6")},
-          soil_water_content_6{get_input(input_quantities, "soil_water_content_6")},
-          soil_saturated_conductivity_6{get_input(input_quantities, "soil_saturated_conductivity_6")},
-          soil_saturation_capacity_6{get_input(input_quantities, "soil_saturation_capacity_6")},
-          soil_field_capacity_6{get_input(input_quantities, "soil_field_capacity_6")},
-          soil_wilting_point_6{get_input(input_quantities, "soil_wilting_point_6")},
-
           // Get pointers to output quantities - Change in water content of each layer
           soil_water_content_op{get_op(output_quantities, "soil_water_content")},
           soil_saturated_conductivity_op{get_op(output_quantities, "soil_saturated_conductivity")},
@@ -80,6 +54,10 @@ class multilayer_soil_profile_avg : public direct_module
           soil_field_capacity_op{get_op(output_quantities, "soil_field_capacity")},
           soil_wilting_point_op{get_op(output_quantities, "soil_wilting_point")}
     {
+        
+        layers.reserve(num_layers);
+        for (size_t i = 1; i < num_layers + 1; ++i)
+            layers.emplace_back(soil_layer(input_quantities, output_quantities, i));
     }
 
     static string_vector get_inputs();
@@ -90,54 +68,7 @@ class multilayer_soil_profile_avg : public direct_module
     // References to input quantities
 
     double const& max_rooting_layer;
-
-    // Parameters for layer 1
-    double const& soil_depth_1;
-    double const& soil_water_content_1;
-    double const& soil_saturated_conductivity_1;
-    double const& soil_saturation_capacity_1;
-    double const& soil_field_capacity_1;
-    double const& soil_wilting_point_1;
-
-    // Parameters for layer 2
-    double const& soil_depth_2;
-    double const& soil_water_content_2;
-    double const& soil_saturated_conductivity_2;
-    double const& soil_saturation_capacity_2;
-    double const& soil_field_capacity_2;
-    double const& soil_wilting_point_2;
-
-    // Parameters for layer 3
-    double const& soil_depth_3;
-    double const& soil_water_content_3;
-    double const& soil_saturated_conductivity_3;
-    double const& soil_saturation_capacity_3;
-    double const& soil_field_capacity_3;
-    double const& soil_wilting_point_3;
-
-    // Parameters for layer 4
-    double const& soil_depth_4;
-    double const& soil_water_content_4;
-    double const& soil_saturated_conductivity_4;
-    double const& soil_saturation_capacity_4;
-    double const& soil_field_capacity_4;
-    double const& soil_wilting_point_4;
-
-    // Parameters for layer 5
-    double const& soil_depth_5;
-    double const& soil_water_content_5;
-    double const& soil_saturated_conductivity_5;
-    double const& soil_saturation_capacity_5;
-    double const& soil_field_capacity_5;
-    double const& soil_wilting_point_5;
-
-    // Parameters for layer 6
-    double const& soil_depth_6;
-    double const& soil_water_content_6;
-    double const& soil_saturated_conductivity_6;
-    double const& soil_saturation_capacity_6;
-    double const& soil_field_capacity_6;
-    double const& soil_wilting_point_6;
+    std::vector<soil_layer> layers;
 
     // Pointers to output parameters
     double* soil_water_content_op;
@@ -151,52 +82,23 @@ class multilayer_soil_profile_avg : public direct_module
 };
 
 string_vector multilayer_soil_profile_avg::get_inputs()
-{
-    return {
-
-        "max_rooting_layer",
-
-        "soil_depth_1",
-        "soil_water_content_1",
-        "soil_saturated_conductivity_1",
-        "soil_saturation_capacity_1",
-        "soil_field_capacity_1",
-        "soil_wilting_point_1",
-
-        "soil_depth_2",
-        "soil_water_content_2",
-        "soil_saturated_conductivity_2",
-        "soil_saturation_capacity_2",
-        "soil_field_capacity_2",
-        "soil_wilting_point_2",
-
-        "soil_depth_3",
-        "soil_water_content_3",
-        "soil_saturated_conductivity_3",
-        "soil_saturation_capacity_3",
-        "soil_field_capacity_3",
-        "soil_wilting_point_3",
-
-        "soil_depth_4",
-        "soil_water_content_4",
-        "soil_saturated_conductivity_4",
-        "soil_saturation_capacity_4",
-        "soil_field_capacity_4",
-        "soil_wilting_point_4",
-
-        "soil_depth_5",
-        "soil_water_content_5",
-        "soil_saturated_conductivity_5",
-        "soil_saturation_capacity_5",
-        "soil_field_capacity_5",
-        "soil_wilting_point_5",
-
-        "soil_depth_6",
-        "soil_water_content_6",
-        "soil_saturated_conductivity_6",
-        "soil_saturation_capacity_6",
-        "soil_field_capacity_6",
-        "soil_wilting_point_6"};
+{   
+    string_vector names = {
+        "soil_depth",
+        "soil_water_content",
+        "soil_saturated_conductivity",
+        "soil_saturation_capacity",
+        "soil_field_capacity",
+        "soil_wilting_point"};
+        
+    string_vector inputs;
+    inputs.reserve(names.size() * num_layers + 1);
+    for (size_t i = 1; i < num_layers + 1; ++i) {
+        for (auto name : names) 
+            inputs.push_back(name + "_" + std::to_string(i));
+    }
+    inputs.push_back("max_rooting_layer");
+    return inputs;
 }
 
 string_vector multilayer_soil_profile_avg::get_outputs()
@@ -211,53 +113,9 @@ string_vector multilayer_soil_profile_avg::get_outputs()
 
 void multilayer_soil_profile_avg::do_operation() const
 {
-    double soil_depth_arr[] = {
-        soil_depth_1,
-        soil_depth_2,
-        soil_depth_3,
-        soil_depth_4,
-        soil_depth_5,
-        soil_depth_6};
-
-    double soil_water_content_arr[] = {
-        soil_water_content_1,
-        soil_water_content_2,
-        soil_water_content_3,
-        soil_water_content_4,
-        soil_water_content_5,
-        soil_water_content_6};
-
-    double soil_saturated_conductivity_arr[] = {
-        soil_saturated_conductivity_1,
-        soil_saturated_conductivity_2,
-        soil_saturated_conductivity_3,
-        soil_saturated_conductivity_4,
-        soil_saturated_conductivity_5,
-        soil_saturated_conductivity_6};
-
-    double soil_saturation_capacity_arr[] = {
-        soil_saturation_capacity_1,
-        soil_saturation_capacity_2,
-        soil_saturation_capacity_3,
-        soil_saturation_capacity_4,
-        soil_saturation_capacity_5,
-        soil_saturation_capacity_6};
-
-    double soil_field_capacity_arr[] = {
-        soil_field_capacity_1,
-        soil_field_capacity_2,
-        soil_field_capacity_3,
-        soil_field_capacity_4,
-        soil_field_capacity_5,
-        soil_field_capacity_6};
-
-    double soil_wilting_point_arr[] = {
-        soil_wilting_point_1,
-        soil_wilting_point_2,
-        soil_wilting_point_3,
-        soil_wilting_point_4,
-        soil_wilting_point_5,
-        soil_wilting_point_6};
+    if (max_rooting_layer > num_layers) {
+        throw std::out_of_range("`max_rooting_layer` exceeds the number of layers in the module `multilayer_soil_profile_avg`. Expected `max_rooting_layer` < " + std::to_string(num_layers));
+    }
 
     // total soil depth - first four layers for Miscanthus roots (30-45cm)
     // double total_soil_depth = 0.0;
@@ -275,12 +133,12 @@ void multilayer_soil_profile_avg::do_operation() const
     double tot_soil_wilting_point = 0.0;
 
     for (int l = 0; l < max_rooting_layer; l++) {
-        tot_soil_depth += soil_depth_arr[l];
-        tot_soil_water_content += soil_water_content_arr[l] * soil_depth_arr[l];
-        tot_soil_saturated_conductivity += soil_saturated_conductivity_arr[l] * soil_depth_arr[l];
-        tot_soil_saturation_capacity += soil_saturation_capacity_arr[l] * soil_depth_arr[l];
-        tot_soil_field_capacity += soil_field_capacity_arr[l] * soil_depth_arr[l];
-        tot_soil_wilting_point += soil_wilting_point_arr[l] * soil_depth_arr[l];
+        tot_soil_depth += layers[l].depth;
+        tot_soil_water_content += layers[l].water_content * layers[l].depth;
+        tot_soil_saturated_conductivity += layers[l].saturated_conductivity * layers[l].depth;
+        tot_soil_saturation_capacity += layers[l].saturation_capacity * layers[l].depth;
+        tot_soil_field_capacity += layers[l].field_capacity * layers[l].depth;
+        tot_soil_wilting_point += layers[l].wilting_point * layers[l].depth;
     }
     // Voulmetric average soil parametric values
     double soil_water_content = tot_soil_water_content / tot_soil_depth;
