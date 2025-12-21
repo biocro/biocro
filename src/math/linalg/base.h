3424c903b4f61352c26b0ea90f03864555cdaace
leavesandlemmas <soswald77@gmail.com>
2025-12-21 11:59:33 -0600

    expresion templates

--- src/math/linalg/base.h
+++ src/math/linalg/base.h
@@ -2,45 +2,115 @@
 #define LINALG_BASE_H
 
 #include <vector>
+#include <array>
 
 namespace linalg {
 
-struct vector {
-    using container = std::vector<double>;
-    
-    size_t dim() const;  
-private:
-    container data;
-    
+// Define common Vector and Matrix Expression intefaces
+template<E>
+struct Vector {
+    static constexpr bool IS_LEAF = false;
+
+    [[nodiscard]]
+    double operator[](size_t i) const {
+        // Delegation to the actual expression type.
+        // This avoids dynamic polymorphism (a.k.a. virtual functions in C++)
+        return static_cast<E const&>(*this)[i];
+    }
+
+    [[nodiscard]]
+    size_t dim() const { 
+        return static_cast<E const&>(*this).dim(); 
+    }
+
 };
 
-struct matrix {
-    using container = std::vector<double>;
+template<E>
+struct Matrix {
     
-    static matrix zeros(size_t row, size_t col) {
-        return matrix(row, col);
+    static constexpr bool IS_LEAF = false;
+
+    [[nodiscard]]
+    double operator()(size_t i, size_t j) const {
+        // Delegation to the actual expression type.
+        // This avoids dynamic polymorphism (a.k.a. virtual functions in C++)
+        return static_cast<E const&>(*this)(i, j);
     }
 
-    const double& operator()(size_t i, size_t j) const {
-        return data[col_dim * i + j];
-    }
-    
-    double& operator()(size_t i, size_t j) {
-        return data[col_dim * i + j];
-    }
+    [[nodiscard]]
+    size_t row() const { 
+        return static_cast<E const&>(*this).row(); 
+    }    
 
-    
-    size_t ncol() const {return row_dim;};
-    size_t nrow() const {return col_dim;};
-private:
-    matrix(size_t row, size_t col) : row_dim{row}, col_dim{col}, data(row * col, 0) {}
-    size_t row_dim;
-    size_t col_dim;
-    
-    container data;
-    
-};
+    [[nodiscard]]
+    size_t col() const { 
+        return static_cast<E const&>(*this).col(); 
+    }
+}
 
+// Declare Concrete Types for storage.
+
+//struct vector {
+//    using container = std::vector<double>;
+//    
+//    size_t dim() const;  
+//private:
+//    container data;
+//    
+//};
+//
+//struct dynamic_matrix {
+//    using container = std::vector<double>;
+//    
+//    static matrix zeros(size_t row, size_t col) {
+//        return matrix(row, col);
+//    }
+//
+//    const double& operator()(size_t i, size_t j) const {
+//        return data[col_dim * i + j];
+//    }
+//    
+//    double& operator()(size_t i, size_t j) {
+//        return data[col_dim * i + j];
+//    }
+//
+//    
+//    size_t col() const {return Row;};
+//    size_t row() const {return Col;};
+//private:
+//    matrix(size_t row, size_t col) : Row{row}, Col{col}, data(Row * Col, 0) {}
+//    size_t Row;
+//    size_t Col;
+//    
+//    container data;
+//    
+//};
+//
+//
+//template<size_t Row, size_t Col>
+//struct matrix {
+//    using container = std::array<double, Row * Col>;
+//    
+//    
+//    
+//    const double& operator()(size_t i, size_t j) const {
+//        return data[Col * i + j];
+//    }
+//    
+//    double& operator()(size_t i, size_t j) {
+//        return data[Col * i + j];
+//    }
+//
+//    
+//    size_t col() const {return Row;};
+//    size_t row() const {return Col;};
+//private:
+//    
+//    container data;
+//    
+//};
+//
+//
 
 
 }
--- src/math/linalg/lu.h
+++ src/math/linalg/lu.h
@@ -2,5 +2,32 @@
 #define LINALG_LU_H
 namespace linalg {
 
+template<typename MatrixType, typename VectorType>
+struct lu {
+    
+    MatrixType A;
+    VectorType b;        
+    
+private:
+    
+    void lu_decomposition(){}
+
+    void forward_eliminate() {
+        for (size_t i = 1; i < A.row(); ++i ) {
+            for (size_t j = 0; j < i - 1; ++j)
+                b[i] -= A(i, j) * b[j];
+        }
+    }
+
+    void backward_eliminate() {
+        for (size_t i = A.row() - 1; i >= 0; --i) {
+            for (size_t j = i; j < A.row(); ++j)
+                b[i] -= A(i, j) * b[j];
+            b[i] /= A(i, i);
+        }
+    }
+};
+
+
 }
 #endif 
