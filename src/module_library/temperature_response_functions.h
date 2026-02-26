e391aef40ea7181f34253d63d351a38b8de760a8
eloch216 <48919455+eloch216@users.noreply.github.com>
2026-02-25 16:33:21 -0800

    Add new temperature response functions and documentation

--- src/module_library/temperature_response_functions.h
+++ src/module_library/temperature_response_functions.h
@@ -36,7 +36,7 @@
  *
  *  @param [in] temperature_k Absolute temperature in Kelvin
  *
- *  @return Return The Arrhenius exponential `e^(c - E_a / R / T)`
+ *  @return The Arrhenius exponential `e^(c - E_a / R / T)`
  */
 inline double arrhenius_exponential(
     double c,                  // dimensionless
@@ -72,23 +72,219 @@ inline double Q10_temperature_response(
 }
 
 /**
- *  @brief A temperature response function originally defined in Johnson,
- *         Eyring, and Williams (1942), but used more recently to describe the
- *         temperature response of triose phosphate utilization limitations in
- *         Harley et al. (1992), Sharkey et al. (2007), and Yang et al. (2016).
+ *  @brief Calculates a relative reaction rate using the Eyring equation
+ *
+ *  The Eyring equation (Eyring 1935) gives the dependence of the rate constant
+ *  of a chemical reaction on the absolute temperature:
+ *
+ *  > `k = kappa * e^(S / R) * e^(-H / R / T) * (k * T / h)` (1)
+ *
+ *  where `k` is the rate constant, `kappa` is the transmission coefficient,
+ *  `S` is the entropy of activation, `H` is the heat of activation, `T` is the
+ *  absolute temperature, `R` is the ideal gas constant, `k` is Boltzmann's
+ *  constant, and `h` is Planck's constant.
+ *
+ *  This function is often used to calculate relative temperature responses; in
+ *  other words, `r(T) = k(T) / k(T_ref)`, where `T_ref` is a reference
+ *  temperature, often chosen to be 25 degrees C. Using Equation (1), the
+ *  relative rate is given by
+ *
+ *  > `r = [kappa * k / (h * k(T_ref))] * T * e^(S / R) * e^(-H / R / T)` (2)
+ *
+ *  For simplicity, here we define a constant scaling factor `gamma`, equal to
+ *  `kappa * k / (h * k(T_ref))`. Thus, Equation 2 can be written as
+ *
+ *  > `r = gamma * T * e^(S / R - H / R / T)` (3)
+ *
+ *  The user should ensure that `gamma` is chosen such that `r` evaluates to 1
+ *  at the reference temperature.
+ *
+ *  Note that the exponential factor is superficially similar to an Arrhenius
+ *  exponential with `c = S / R` and `E_a = H`. We use this to simplify the
+ *  calculations in the code.
  *
  *  References:
+ *  - [Eyring, Henry. Chemical Reviews 17, 65–77 (1935)]
+ *    (https://doi.org/10.1021/cr60056a006)
+ *
+ *  @param [in] gamma Constant scaling factor (K^-1)
+ *
+ *  @param [in] H Heat of activation (J / mol)
+ *
+ *  @param [in] S Entropy of activation (J / K / mol)
+ *
+ *  @param [in] temperature_k Absolute temperature (K)
+ *
+ *  @return Relative reaction rate `r` (dimensionless)
+ */
+inline double eyring_response(
+    double gamma,         // K^-1
+    double H,             // J / mol
+    double S,             // J / K / mol
+    double temperature_k  // K
+)
+{
+    using physical_constants::ideal_gas_constant;  // J / k / mol
+
+    return gamma * temperature_k *
+           arrhenius_exponential(S / ideal_gas_constant, H, temperature_k);
+}
+
+/**
+ *  @brief A temperature response function originally defined in Hall (1979),
+ *  often used to represent peaked temperature responses in plant models.
+ *
+ *  The peaked Arrhenius response gives the dependence of the value of a
+ *  parameter on the absolute temperature:
+ *
+ *  > `p = k * exp(-H_a / R / T) / [1 + exp(S / R) * exp(-H_d / R / T)]` (1)
+ *
+ *  Where `p` is the parameter value, `k` is a scaling factor, `H_a` is the
+ *  enthalpy of activation, `S` is the entropy, `H_d` is the enthalpy of
+ *  deactivation, `T` is the absolute temperature, and `R` is the ideal gas
+ *  constant.
+ *
+ *  By defining `c` such that `k = exp(c)`, this can be written as:
+ *
+ *  > `p = exp(c - H_a / R / T) / [1 + exp(S / R - H_d / R / T)]` (2)
+ *
+ *  This is the form used here. Typically `c` is chosen such that `p` is equal
+ *  to 1 at a particular reference temperature, often 25 degrees C. Then `p`
+ *  represents a relative parameter value.
+ *
+ *  This function was originally based on the Johnson-Eyring-Williams equation,
+ *  but with the linear `T` factor "omitted because it has little influence on
+ *  the function" (Hall 1979). Without this factor, the peaked Arrhenius
+ *  equation simplifies to the Arrhenius equation when `H_d` is infinitely
+ *  large.
+ *
+ *  The name of this function is somewhat arbitrary, since it also resembles the
+ *  Eyring equation and the Johnson-Eyring-Williams equation. In the literature,
+ *  it is also sometimes called the "modified Arrhenius response." Here we call
+ *  it the "peaked Arrhenius response" because of its peaked shape and to
+ *  highlight the fact that it simplifies to the Arrhenius equation for certain
+ *  parameter values.
+ *
+ *  Note that Johnson, Eyring, and Williams (1942) is sometimes erroneously
+ *  cited as the source of the peaked Arrhenius equation. (For example, see
+ *  Harley et al. 1992). However, this equation does not appear in the 1942
+ *  publication, and seems to have originated in Hall (1979). It is more widely
+ *  used than the Johnson-Eyring-Williams equation, most famously in Farquhar
+ *  et al. (1980) and Sharkey et al. (2007).
+ *
+ *  For an in-depth discussion of these equations and their role in plant
+ *  modeling, see Murphy & Stinziano (2021) and Yin (2021).
+ *
+ *  Note that the exponential factor in the numerator is equivalent to an
+ *  Arrhenius exponential, and the exponential factor in the denominator is
+ *  superficially similar to one with `c = S / R` and `E_a = H_d`. We use this
+ *  to simplify the calculations in the code.
+ *
+ *  References:
+ *  - [Hall, A. E. Oecologia 43, 299–316 (1979)]
+ *    (https://doi.org/10.1007/BF00344957)
+ *
  *  - [Johnson, F. H., Eyring, H. & Williams, R. W. Journal of Cellular and
  *    Comparative Physiology 20, 247–268 (1942)](https://doi.org/10.1002/jcp.1030200302)
  *
  *  - [Harley, P. C., Thomas, R. B., Reynolds, J. F. & Strain, B. R. Plant, Cell
  *    & Environment 15, 271–282 (1992)](https://doi.org/10.1111/j.1365-3040.1992.tb00974.x)
  *
+ *  - [Farquhar, G. D., von Caemmerer, S. & Berry, J. A. Planta 149, 78–90 (1980)]
+ *    (https://doi.org/10.1007/BF00386231)
+ *
  *  - [Sharkey, T. D., Bernacchi, C. J., Farquhar, G. D. & Singsaas, E. L.
  *    Plant, Cell & Environment 30, 1035–1040 (2007)](10.1111/j.1365-3040.2007.01710.x)
  *
+ *  - [Murphy, B. K. & Stinziano, J. R. New Phytologist 231, 2371–2381 (2021)]
+ *    (https://doi.org/10.1111/nph.16883)
+ *
+ *  - [Yin, X. New Phytologist 231, 2113–2116 (2021)]
+ *    (https://doi.org/10.1111/nph.17341)
+ *
+ *  @param [in] c Dimensionless scaling parameter
+ *
+ *  @param [in] Ha Enthalpy of activation (J / mol)
+ *
+ *  @param [in] Hd Enthalpy of deactivation (J / mol)
+ *
+ *  @param [in] S Entropy (J / K / mol)
+ *
+ *  @param [in] temperature_k Absolute temperature (K)
+ *
+ *  @return The value of a reaction rate (or some other parameter) at the
+ *          specified temperature; the units of the return value will depend on
+ *          the particular situation where this function is being used.
+ */
+inline double peaked_arrhenius_response(
+    double c,             // dimensionless
+    double Ha,            // J / mol
+    double Hd,            // J / mol
+    double S,             // J / K / mol
+    double temperature_k  // K
+)
+{
+    using physical_constants::ideal_gas_constant;  // J / k / mol
+
+    double const top = arrhenius_exponential(c, Ha, temperature_k);
+
+    double const bot =
+        1.0 + arrhenius_exponential(S / ideal_gas_constant, Hd, temperature_k);
+
+    return top / bot;
+}
+
+/**
+ *  @brief A temperature response function originally defined in Johnson,
+ *  Eyring, and Williams (1942), but used more recently to describe the
+ *  temperature response of triose phosphate utilization limitations in Yang et
+ *  al. (2016).
+ *
+ *  The Johnson-Eyring-Williams response gives the dependence of the rate
+ *  constant of a chemical reaction on the absolute temperature:
+ *
+ *  > `k = c'' * T * exp(-H_a / (R * T)) / [1 + exp(S / R) * exp(-H_d / (R * T))]` (1)
+ *
+ *  Where `k` is the rate constant, `c''` is a scaling factor, `H_a` is the
+ *  enthalpy of activation, `S` is the entropy, `H_d` is the enthalpy of
+ *  deactivation, `T` is the absolute temperature, and `R` is the ideal gas
+ *  constant.
+ *
+ *  By defining `c` such that `c'' = exp(c)`, this can be written as:
+ *
+ *  > `k = T * exp(c - H_a / (R * T)) / [1 + exp(S / R) * exp(-H_d / (R * T))]` (2)
+ *
+ *  This is the form used here. Typically `c` is chosen such that `k` is equal
+ *  to 1 at a particular reference temperature, often 25 degrees C. Then `k`
+ *  represents a relative reaction rate.
+ *
+ *  Note that a similar equation is also derived in Sharpe and DeMichele (1977),
+ *  where the main difference is the inclusion of an additional entropy term.
+ *  As this new `S` approaches negative infinity, the equation from Sharpe and
+ *  DeMichele approaches the Johnson-Eyring-Williams equation.
+ *
+ *  For an in-depth discussion of these equations and their role in plant
+ *  modeling, see Murphy & Stinziano (2021) and Yin (2021).
+ *
+ *  Note that this equation is equivalent to a "peaked Arrhenius response" with
+ *  an additional multiplicative factor of `T`. We use this to simplify the
+ *  calculations in the code.
+ *
+ *  References:
+ *  - [Johnson, F. H., Eyring, H. & Williams, R. W. Journal of Cellular and
+ *    Comparative Physiology 20, 247–268 (1942)](https://doi.org/10.1002/jcp.1030200302)
+ *
  *  - [Yang, J. T., Preiser, A. L., Li, Z., Weise, S. E. & Sharkey, T. D. Planta
  *    243, 687–698 (2016)](10.1007/s00425-015-2436-8)
+ *
+ *  - [Sharpe, P. J. H. & DeMichele, D. W. Journal of Theoretical Biology 64,
+ *    649–670 (1977)](https://doi.org/10.1016/0022-5193(77)90265-X)
+ *
+ *  - [Murphy, B. K. & Stinziano, J. R. New Phytologist 231, 2371–2381 (2021)]
+ *    (https://doi.org/10.1111/nph.16883)
+ *
+ *  - [Yin, X. New Phytologist 231, 2113–2116 (2021)]
+ *    (https://doi.org/10.1111/nph.17341)
  *
  *  @param [in] c Dimensionless scaling parameter
  *
@@ -112,15 +308,7 @@ inline double johnson_eyring_williams_response(
     double temperature_k  // K
 )
 {
-    using physical_constants::ideal_gas_constant;  // J / k / mol
-
-    double const top =
-        temperature_k * arrhenius_exponential(c, Ha, temperature_k);
-
-    double const bot =
-        1.0 + arrhenius_exponential(S / ideal_gas_constant, Hd, temperature_k);
-
-    return top / bot;
+    return temperature_k * peaked_arrhenius_response(c, Ha, Hd, S, temperature_k);
 }
 
 /**
