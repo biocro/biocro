1b80872662033b710433a1d2efe36bbc78f4a24b
Yufeng He <34378264+yufenghe22@users.noreply.github.com>
2026-04-01 16:24:13 -0500

    use swc as the driving variable

--- src/module_library/stomata_water_stress_linear_v2.h
+++ src/module_library/stomata_water_stress_linear_v2.h
@@ -59,11 +59,10 @@ void stomata_water_stress_linear_v2::do_operation() const
     double soil_field_capacity = *soil_field_capacity_ip;
     double soil_water_content = *soil_water_content_ip;
 
-    double REW = (soil_water_content - soil_wilting_point) / (soil_field_capacity - soil_wilting_point);
-    
     // This linear fitted equation is from a reverse engineering of fitting observed gs data
     // Ref: Gray, S., Dermody, O., Klein, S. et al. Intensifying drought eliminates the expected benefits of elevated carbon dioxide for soybean. Nature Plants 2, 16132 (2016). https://doi.org/10.1038/nplants.2016.132
-    double linear_fit = 0.27 * REW + 0.739;
+    double linear_fit = 1.88 * soil_water_content + 0.379;
+    // From my data fitting, I do not see any StomataWS values beyond this range
     double StomataWS_min = 0.5;
     double StomataWS_max = 1.5;
 
