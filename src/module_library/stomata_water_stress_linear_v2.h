4a5d20d84cd2e4e3b6644980640d9a5174ba4ab2
Yufeng He <34378264+yufenghe22@users.noreply.github.com>
2026-04-14 09:16:30 -0500

    make input references

--- src/module_library/stomata_water_stress_linear_v2.h
+++ src/module_library/stomata_water_stress_linear_v2.h
@@ -12,10 +12,10 @@ class stomata_water_stress_linear_v2 : public direct_module
     stomata_water_stress_linear_v2(state_map const& input_quantities, state_map* output_quantities)
         : direct_module{},
 
-          // Get pointers to input quantities
-          soil_field_capacity_ip{get_ip(input_quantities, "soil_field_capacity")},
-          soil_wilting_point_ip{get_ip(input_quantities, "soil_wilting_point")},
-          soil_water_content_ip{get_ip(input_quantities, "soil_water_content")},
+          // Get references to input quantities
+          soil_field_capacity{get_input(input_quantities, "soil_field_capacity")},
+          soil_wilting_point{get_input(input_quantities, "soil_wilting_point")},
+          soil_water_content{get_input(input_quantities, "soil_water_content")},
 
           // Get pointers to output quantities
           StomataWS_op{get_op(output_quantities, "StomataWS")}
@@ -26,10 +26,10 @@ class stomata_water_stress_linear_v2 : public direct_module
     static std::string get_name() { return "stomata_water_stress_linear_v2"; }
 
    private:
-    // Pointers to input quantities
-    const double* soil_field_capacity_ip;
-    const double* soil_wilting_point_ip;
-    const double* soil_water_content_ip;
+    // References to input quantities
+    const double& soil_field_capacity_ip;
+    const double& soil_wilting_point_ip;
+    const double& soil_water_content_ip;
 
     // Pointers to output quantities
     double* StomataWS_op;
@@ -54,18 +54,13 @@ string_vector stomata_water_stress_linear_v2::get_outputs()
 
 void stomata_water_stress_linear_v2::do_operation() const
 {
-    // Collect inputs and make calculations
-    double soil_wilting_point = *soil_wilting_point_ip;
-    double soil_field_capacity = *soil_field_capacity_ip;
-    double soil_water_content = *soil_water_content_ip;
-     
-    double REW = (soil_water_content - soil_wilting_point) / (soil_field_capacity - soil_wilting_point);
+    // We assume StomataWS is between 0-1 
+    constexpr double StomataWS_min = 0.0;
+    constexpr double StomataWS_max = 1.0;
+    const double REW = (soil_water_content - soil_wilting_point) / (soil_field_capacity - soil_wilting_point);
     // This linear fitted equation is from a reverse engineering of fitting observed gs data
     // Ref: Gray, S., Dermody, O., Klein, S. et al. Intensifying drought eliminates the expected benefits of elevated carbon dioxide for soybean. Nature Plants 2, 16132 (2016). https://doi.org/10.1038/nplants.2016.132
     double linear_fit = 0.641 + 0.139 * REW; 
-    // We assume StomataWS is between 0-1 
-    const double StomataWS_min = 0.0;
-    const double StomataWS_max = 1.0;
 
     // Update the output quantity list
     update(StomataWS_op, std::min(std::max(linear_fit, StomataWS_min), StomataWS_max));
