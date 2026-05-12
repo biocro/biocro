6b89cac79c8e2ceae6817670aa36775305936e16
eloch216 <48919455+eloch216@users.noreply.github.com>
2026-05-12 15:55:46 -0700

    Remove blank line that was added previously

--- src/module_library/c3photo.h
+++ src/module_library/c3photo.h
@@ -4,7 +4,6 @@
 #include "photosynthesis_outputs.h"   // for photosynthesis_outputs
 #include "c3_temperature_response.h"  // for c3_temperature_response_parameters
 
-
 photosynthesis_outputs c3photoC(
     c3_temperature_response_parameters const tr_param,
     double const absorbed_ppfd,
