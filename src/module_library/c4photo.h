a7d3ef492374fdc38b4702d25303365e854ee391
eloch216 <48919455+eloch216@users.noreply.github.com>
2026-08-03 10:39:18 -0700

    No lambda side effects in c4photoC

--- src/module_library/c4photo.h
+++ src/module_library/c4photo.h
@@ -3,25 +3,52 @@
 
 #include "photosynthesis_outputs.h"  // for photosynthesis_outputs
 
+double collatz_assim(
+    double const atmospheric_pressure,  // Pa
+    double const beta,                  // dimensionless
+    double const Ci_pa,                 // Pa
+    double const kT,                    // mol / m^2 / s
+    double const M,                     // micromol / m^2 / s
+    double const RT                     // micromol / m^2 / s
+);
+
+double check_c4_assim_rate(
+    double const ambient_temperature,   // degrees C
+    double const atmospheric_pressure,  // Pa
+    double const bb0_adj,               // mol / m^2 / s
+    double const bb1_adj,               // dimensionless
+    double const beta,                  // dimensionless
+    double const Ca,                    // micromol / mol
+    double const Ca_pa,                 // Pa
+    double const Ci_pa,                 // Pa
+    double const gbw,                   // mol / m^2 / s
+    double const kT,                    // micromol / m^2 / s
+    double const leaf_temperature,      // degrees C
+    double const M,                     // micromol / m^2 / s
+    double const relative_humidity,     // dimensionless
+    double const RT                     // micromol / m^2 / s
+);
+
 photosynthesis_outputs c4photoC(
-    double const Qp,
-    double const leaf_temperature,
-    double const ambient_temperature,
-    double const relative_humidity,
-    double const Vcmax_at_25,
-    double const alpha,
-    double const kparm,
-    double const theta,
-    double const beta,
-    double const RL,
-    double const bb0,
-    double const bb1,
-    double const Gs_min,
-    double const StomaWS,
-    double const Ca,
-    double const atmospheric_pressure,
-    double const upperT,
-    double const lowerT,
-    double const gbw);
+    double const Qp,                    // micromol / m^2 / s
+    double const leaf_temperature,      // degrees C
+    double const ambient_temperature,   // degrees C
+    double const relative_humidity,     // dimensionless from Pa / Pa
+    double const Vcmax_at_25,           // micromol / m^2 / s
+    double const alpha,                 // mol / mol
+    double const kparm,                 // mol / m^2 / s
+    double const theta,                 // dimensionless
+    double const beta,                  // dimensionless
+    double const RL_at_25,              // micromol / m^2 / s
+    double const bb0,                   // mol / m^2 / s
+    double const bb1,                   // dimensionless from [mol / m^2 / s] / [mol / m^2 / s]
+    double const Gs_min,                // mol / m^2 / s
+    double const StomaWS,               // dimensionless
+    double const Ca,                    // micromol / mol
+    double const atmospheric_pressure,  // Pa
+    double const upperT,                // degrees C
+    double const lowerT,                // degrees C
+    double const gbw                    // mol / m^2 / s
+);
 
 #endif
