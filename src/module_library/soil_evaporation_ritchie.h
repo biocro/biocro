5e8a128c18a0818e9cbc0187bac7c0574f4eacd3
eloch216 <48919455+eloch216@users.noreply.github.com>
2026-08-03 14:18:20 -0700

    Relax Euler requirement and update some units

--- src/module_library/soil_evaporation_ritchie.h
+++ src/module_library/soil_evaporation_ritchie.h
@@ -163,10 +163,10 @@ namespace standardBML
  *  ### Notes about solver
  *
  *  The original model (and its DSSAT implementation) is formulated using
- *  difference equations rather than differential equations. For this and
- *  several other reasons, this module is only compatible with an Euler solver
- *  using a time step of 1 hour. Here are several (but not all) reasons for why
- *  such a solver is required:
+ *  difference equations rather than differential equations. Effectively, this
+ *  can be viewed as assuming an Euler solver with a fixed step size of 1 is
+ *  being used to solve a set of corresponding differential equations. Here are
+ *  several examples of where this occurs:
  *
  *  1. For several quantities (`days_stage2`, `soil_evaporation_rate`, `sumes1`,
  *     and `sumes2`), this module returns the change relative to the value at
@@ -189,9 +189,11 @@ namespace standardBML
  *     (mm / day).
  *
  *  To clarify these points, we have defined a `timestep` variable in the code
- *  below. Note that this is highly unusual, and BioCro modules generally should
- *  not need to know the size of the time step being used to solve the set of
- *  coupled differential equations.
+ *  below.
+ *
+ *  Note that although some of the equations were developed by assuming an Euler
+ *  solver with a step size of 1, this does not prevent other solvers from being
+ *  used to solve the equations.
  *
  *  ### Definitions for some parameters
  *
@@ -221,7 +223,7 @@ class soil_evaporation_ritchie : public differential_module
     soil_evaporation_ritchie(
         state_map const& input_quantities,
         state_map* output_quantities)
-        : differential_module(true),
+        : differential_module(),
 
           // Get references to input quantities
           atmospheric_pressure{get_input(input_quantities, "atmospheric_pressure")},
@@ -339,10 +341,10 @@ string_vector soil_evaporation_ritchie::get_inputs()
 string_vector soil_evaporation_ritchie::get_outputs()
 {
     return {
-        "days_stage2",            // day
-        "soil_evaporation_rate",  // Mg / ha / hr
-        "sumes1",                 // mm
-        "sumes2"                  // mm
+        "days_stage2",            // day / hr
+        "soil_evaporation_rate",  // Mg / ha / hr^2
+        "sumes1",                 // mm / hr
+        "sumes2"                  // mm / hr
     };
 }
 
@@ -649,10 +651,10 @@ void soil_evaporation_ritchie::do_operation() const
     double const delta_ES = ES - old_ES;                              // mm / hr
 
     // Update the output quantity list
-    update(sumes1_op, delta_sumes1);                               // mm
-    update(sumes2_op, delta_sumes2);                               // mm
-    update(days_stage2_op, delta_days_stage2);                     // day
-    update(soil_evaporation_rate_op, delta_ES * mm_to_Mg_per_ha);  // Mg / ha / hr
+    update(sumes1_op, delta_sumes1 / timestep);                               // mm / hr
+    update(sumes2_op, delta_sumes2 / timestep);                               // mm / hr
+    update(days_stage2_op, delta_days_stage2 / timestep);                     // day / hr
+    update(soil_evaporation_rate_op, delta_ES * mm_to_Mg_per_ha / timestep);  // Mg / ha / hr^2
 }
 
 }  // namespace standardBML
